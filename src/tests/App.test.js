c98febc52fc4aef4256c69070f51520f371c59fd
Mateus Coury <mateuscoury@gmail.com>
2021-02-09 16:49:50 -0300

    primeiro exercicio

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -11,4 +14,54 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+test('verifica se existe os links', () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={ history }>
+      <App />
+    </Router>,
+  );
+  const homeLink = screen.getByRole('link', {
+    name: 'Home',
+  });
+  expect(homeLink).toBeInTheDocument();
+
+  userEvent.click(homeLink);
+
+  const pokedexText = screen.getByRole('heading', {
+    level: 2,
+    name: /Encountered pokémons/i,
+  });
+  expect(pokedexText).toBeInTheDocument();
+
+  const aboutLink = screen.getByRole('link', {
+    name: 'About',
+  });
+  expect(aboutLink).toBeInTheDocument();
+
+  userEvent.click(aboutLink);
+
+  const aboutText = screen.getByRole('heading', {
+    level: 2,
+    name: /About Pokédex/i,
+  });
+  expect(aboutText).toBeInTheDocument();
+
+  const favoriteLink = screen.getByRole('link', {
+    name: /favorite/i,
+  });
+  expect(favoriteLink).toBeInTheDocument();
+
+  userEvent.click(favoriteLink);
+
+  const favoriteText = screen.getByRole('heading', {
+    level: 2,
+    name: /Favorite pokémons/i,
+  });
+  expect(favoriteText).toBeInTheDocument();
+
+  history.push('/algumacoisa/');
+  const notfound = screen.getByText(/Page requested not found/i);
+  expect(notfound).toBeInTheDocument();
 });
