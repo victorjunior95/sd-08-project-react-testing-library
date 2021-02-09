d7bc681dd2b0d6c26d8c57c2620d79ee89f22052
wesleypribeiro <wesley_paulo00@hotmail.com>
2021-02-09 16:23:43 -0300

    R01 - Concluido

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
+import { About, FavoritePokemons } from '../components';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -13,6 +14,21 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
+test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const homeLink = getByText(/home/i);
+  const aboutLink = getByText(/about/i);
+  const favoriteLink = getByText(/favorite pokémons/i);
+  expect(homeLink).toBeInTheDocument();
+  expect(aboutLink).toBeInTheDocument();
+  expect(favoriteLink).toBeInTheDocument();
+});
+
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
     <MemoryRouter initialEntries={ ['/'] }>
@@ -21,4 +37,34 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('shows the About when the route is `/about`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/about'] }>
+      <About />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('About Pokédex')).toBeInTheDocument();
+});
+
+test('shows the favorites when the route is `/favorites`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/favorites'] }>
+      <FavoritePokemons />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Favorite pokémons')).toBeInTheDocument();
+});
+
+test('Página Not Found ao entrar em uma URL desconhecida', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/pagina/que-nao-existe'] }>
+      <App />
+    </MemoryRouter>,
+  );
+  const noMatch = getByText(/Page requested not found/i);
+  expect(noMatch).toBeInTheDocument();
 });
