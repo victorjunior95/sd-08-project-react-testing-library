2e84b2807ddcde3fe611ef81b21d3fa79b2c8a0a
Seu nome <luiza.dilly@gmail.com>
2021-02-09 20:58:53 -0300

    REQ 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('REQUISITO 3', () => {
+  it('É exibido na "No favorite pokemon found", se a pessoa não tiver pokémons favoritos',
+    () => {
+      const { getByText } = renderWithRouter(<FavoritePokemons />);
+      const text = getByText('No favorite pokemon found');
+      expect(text).toBeInTheDocument();
+    });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+
+  });
+});
