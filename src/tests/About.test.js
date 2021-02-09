2c9152d0cedc5a65dcc879ccb76841f7d91721f4
emassaki <massakihirayama@gmail.com>
2021-02-09 16:49:27 -0300

    Adicionado requisito 2 - testar a pagina About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+import { About } from '../components';
+
+describe('About Page', () => {
+  it('Should render the text "About Pokédex"', () => {
+    const { getByText } = renderWithRouter(<About />);
+
+    const aboutTitle = getByText(/About Pokédex/i);
+
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  it('Should contain two paragraphs descripting the pokédex', () => {
+    const { getAllByTestId } = renderWithRouter(<About />);
+
+    const aboutParagraphs = getAllByTestId('paragraph');
+    expect(aboutParagraphs.length).toBe(2);
+  });
+
+  it('Should contain an image with the corresponding source', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const imageURL = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    const aboutImage = getByRole('img');
+
+    expect(aboutImage.src).toBe(imageURL);
+  });
+});
