a7f2b03bec9de747b80e1a2da9397a2903109a44
Leonardo <lecastro16@gmail.com>
2021-02-09 17:40:01 -0300

    requisito 3 v0

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+it('test if the page has no Favorite Pokemon', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <FavoritePokemons />
+    </MemoryRouter>,
+  );
+  const headingFavorite = getByText(/No favorite pokemon found/);
+  expect(headingFavorite).toBeInTheDocument();
+});
