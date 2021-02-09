902e3642bea5313eee800be2a660471be153139a
hugobrr <hugobrr@gmail.com>
2021-02-09 19:58:22 -0300

    Segundo requisito passando na maquina 14%

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from '../App';
-import renderWhitRouter from './renderWithRouter';
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import About from '../components/About';
+// import renderWhitRouter from './renderWithRouter';
 
-describe('App.js ', () => {
-  test('O primeiro link deve possuir o texto Home', () => {
-    const { history } = renderWhitRouter(<App />);
-    const HomeLink = screen.getByRole('link', {
-      name: 'Home',
+describe('About.js ', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    render(<About />);
+    const aboutText = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
     });
-    userEvent.click(HomeLink);
-    const { pathname } = history.location;
-    expect(pathname).toBe('/');
+    expect(aboutText).toBeInTheDocument();
+  });
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    render(<About />);
+    const firtParagraphText = screen.getByText(
+      /This application simulates a Pokédex, a digital encliclopedia containing all*/i,
+    );
+    expect(firtParagraphText).toBeInTheDocument();
+    const secundParagraphText = screen.getByText(
+      /One can filter Pokémons by type, and see more details for each one of them/i,
+    );
+    expect(secundParagraphText).toBeInTheDocument();
+  });
+  test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    render(<About />);
+    const aboutImg = screen.getByRole('img');
+    // https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f
+    expect(aboutImg).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(aboutImg).toHaveAttribute('alt', 'Pokédex');
+    expect(aboutImg).toBeInTheDocument();
   });
 });
