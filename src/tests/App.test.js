61629e54967a432329831980713eac27f5028bb1
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-02-09 15:38:35 -0300

    Requisito 1 : Parte obrigatoria

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('1- A página principal deve ser renderizada no caminho "/".', () => {
+  it('Deve ser renderizado um texto `Pokédex`', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+    expect(pathname).toBe('/');
+  });
+});
+
+describe('2- Deve aparecer um conjunto de links fixos.', () => {
+  it('O primeiro link deve ter o texto "Home"', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', {
+      name: /Home/i,
+    });
+    expect(homeLink).toBeInTheDocument();
+  });
+  it('O segundo link deve ter o texto "About"', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', {
+      name: /About/i,
+    });
+    expect(aboutLink).toBeInTheDocument();
+  });
+  it('O terceiro link deve ter o texto "Favorite Pokémons"', () => {
+    renderWithRouter(<App />);
+    const favoriteLink = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    expect(favoriteLink).toBeInTheDocument();
+  });
 });
