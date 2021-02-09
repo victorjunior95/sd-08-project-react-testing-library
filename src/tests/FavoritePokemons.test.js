1c33dc8e3c3896e2c44686b9d6989b508a88c7e2
Igor Nascimento <igorln96@gmail.com>
2021-02-09 19:05:27 -0300

    req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('req 3', () => {
+  it('no favorite pokemon', () => {
+    const { getByText } = render(<FavoritePokemons />);
+    const title = getByText('No favorite pokemon found');
+
+    expect(title).toBeInTheDocument();
+  });
+
+  it('with favorite pokemon', () => {
+    const { getByText, queryByText } = renderWithRouter(
+      <FavoritePokemons pokemons={ [pokemons[0]] } />,
+    );
+    const pikachu = getByText('Pikachu');
+    const charmander = queryByText('Charmander');
+    expect(pikachu).toBeInTheDocument();
+    expect(charmander).toBeNull();
+  });
+});
