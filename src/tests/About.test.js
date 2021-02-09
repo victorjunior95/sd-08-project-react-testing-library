2281317d4e93ad6c489d81b41c2029a133f85257
Mateus Coury <mateuscoury@gmail.com>
2021-02-09 17:39:28 -0300

    req2 correcao

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -18,8 +18,10 @@ test('sobre about', () => {
     name: /About Po/i,
   });
   expect(aboutPokeText).toBeInTheDocument();
-  const paragraphs = screen.getAllByTestId('paragr');
-  expect(paragraphs.length).toBe(2);
+  const paragraph1 = screen.getByText(/This application simulates a Pokédex/);
+  const paragraph2 = screen.getByText(/One can filter Pokémons by type/);
+  expect(paragraph1).toBeInTheDocument();
+  expect(paragraph2).toBeInTheDocument();
   const imagem = screen.getByRole('img');
   expect(imagem.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('pokemon favoritado', () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={ history }>
+      <FavoritePokemons />
+    </Router>,
+  );
+  const noFavPoke = screen.getByText(
+    /No fav/i,
+  );
+  expect(noFavPoke).toBeInTheDocument();
+});
