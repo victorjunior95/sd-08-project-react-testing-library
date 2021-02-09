3e9d7d8ce66f6d38bf2fb2cf131d3fa24ae124a7
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-02-09 17:53:57 -0300

    Complete requirement 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+
+import { About } from '../components';
+
+describe('About.js', () => {
+  it('shows About  info', () => {
+    const { getByText } = renderWithRouter(<About />);
+
+    const aboutInfo = getByText(/This application simulates a Pokédex/i);
+    expect(aboutInfo).toBeInTheDocument();
+  });
+
+  it('shows About heading', () => {
+    const { getByRole } = renderWithRouter(<About />);
+
+    const aboutHeading = getByRole('heading', {
+      level: 2,
+    });
+    expect(aboutHeading).toBeInTheDocument();
+  });
+
+  it('has two paragraphs', () => {
+    const { getAllByTestId } = renderWithRouter(<About />);
+
+    const aboutParagraphs = getAllByTestId('about-paragraph');
+    expect(aboutParagraphs).toHaveLength(2);
+  });
+
+  it('has a pokedex image', () => {
+    const { getByAltText } = renderWithRouter(<About />);
+
+    const aboutImage = getByAltText('Pokédex');
+    expect(aboutImage.src).toContain('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
