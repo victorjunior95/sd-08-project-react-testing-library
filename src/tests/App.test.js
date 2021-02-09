19c28edfc02d7adc7b2ea871f1307bea0485386a
victorjunior95 <victorjr.muchacho@gmail.com>
2021-02-09 19:00:01 -0300

    Requisito 1 - one kill mutant (1/3)

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,65 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('App.js ', () => {
+  //  'renderiza uma leitura com o texto `Pokédex`'
+  test('renders a reading with the text `Pokédex`', () => {
+    const { history } =  renderWithRouter(<App />);
+    // const { getByText } = render(
+    //   <MemoryRouter>
+    //     <App />
+    //   </MemoryRouter>,
+    // );
+
+    // Teste se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação.
+    const homeLink = screen.getByRole('link', {
+      name: /Home/i,
+    })
+    userEvent.click(homeLink);
+
+    const heading = screen.getByRole('heading', {
+      level: 1,
+      name: /Pokédex/i,
+    });
+    expect(heading).toBeInTheDocument();
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  // Teste se a página principal da Pokédex é renderizada ao carregar a aplicação no caminho de URL /.
+  //     'mostra a Pokédex quando a rota é `/`'
+  test('shows the Pokédex when the route is `/`', () => {
+    const { history } =  renderWithRouter(<App />);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+
+    expect(screen.getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  // Teste se o topo da aplicação contém um conjunto fixo de links de navegação.
+    // O primeiro link deve possuir o texto Home.
+    // O segundo link deve possuir o texto About.
+    // O terceiro link deve possuir o texto Favorite Pokémons.
+  // test('the header application contains a fixed set of navigation links', () => {
+  //   const
+  // })
 });
+
+
+
+
+
+
+
+
+
+// Teste se a aplicação é redirecionada para a página de About, na URL /about, ao clicar no link About da barra de navegação.
+
+// Teste se a aplicação é redirecionada para a página de Pokémons Favoritados, na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.
+
+// Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.
