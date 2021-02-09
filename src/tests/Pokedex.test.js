304bc411960f5365247e3ffc649ddf1347c02f32
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-02-09 18:18:15 -0300

    Requisito 05, 25% covered

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import { Pokedex } from '../components';
+
+describe('tests Pokédex application features with events', () => {
+  test('renders a heading level 2 with text `Encounterd pokémons`', () => {
+    const { getByText, getByRole, getByAltText } = renderWithRouter(<App />);
+    const heading = getByRole('heading', { level: 2 });
+    expect(heading).toBeInTheDocument();
+  });
+});
