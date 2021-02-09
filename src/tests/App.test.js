62a5a5a8f5311cfab7235c4886c8d89afe26c34a
Leonardo <lecastro16@gmail.com>
2021-02-09 16:06:39 -0300

    requisito 1 v0

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +12,29 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+describe('group of tests for App.js', () => {
+  it('test links from Pokedex', async () => {
+    const { getByRole } = renderWithRouter(<App />);
+
+    const pokedexHeader = getByRole('heading', {
+      level: 1,
+    });
+    expect(pokedexHeader).toBeInTheDocument();
+
+    const linkHome = getByRole('link', {
+      name: /home/i,
+    });
+    const linkAbout = getByRole('link', {
+      name: /about/i,
+    });
+    const linkFavoritePokemons = getByRole('link', {
+      name: /favorite pokémons/i,
+    });
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavoritePokemons).toBeInTheDocument();
+  });
 });
