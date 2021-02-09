60f3d0163167227a1c7a5da84754a6ec7f2ff191
Lucas Nascimento <lucasmvnascimento@gmail.com>
2021-02-09 15:38:26 -0300

    finishing Test About.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('test if the page contains information about Pokedex', () => {
+  const { getByText, getByRole, getAllByTestId } = render(
+    <MemoryRouter initialEntries={ ['/about'] }>
+      <App />
+    </MemoryRouter>,
+  );
+  const infoPokedex = getByText(/this application simulates/i);
+  expect(infoPokedex).toBeInTheDocument();
+  const aboutText = getByRole('heading', { level: 2 });
+  expect(aboutText).toHaveTextContent('About Pokédex');
+  const paragraphs = getAllByTestId('about-paragraph');
+  expect(paragraphs.length).toBe(2);
+  const pokedexImage = getByRole('img');
+  expect(pokedexImage.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
