69579d3ede02cbd0b8d426162d7eb9603e1c4f40
Paulo Simões <paulohbsimoes@gmail.com>
2021-02-09 17:29:54 -0300

    Requisito 2 concluído

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,15 +1,31 @@
+import React from 'react';
 import renderWithRouter from '../helpers/renderWithRouter';
+import About from '../components/About';
 
 describe('Teste o componente <About />', () => {
-  test.todo('Teste se a página contém as informações sobre a Pokédex.', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    const { getByText } = renderWithRouter(<About />);
     const text = 'One can filter Pokémons by type, and see more details for';
-    const { getByText } = renderWithRouter('/about');
     expect(getByText(new RegExp(text, 'i'))).toBeInTheDocument();
   });
 
-  test.todo('Teste se a página contém um heading h2 com o texto About Pokédex.');
+  test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const heading = getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    expect(heading).toBeInTheDocument();
+  });
 
-  test.todo('Teste se a página contém dois parágrafos com texto sobre a Pokédex.');
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { getAllByTestId } = renderWithRouter(<About />);
+    const headings = getAllByTestId('about-paragraph');
+    expect(headings.length).toBe(2);
+  });
 
-  test.todo('Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.');
+  test('Teste se a página contém a imagem de uma Pokédex.', () => {
+    const { getByAltText } = renderWithRouter(<About />);
+    const image = getByAltText('Pokédex');
+    expect(image).toBeInTheDocument();
+    const url = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(image.src).toBe(url);
+  });
 });
