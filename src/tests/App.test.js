8f09f3cd0a7560eadc122d4a5967a4814f80987e
emassaki <massakihirayama@gmail.com>
2021-02-09 15:43:11 -0300

    Adicionado requisito 1 - checar pagina inicial e links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,60 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Home page', () => {
+  it('should render a reading the home path "/" with the text `Pokédex`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should render a fixed navigation bar with links', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const homeLink = getByText('Home');
+    const aboutLink = getByText('About');
+    const favoriteLink = getByText('Favorite Pokémons');
+
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
+
+  it('should redirect to path "/" when "Home" link is accessed', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    userEvent.click(getByText('Home'));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('should redirect to path "/about" when "About" link is accessed', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    userEvent.click(getByText('About'));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('should redirect to path "/favorites" when "Favorite Pokémons" link is accessed',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+
+      userEvent.click(getByText('Favorite Pokémons'));
+      const { pathname } = history.location;
+      expect(pathname).toBe('/favorites');
+    });
+
+  it('should render "Not Found" page when typed an unrecognized URL', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/not-found');
+    const pageNotFoundText = getByText(/not found/i);
+    expect(pageNotFoundText).toBeInTheDocument();
+  });
 });
