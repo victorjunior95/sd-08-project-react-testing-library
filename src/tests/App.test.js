3a027b28804ffbfe5301cbdb0b5b8597d5d3789b
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-02-09 15:57:52 -0300

    req1 ok - c1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,69 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
-test('renders a heading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe.only('Testes do requisito 1', () => {
+  it('renders a heading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('shows the Pokedex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialENtries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  it('shows the nav links', () => {
+    const { getAllByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const link = getAllByRole('link');
+
+    expect(link[0].textContent).toBe('Home');
+    expect(link[1].textContent).toBe('About');
+    expect(link[2].textContent).toBe('Favorite Pokémons');
+  });
+
+  it('testing route Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('testing route About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('testing route Favorites', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('testing route 404', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/ruani');
+    const number404 = getByText(/not found/);
+    expect(number404).toBeInTheDocument();
+  });
 });
-
-test('shows the Pokedex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialENtries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
-
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
-})
