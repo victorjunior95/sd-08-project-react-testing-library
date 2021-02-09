89b29a4a5a24bb55e498f07d3224408f19f597dd
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-02-09 16:39:07 -0300

    Complete requirement 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+const favoriteLink = 'Favorite Pokémons';
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+describe('App.js', () => {
+  it('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = renderWithRouter(<App />);
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  it('shows the header nav links', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const navLinks = getAllByRole('link');
+
+    expect(navLinks[0]).toHaveTextContent('Home');
+    expect(navLinks[1]).toHaveTextContent('About');
+    expect(navLinks[2]).toHaveTextContent(favoriteLink);
+  });
+
+  it('redirects to `/` when Home link is clicked', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const homeLink = getByText('Home');
+
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('redirects to `/about` when About link is clicked', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const aboutLink = getByText('About');
+
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('redirects to `/favorites` when Favorite Pokémons link is clicked', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const favoritePokemonLink = getByText(favoriteLink);
+
+    userEvent.click(favoritePokemonLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('redirects to Not Found page when invalid url is typed', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    const route = '/wrong-url';
+    history.push(route);
+
+    const pageNotFound = getByText('Page requested not found');
+    expect(pageNotFound).toBeInTheDocument();
+  });
 });
