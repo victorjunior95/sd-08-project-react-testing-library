b4be6eebcd2be2591d8efbb51866c62a678264b6
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-02-09 13:23:56 -0300

    NotFound.test.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testando a página de link incorreto', () => {
+  it('Teste se pág contém um heading h2 com o texto "Page requested not found"', () => {
+    render(<NotFound />);
+    const h2 = screen.getByText(/Page requested not found/i);
+    expect(h2).toBeInTheDocument();
+    expect(h2.tagName).toBe('H2');
+  });
+
+  it('A página deve conter uma imagem com uma rota específica', () => {
+    render(<NotFound />);
+    const img = document.querySelector('img');
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
