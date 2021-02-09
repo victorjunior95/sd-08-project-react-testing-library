07c0ee36bf9de98511af79ed71724fa6fc571212
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-02-09 17:54:28 -0300

    adicionando requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+// import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Requisito 3', () => {
+  it('should have no favorite pokemon', () => {
+    const { queryByText, getAllByText } = renderWithRouter(<FavoritePokemons />);
+    if (!localStorage.FavoritePokemons) {
+      const noPokemonText = queryByText(/No favorite pokemon found/i);
+      expect(noPokemonText).toBeInTheDocument();
+    } else {
+      const noPokemonText = queryByText(/No favorite pokemon found/i);
+      expect(noPokemonText).not.toBeInTheDocument();
+      const favoritePokemonFromLocalStorage = JSON.parse(localStorage.FavoritePokemons);
+      const pokemonsCard = getAllByText(/More details/i);
+      expect(favoritePokemonFromLocalStorage.length).toBe(pokemonsCard.length);
+    }
+  });
+});
