4b37613c8918fa15ee7475d2b2e8c8afa4cfaee7
albertassi88 <albertassi88@gmail.com>
2021-02-09 18:37:55 -0300

    Criacao dos testes App, About e NotFound

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,60 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Testes Requisito 1', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 
+  test('Teste se o topo da aplicação contém os Links Home, About, Favorite Pokémons', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(getByText('Home')).toBeInTheDocument();
+    expect(getByText('About')).toBeInTheDocument();
+    expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
+
+  test('Teste se a aplicação é redirecionada para a página inicial, ao clicar no link Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText('Home'));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('Teste se a aplicação é redirecionada para a página de About, ao clicar no link About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText('About'));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('Teste se a aplicação é redirecionada para a página de Favorite Pokémons, ao clicar no link About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText('Favorite Pokémons'));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/oi');
+    expect(getByText(/not found/i)).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste Requisito 4', () => {
+    test('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+       const { getByRole } = render(<NotFound />);
+       
+       expect(getByRole('heading', {
+           name: /Page requested not found/i,
+           level: 2,
+       }));
+    });
+
+    test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+        const { getByRole } = renderWithRouter(<NotFound />);
+        const img = 'Pikachu crying because the page requested was not found';
+
+        expect(getByRole('img', { name: img })).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    });
+});
