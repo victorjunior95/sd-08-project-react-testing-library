2575fb760fec3781a6ecdd5c58ab35dd3969caeb
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-02-09 17:46:45 -0300

    corrige about

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,6 @@ const About = () => (
       </p>
       <p data-testid="paragraph2">One can filter Pokémons by type, and see more details for each one of them</p>
       <img
-        role='image'
         className="pokedex-image"
         src={`https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`}
         alt="Pokédex"
--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -19,18 +19,14 @@ describe.only('About.js test', () => {
     expect(info).toBeInTheDocument();
   });
   it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
-    const { getByTestId } = renderWithRouter(<About />);
-    const paragraph1 = getByTestId('paragraph1');
-    const paragraph2 = getByTestId('paragraph2');
-
-    expect(paragraph1).toBeInTheDocument();
-    expect(paragraph2).toBeInTheDocument();
+    const { container } = renderWithRouter(<About />);
+    const paragraphs = container.getElementsByTagName('p');
+    expect(paragraphs.length).toBe(2);
   });
   it('Teste se a página contém a imagem de uma Pokédex', () => {
     const { getByRole } = renderWithRouter(<About />);
     const src = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
-    const img = getByRole('image');
-    console.log(img.src);
+    const img = getByRole('img');
     expect(img.src).toBe(src);
   });
 });
