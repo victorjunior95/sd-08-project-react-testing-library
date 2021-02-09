922ddf63d30611e2a644c0e535997192ff4e7bab
jnpefo <jnpefo@gmail.com>
2021-02-09 17:46:54 -0300

    Req 2, tentativa 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { getByRole, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
 import About from '../components/About';
 
