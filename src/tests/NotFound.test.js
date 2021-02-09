b1dd3746a324a169abb16e4accb0f1e84c9ed706
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-02-09 16:46:39 -0300

    testing NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('Tests Not Found', () => {
+  const { getByRole } = render(<NotFound />);
+
+  const heading = getByRole('heading', {
+    level: 2,
+    name: 'Page requested not found Crying emoji',
+  });
+
+  const image = getByRole('img', {
+    name: 'Pikachu crying because the page requested was not found',
+  });
+
+  expect(heading).toBeInTheDocument();
+  expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
