d00dcdacb63f4217cea1710fc0cb4cc7bbb35584
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-02-09 17:36:52 -0300

    about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -30,6 +30,7 @@ describe.only('About.js test', () => {
     const { getByRole } = renderWithRouter(<About />);
     const src = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
     const img = getByRole('image');
+    console.log(img.src);
     expect(img.src).toBe(src);
   });
 });
