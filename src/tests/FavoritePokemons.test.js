553e5f9281ab56bb321e431c48eb87253b128e8c
Silvio Dayube <scarige@gmail.com>
2021-02-09 17:46:09 -0300

    Tests if Req 3 is passing

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+describe('Tests "Favorite Pokémons" page', () => {
+  it('renders a subheading with the text `Favorite pokémons`', () => {
+    const { getByRole } = render(
+      <MemoryRouter initialEntries={ ['/favorites'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const subHeading = getByRole('heading', { level: 2 });
+    expect(subHeading).toContainHTML('Favorite pokémons');
+  });
+
+  it('renders a message when no favorite is found', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/favorites'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const noFavorite = getByText(/No favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
