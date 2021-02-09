283b0605cb8044c9530a1e727a3e553bed81f5c5
JRGCast <joao.ralph1@gmail.com>
2021-02-09 20:28:53 -0300

    favoritePokemons testado e strykeado, passou

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('tests for FavoritePokemons.js', () => {
+  it('shows the title `Favorite pokémons`', () => {
+    const { getByRole } = renderWithRouter(<FavoritePokemons />);
+    const title = getByRole('heading', { level: 2, name: 'Favorite pokémons' });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('shows not found favorite pokemons message correctly', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    const message = getByText('No favorite pokemon found');
+    expect(message).toBeInTheDocument();
+  });
+});
