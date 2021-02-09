529fb3ec7b835bdec0846664e5d761ce0fb999e2
Ediberto Oliveira <edibertooliveira@aol.com>
2021-02-09 16:43:20 -0300

    feat: test about requirement 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { About } from '../components';
+
+describe('Teste o componente <About.js />', () => {
+  test('se a página contém as informações sobre a Pokédex.', () => {
+    const { getAllByText } = render(<About />);
+    expect(getAllByText(/pokédex/gi));
+  });
+
+  test('se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const { getByRole } = render(<About />);
+    expect(getByRole('heading', {
+      level: 2,
+    }));
+  });
+
+  test('se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { container } = render(<About />);
+    const paragraph = container.querySelectorAll('p');
+    expect(paragraph.length).toBe(2);
+  });
+
+  test('se a página contém a seguinte imagem de uma Pokédex:', () => {
+    const { getByRole } = render(<About />);
+    const image = getByRole('img', {
+      name: /pokédex/i,
+    });
+    const http = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(image).toHaveAttribute('src', http);
+  });
+});
