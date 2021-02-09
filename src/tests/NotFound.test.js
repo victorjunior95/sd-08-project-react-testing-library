a65e9b219785f49ba7b6ae0ad27f49abf0524243
Lucas Gama <lucasbg@protonmail.com>
2021-02-09 16:37:28 -0300

    Requisito 4: Testa o componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Requisito 4', () => {
+  test('Verifica conteúdo da página notFound', () => {
+    const { getByRole, getByAltText } = renderWithRouter(<NotFound />);
+    const title = getByRole('heading', { level: 2, name: /Page requested not found/ });
+    expect(title).toBeInTheDocument();
+    const img = getByAltText(/Pikachu crying/);
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
