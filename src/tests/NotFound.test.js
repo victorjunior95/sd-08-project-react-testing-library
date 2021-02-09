1f9c6e24a842b286768796336948e12217da5544
Lucas Nascimento <lucasmvnascimento@gmail.com>
2021-02-09 16:36:57 -0300

    finish NotFound test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('tests if Not Found page has the text and the image', () => {
+  const { getByRole, getAllByRole } = render(
+    <MemoryRouter initialEntries={ ['/digimons'] }>
+      <App />
+    </MemoryRouter>,
+  );
+  const notFoundText = getByRole('heading', { level: 2 });
+  expect(notFoundText).toHaveTextContent('Page requested not found 😭');
+  const notFoundImage = getAllByRole('img');
+  expect(notFoundImage[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
