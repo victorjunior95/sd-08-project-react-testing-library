d63cf864e4c541f999f81d76b2136ae4bd6ba133
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-02-09 16:31:20 -0300

    Requisito 3 : Parte obrigatoria

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('1- Caso não exista pokemons favoritados.', () => {
+  it('A página deve renderizar a mensagem "No favorite pokemon found"', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const heading = getByText(/No favorite pokemon found/i);
+    expect(heading).toBeInTheDocument();
+  });
+});
