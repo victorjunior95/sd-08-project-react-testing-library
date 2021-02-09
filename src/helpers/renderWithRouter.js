255b806a0ae43c49b13d819299a933c6635b4037
Paulo Simões <paulohbsimoes@gmail.com>
2021-02-09 15:36:49 -0300

    Adiciona helper renderWithRouter

---
+++ src/helpers/renderWithRouter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return { ...<Router history={ history }>{ component }</Router>, history };
+};
+
+export default renderWithRouter;
