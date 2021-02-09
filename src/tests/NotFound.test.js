328b593ecbfaeac8ef19ba496b502a1429281293
Clênio Galvão <clenio.cgm@gmail.com>
2021-02-09 20:07:33 -0300

    Resolve quesito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
+
+test('página contém um heading h2 com o texto Page requested not found', () => {
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/outropath');
+  const noMatch = screen.getByRole('heading', {
+    level: 2,
+    name: /Page requested not found/i,
+  });
+  expect(noMatch).toBeInTheDocument();
+});
+
+test('Se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/outropath');
+  const imagem = screen.getByRole('img', {
+    name: 'Pikachu crying because the page requested was not found',
+  });
+  expect(imagem.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
