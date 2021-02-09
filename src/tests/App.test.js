d05d3af4b8369457a5467c4ccd0020b48f7526fd
Digo-Castro <dygo.castro@gmail.com>
2021-02-09 18:43:06 -0300

    Implementa 'renderWithRouter' e modifica a estrutura do 'App.test'

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { history } = renderWithRouter(<App />);
+
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
