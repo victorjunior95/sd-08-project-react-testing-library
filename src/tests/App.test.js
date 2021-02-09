4895bc83a9c3c53ef5ae162f701c8dc6ec574bae
MEianelli <filipelouzano@gmail.com>
2021-02-09 15:58:15 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,62 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Requisito 1', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.',
+    () => {
+      renderWithRouter(<App />);
+      const links = screen.getAllByRole('link');
+      expect(links[0].innerHTML).toBe('Home');
+      expect(links[1].innerHTML).toBe('About');
+      expect(links[2].innerHTML).toBe('Favorite Pokémons');
+    });
+
+  it('Teste se a aplicação é redirecionada para a página inicial',
+    () => {
+      renderWithRouter(<App />);
+      const links = screen.getAllByRole('link');
+      userEvent.click(links[0]);
+      const homeH2 = screen.getByText('Encountered pokémons');
+      expect(homeH2).toBeInTheDocument();
+    });
+
+  it('Teste se a aplicação é redirecionada para a página de About',
+    () => {
+      renderWithRouter(<App />);
+      const links = screen.getAllByRole('link');
+      userEvent.click(links[1]);
+      const aboutH2 = screen.getByText('About Pokédex');
+      expect(aboutH2).toBeInTheDocument();
+    });
+
+  it('Teste se a aplicação é redirecionada para a página de Pokémons Favoritados',
+    () => {
+      renderWithRouter(<App />);
+      const links = screen.getAllByRole('link');
+      userEvent.click(links[2]);
+      const favsH2 = screen.getByText('Favorite pokémons');
+      expect(favsH2).toBeInTheDocument();
+    });
+
+  it('Teste se a aplicação é redirecionada para a página Not Found',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/ajidsjaoisdj');
+      const notFound = screen.getByText('Page requested not found');
+      expect(notFound).toBeInTheDocument();
+    });
 });
