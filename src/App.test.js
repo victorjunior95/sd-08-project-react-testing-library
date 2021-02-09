83842cf2c5fd90a850d63f76abc38d8e8e90b8b7
rafasysop <rafaelcostamoura@gmail.com>
2021-02-09 18:27:46 -0300

    iniciando App.test

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+test('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>,
+    );
+  
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
