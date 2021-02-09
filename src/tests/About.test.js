f68e709b70ba884cb73531a6d4f10156d003b42e
Silvio Dayube <scarige@gmail.com>
2021-02-09 17:21:46 -0300

    Tests if Req 2 is passing

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+describe('Tests "About" page', () => {
+  it('renders a subheading with the text `About Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/about'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const subHeading = getByText(/About Pokédex/i);
+    expect(subHeading).toBeInTheDocument();
+  });
+
+  it('renders an image of the selected pokémon', () => {
+    const { getByAltText } = render(
+      <MemoryRouter initialEntries={ ['/about'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const pokedexImage = getByAltText('Pokédex');
+    expect(pokedexImage.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
