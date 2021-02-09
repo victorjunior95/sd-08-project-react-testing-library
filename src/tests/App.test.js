221cf12a458fa361e63b67a4e995abdc77a0bdcd
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-02-09 14:47:09 -0300

    Req01: testa conjunto de links de navegaçao

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
+import { history, createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 import App from '../App';
 
@@ -11,5 +12,17 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
-  
+});
+
+test('testa conjunto de links de navegação', () => {
+  const { getAllByRole } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const links = getAllByRole('link');
+  expect(links.length).toBe(4);
+  expect(links[0]).toHaveTextContent(/home/i);
+  expect(links[1]).toHaveTextContent(/about/i);
+  expect(links[2]).toHaveTextContent(/favorite pokémons/i);
 });
