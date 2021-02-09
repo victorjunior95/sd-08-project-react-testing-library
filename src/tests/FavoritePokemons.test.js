bcd8c0bd4b03ebead1c9fe348f07318dc166980a
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-02-09 20:24:31 -0300

    REQ 3: Implemented favorite message test for FavoritePokemons.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Testing component FavoritePokemons.js', () => {
+  test('It must show up a message if there is no favorited pokémon', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const noFavoriteMessage = 'No favorite pokemon found';
+    expect(getByText(noFavoriteMessage)).toBeInTheDocument();
+  });
+});
