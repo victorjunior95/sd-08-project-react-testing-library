2c72e13b75130d98754d7b685b4396186b1676be
Daniel <danieldsrd@gmail.com>
2021-02-09 17:29:26 -0300

    Bug requisito 6

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+test('testing pokemon card', () => {
+  const history = createMemoryHistory();
+  const { getByTestId, getByAltText, getByRole, getByText } = render(
+    <Router history={ history }>
+      <App />
+    </Router>,
+  );
+  const pokemonLink = getByRole('link', { name: /more details/i });
+  userEvent.click(pokemonLink);
+  const { pathname } = history.location;
+  const text = getByText('Pikachu Details');
+  expect(text).toBeInTheDocument();
+  expect(pathname).toBe('/pokemons/25');
+
+  const textDetails = getByText('Pikachu Details');
+  expect(textDetails).toBeInTheDocument();
+  // const pokemonLinkDetails = getByText(/more details/i);
+  // expect(pokemonLinkDetails).toBeNull();
+});
