01bf5c22bbfdb2c7dcc76709c0d9b34d9767b82d
Paulo Simões <paulohbsimoes@gmail.com>
2021-02-09 17:57:19 -0300

    Requisito 4 concluído

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound />', () => {
+  test(`Teste se página contém um heading h2 com o texto Page 
+requested not found 😭`, () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const regex = new RegExp('Page requested not found', 'i');
+    expect(getByRole('heading', { level: 2, name: regex })).toBeInTheDocument();
+  });
+
+  test(`Teste se página mostra a imagem 
+https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.`, () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const alt = 'Pikachu crying because the page requested was not found';
+    const image = getByAltText(new RegExp(alt, 'i'));
+    expect(image).toBeInTheDocument();
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image.src).toBe(url);
+  });
+});
