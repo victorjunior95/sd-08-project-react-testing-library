968403aaaaaeabbd44c3f7c73f27a8bd76cabb8d
FelipeBelarmino <70665848+FelipeBelarmino@users.noreply.github.com>
2021-02-09 16:28:42 -0300

    renderWithRouter e tests adicionados ao App.js e testados

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,66 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Testes no App.js', () => {
+  //Testes de texto
+  test('Existe o h1 `Pokédex` ?', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Existe o texto `Home` ?', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Home/i);
+    expect(home).toBeInTheDocument();
+  });
+
+  test('Existe o texto `About` ?', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const about = getByText(/About/i);
+    expect(about).toBeInTheDocument();
+  });
+
+  test('Existe o texto `Favorite Pokémons` ?', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const favouritePokemons = getByText(/Favorite Pokémons/i);
+    expect(favouritePokemons).toBeInTheDocument();
+  });
+
+  //Testes de click
+  test('Deve renderizar o componente Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/');
+    const homePage = getByText(/Pokédex/i);
+    expect(homePage).toBeInTheDocument();
+  });
+
+  test('Deve renderizar o componente About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/about');
+    const aboutPage = getByText(/About Pokédex/i);
+    expect(aboutPage).toBeInTheDocument();
+  });
+
+  test('Deve renderizar o componente Favorite Pokémons', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/favorites');
+    const favoritePage = getByText(/No favorite pokemon found/i);
+    expect(favoritePage).toBeInTheDocument();
+  });
+
+  test('Deve testar página inexistente `Not found`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pagina-inexistente/');
+    const noMatch = getByText(/Page requested not found/i);
+    expect(noMatch).toBeInTheDocument();
+  });
 });
