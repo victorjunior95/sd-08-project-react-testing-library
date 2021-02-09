c62a084baafdc6e4490df5c81fde124476b0d575
Lucas Nascimento <lucasmvnascimento@gmail.com>
2021-02-09 15:49:47 -0300

    fixing About test

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,7 +4,7 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 test('test if the page contains information about Pokedex', () => {
-  const { getByText, getByRole, getAllByTestId } = render(
+  const { getByText, getByRole, getAllByRole } = render(
     <MemoryRouter initialEntries={ ['/about'] }>
       <App />
     </MemoryRouter>,
@@ -13,8 +13,10 @@ test('test if the page contains information about Pokedex', () => {
   expect(infoPokedex).toBeInTheDocument();
   const aboutText = getByRole('heading', { level: 2 });
   expect(aboutText).toHaveTextContent('About Pokédex');
-  const paragraphs = getAllByTestId('about-paragraph');
-  expect(paragraphs.length).toBe(2);
+  const paragraphOne = getByText(/This application simulates/i);
+  const paragraphTwo = getByText(/one can filter pokémons/i);
+  expect(paragraphOne).toBeInTheDocument();
+  expect(paragraphTwo).toBeInTheDocument();
   const pokedexImage = getByRole('img');
   expect(pokedexImage.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
