9ceeb8a2cc087ab3297d6dcb9742abfbf25508d8
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-02-09 18:13:27 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { getAllByRole } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('NotFound.js test', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const heading = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  it('Teste se página mostra a imagem', () => {
+    const { getAllByRole } = renderWithRouter(<NotFound />);
+    // const image = getByRole('img', {
+    //   src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    // });
+    // expect(image).toBeInTheDocument();
+    const image = getAllByRole('img');
+    expect(image[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
