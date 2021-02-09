d3c68e4f9fb8728f0ac1e3ca91bf729775901cdb
VitorLimaRios <vitorlimarios999@gmail.com>
2021-02-09 19:07:52 -0300

    first requirement

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const head = getByText(/Pokédex/i);
-  expect(head).toBeInTheDocument();
+describe('Pokédex main page', () => {
+  it('should have the path `/`', () => {
+    const { history } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  it('should have three links: home, about and favorite pokémons', () => {
+    renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    expect(links[0].textContent).toBe('Home');
+    expect(links[1].textContent).toBe('About');
+    expect(links[2].textContent).toBe('Favorite Pokémons');
+  });
+  it('should be directed to the path / when the link `Home` is pressed', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const homeLink = getByText('Home');
+    fireEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  it('should be directed to the path /about when the link `About` is pressed', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const aboutLink = getByText('About');
+    fireEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  it('should be directed to the path /favorites'
+    + ' when the link `Favorite Pokémons` is pressed',
+  () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const favoriteLink = getByText('Favorite Pokémons');
+    fireEvent.click(favoriteLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
 });
