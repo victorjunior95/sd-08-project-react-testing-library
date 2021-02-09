bcdd51a221e6eac55fad17db4dbef917e731567c
Lucas Neiva <lucassneiva.code@gmail.com>
2021-02-09 17:53:28 -0300

    fix stryker

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -38,9 +38,12 @@ describe('Teste se a página contém dois parágrafos com texto sobre a Pokédex
 
 describe('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
   it('testando se tem imagem na pagina', () => {
+
+    const THE_URL = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
     const { getByAltText } = renderWithRouter(<About />);
     const picuteAbout = getByAltText('Pokédex');
     expect(picuteAbout).toBeInTheDocument();
-    expect(getByAltText('Pokédex', picuteAbout)).toBeTruthy();
+    expect(picuteAbout.src).toContain(THE_URL);
   });
 });
