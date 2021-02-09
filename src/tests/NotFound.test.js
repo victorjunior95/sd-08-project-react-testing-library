1a9c8a8f6fbb1be4766a6985d1f48556023c6e13
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-02-09 19:11:54 -0300

    conclui testes de NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/dom';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('NotFound.js', () => {
+  test('if contain a h2 heading', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found Crying emoji/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('if contain a img', () => {
+    renderWithRouter(<NotFound />);
+
+    const displayedImage = document.querySelector('img');
+    expect(displayedImage.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
