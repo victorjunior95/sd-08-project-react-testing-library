3a80eddece67b24233bef800d2e8fd96ad61a61c
liisaambrosen <liisa.ambrosen@gmail.com>
2021-02-09 19:08:00 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+test('searches for a Home link', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/Home/i));
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+  const homeText = getByText(/Pokédex/i);
+  expect(homeText).toBeInTheDocument();
+});
+
+test('searches for an About link', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/About/i));
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+  const aboutText = getByText(/About Pokédex/i);
+  expect(aboutText).toBeInTheDocument();
+});
+
+test('searches for a Favorite Pokemon link', () => {
+  const { getByText, getByRole, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/Favorite Pokémons/i));
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+  const faveText = getByRole('heading', {
+    level: 2,
+    name: /Favorite Pokémons/i
+  });
+  expect(faveText).toBeInTheDocument();
+});
+
+test('testing Not Found Page', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/page/not-found');
+  const notFound = getByText(/Page requested not found/i);
+  expect(notFound).toBeInTheDocument();
+}) 
