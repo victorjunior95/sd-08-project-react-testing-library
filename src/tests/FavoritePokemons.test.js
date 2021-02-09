fc1d33dcf7e275a3d30d60b06eab8c856846e487
wesleypribeiro <wesley_paulo00@hotmail.com>
2021-02-09 17:46:55 -0300

    R03 - Concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../RenderWithRouter';
+import { FavoritePokemons } from '../components';
+
+test('A página deve renderizar a mensagem "No favorite pokemon found"', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+
+  expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+});
