b51d56c607db68387ab8a2141c309979b33cb7ac
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-02-09 17:40:53 -0300

    Requisito 03, 100% covered

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+test('renders the page FavoriteProkemons after click on link `Favorite Pokémons`', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const favoritePokemons = getByText('No favorite pokemon found');
+  expect(favoritePokemons).toBeInTheDocument();
+});
