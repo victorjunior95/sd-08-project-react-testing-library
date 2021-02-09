11d60056bed33c9e99c9c8b8df7d65722b7f9c54
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-02-09 14:30:37 -0300

    initial

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,7 +12,7 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
-test('shows the Pokédex when the route is `/`', () => {
+test('shows the Pokédex when the route is "/"', () => {
   const { getByText } = render(
     <MemoryRouter initialEntries={ ['/'] }>
       <App />
