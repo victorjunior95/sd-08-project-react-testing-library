e106655c8ffa79d75d7ed763eebc1ea517c10e6b
jnpefo <jnpefo@gmail.com>
2021-02-09 21:43:45 -0300

    Req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('A página contém um heading h2 com o texto Page requested not found?', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const heading = getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent('Page requested not found 😭');
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const img = getByRole(
+      'img',
+      { name: /pikachu crying because the page requested was not found/i },
+    );
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
