03d6f954d4eb9d604aed2c1de06dd0b61a1ffa0f
Uilton <rosieleserpa@hotmail.com>
2021-02-10 17:35:27 -0300

    requisito 4: testa o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('NotFound compent test', () => {
+  it('Not found page should contain an h2 with text Page requested not found 😭.', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const heading = getByRole('heading', {
+      level: 2,
+      name: 'Page requested not found Crying emoji',
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('The not found page should contain a image with the following link: https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const image = getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(image.src).toBe(src);
+  });
+});
