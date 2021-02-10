b0cf6de2bd84de89d87c86fe86ae98ec0bef1a85
Vitor Nunes <vitornunes200@gmail.com>
2021-02-10 19:58:08 -0300

    Requisito 4 - testa heading e img na pagina NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+test('verifica heading e img na pagina NotFound', () => {
+
+  const { getByText } = renderWithRouter(<NotFound />);
+  const aboutOnly = getByText(/Page requested not found/i);
+  expect(aboutOnly).toBeInTheDocument();
+
+  const imgAbout = screen.getAllByRole('img', {
+    alt: /Pikachu crying because the page requested was not found/i,
+  });
+  expect(imgAbout[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
