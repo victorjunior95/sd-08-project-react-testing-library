7df36762ccd29f8a6217a719794970647be7fbe8
VitorLimaRios <vitorlimarios999@gmail.com>
2021-02-10 19:25:47 -0300

    fourth requirement

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Favorite Pokemons page', () => {
+  it('should have a h2 with the text `Page requested not found 😭`', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const pageNotFound = getByRole('heading', { level: 2 });
+    expect(pageNotFound).toBeInTheDocument();
+    expect(pageNotFound.textContent).toBe('Page requested not found 😭');
+  });
+  it('should display a image', () => {
+    const { getAllByRole } = renderWithRouter(<NotFound />);
+    const img = getAllByRole('img');
+    expect(img[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
