46c418b9887d7cb9d1f4c47679fffa6e5aa40ad0
Aline Debastiani <debastianialine@gmail.com>
2021-02-10 17:46:08 -0300

    cumpre requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,12 +3,42 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('shows the Pokédex when the route is `/`', () => {
+test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
+    <MemoryRouter>
       <App />
     </MemoryRouter>,
   );
+  const heading = getByText(/Pokédex/i);
+  expect(heading).toBeInTheDocument();
+});
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+test('renders a link with the text Home', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const linkHomeElement = getByText(/Home/i);
+  expect(linkHomeElement).toBeInTheDocument();
+});
+
+test('renders a link with the text About', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const linkAboutElement = getByText(/About/i);
+  expect(linkAboutElement).toBeInTheDocument();
+});
+
+test('renders a link with the text Favorite Pokémons', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const linkFavoriteElement = getByText(/Favorite Pokémons/i);
+  expect(linkFavoriteElement).toBeInTheDocument();
 });
