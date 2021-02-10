9d9090cf5dc148eed6eb31b1d33c50a6cee6c81a
Cainan Coutinho <cainan6697@gmail.com>
2021-02-09 22:27:45 -0300

    3 mutantes eliminados no App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,12 +3,49 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('App.js', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+  test('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+  test('If the home link is on the page', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const homeLink = getByText(/home/i);
+    expect(homeLink).toBeInTheDocument();
+  });
+  test('If the about link is on the page', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const aboutLink = getByText(/about/i);
+    expect(aboutLink).toBeInTheDocument();
+  });
+  test('If the favorite pokemons link is on the page', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const favoriteLink = getByText(/favorite pokémons/i);
+    expect(favoriteLink).toBeInTheDocument();
+  });
 });
