ab83c67ffb640fe3e2d0f56af3a8bc9fdec76bfa
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-02-09 22:19:44 -0300

    Requisito 05, 75% covered

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { render } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import pokemons from '../data';
 import App from '../App';
@@ -14,18 +14,40 @@ describe('tests Pokédex application features with events', () => {
   // });
 
   test('next Pokémon button', () => {
-    const { getByText, getByRole, getByAltText } = renderWithRouter(<App />);
+    const { getByText } = renderWithRouter(<App />);
     const nextPokemonButton = getByText('Próximo pokémon');
     expect(nextPokemonButton).toBeInTheDocument();
   });
 
   test('renders a heading level 2 with text ``', () => {
-    const { getByText, getByRole, getByAltText } = renderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ App.setIsPokemonFavoriteById() } />);
+    const { getByRole } = renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      />,
+    );
     const heading = getByRole('heading', { level: 2 });
     expect(heading).toBeInTheDocument();
     expect(heading).toHaveTextContent('Encountered pokémons');
   });
 
+  test('type buttons text content', () => {
+    const { queryAllByTestId } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+    />);
+    const buttons = queryAllByTestId('pokemon-type-button');
+    console.log(buttons[0].textContent);
+    const types = pokemons.reduce((acc, cur) => {
+      if (!acc.includes(cur.type)) {
+        acc.push(cur.type);
+      }
+      return acc;
+    }, []);
+    buttons.forEach((pokemon, index) => {
+      expect(pokemon.textContent).toEqual(types[index]);
+    });
+  });
   // test('render heading level 2 with text `Encounterd pokémons`', () => {
   //   const { getByText, getByRole } = render(
   //     <MemoryRouter>
