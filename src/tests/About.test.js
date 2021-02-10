0d95e81ed948d4f772d42dc0ea746b653b863c8d
TandyFW <tandyfw@gmail.com>
2021-02-10 14:58:39 -0300

    Requisito 3 concluido

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -23,7 +23,6 @@ describe('Teste o componente <About.js /.', () => {
   it('Página contém dois parágrafos com texto sobre a Pokédex', () => {
     render(<About />);
     const section = screen.getByRole('img').parentNode;
-    console.log(section);
     const filhos = [...section.children].filter((element) => element.tagName === 'P');
     expect(filhos.length).toBe(2);
   });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -7,7 +7,7 @@ import renderWithRouter from './renderWithRouter';
 describe('Teste o componente <FavoritePokemons.js />', () => {
   it('Teste se é exibido na tela a mensagem No favorite pokemon found,'
     + ' se a pessoa não tiver pokémons favoritos.', () => {
-    render(<FavoritePokemons />);
+    render(<FavoritePokemons pokemons={ [] } />);
     const text = screen.getByText('No favorite pokemon found');
     expect(text).toBeInTheDocument();
   });
@@ -24,5 +24,19 @@ describe('Teste o componente <FavoritePokemons.js />', () => {
     expect(weight.length).toBe(pokemon.length);
     expect(img.length).toBe(pokemon.length);
     expect(imgFavorite.length).toBe(pokemon.length);
+  });
+
+  it('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    const names = screen.queryAllByTestId('pokemon-name');
+    const type = screen.queryAllByTestId('pokemonType');
+    const weight = screen.queryAllByTestId('pokemon-weight');
+    const img = screen.queryAllByAltText(/sprite/);
+    const imgFavorite = screen.queryAllByAltText(/marked/i);
+    expect(names.length).toBe(0);
+    expect(type.length).toBe(0);
+    expect(weight.length).toBe(0);
+    expect(img.length).toBe(0);
+    expect(imgFavorite.length).toBe(0);
   });
 });
