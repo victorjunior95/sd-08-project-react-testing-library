75c1d059b81e2b1adffb1ef12064583703c6e822
FelipeBelarmino <70665848+FelipeBelarmino@users.noreply.github.com>
2021-02-10 14:41:18 -0300

    Lint e refatoração, add test img

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import { fireEvent, screen } from '@testing-library/react';
-import renderWithRouter from '../renderWithRouter';
-import App from '../App';
+import { screen, render } from '@testing-library/react';
+import About from '../components/About';
 
 describe('Testes no About.js', () => {
   test('Teste se a página contém as informações sobre a Pokédex', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/About/i));
-    const { pathname } = history.location;
-    expect(pathname).toBe('/about');
+    const { getByText } = render(<About />);
     const aboutPage = getByText(/About Pokédex/i);
     expect(aboutPage).toBeInTheDocument();
   });
 
   test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/About/i));
-    const { pathname } = history.location;
-    expect(pathname).toBe('/about');
+    render(<About />);
     expect(screen.getByRole('heading', {
       level: 2,
       name: /About Pokédex/i,
@@ -25,20 +18,16 @@ describe('Testes no About.js', () => {
   });
 
   test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/About/i));
-    const { pathname } = history.location;
-    expect(pathname).toBe('/about');
-
+    const { getByText } = render(<About />);
     const primeiroP = getByText(/This application simulates a Pokédex/i);
     expect(primeiroP).toBeInTheDocument();
-
     const segundoP = getByText(/One can filter Pokémons by type/i);
     expect(segundoP).toBeInTheDocument();
   });
-  // test('Teste se a página contém as informações sobre a Pokédex', () => {
-  //     const { getByText } = renderWithRouter(<App />);
-  //     const pokedexInfo = getByText(/About Pokédex/i);
-  //     expect(pokedexInfo).toBeInTheDocument();
-  // });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    render(<About />);
+    const img = screen.getByAltText(/Pokédex/i);
+    expect(img).toContainHTML('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
