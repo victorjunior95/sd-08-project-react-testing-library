9a0c44f9c0c1549f914fcfe537e2834f44a34751
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-02-10 19:34:09 -0300

    renderWithRouter Implementation

---
+++ src/services/renderWithRouter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { creatMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
+const renderWithRouter = (component) => {
+  const history = creatMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>
+      { component }
+    </Router>),
+    history,
+  });
+};
+
+export default renderWithRouter
