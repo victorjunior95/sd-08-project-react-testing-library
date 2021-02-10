d5882e5e6e77eb3b7c2b2e02717e4023d33ce71c
hugobrr <hugobrr@gmail.com>
2021-02-09 21:51:59 -0300

    Terceiro requisito passando na maquina 28%

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWhitRouter from './renderWithRouter';
+
+describe('Favorite.js ', () => {
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    render(<FavoritePokemons />);
+    const favoriteText = screen.getByRole('heading', {
+      level: 2,
+      name: /Favorite pokémons/i,
+    });
+    expect(favoriteText).toBeInTheDocument();
+    const notFindPokemonText = screen.getByText(/No favorite pokemon found/i);
+    expect(notFindPokemonText).toBeInTheDocument();
+  });
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    render(<FavoritePokemons />);
+    const favoriteText = screen.getByRole('heading', {
+      level: 2,
+      name: /Favorite pokémons/i,
+    });
+    expect(favoriteText).toBeInTheDocument();
+  });
+  test('Teste se nenhum card de pokémon é exibido.', () => {
+    render(<FavoritePokemons />);
+    const favoriteText = screen.getByRole('heading', {
+      level: 2,
+      name: /Favorite pokémons/i,
+    });
+    expect(favoriteText).toBeInTheDocument();
+  });
+});
