fc87375294a66fbf31a9ea2273a8079cdf9a323f
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-02-09 21:40:42 -0300

    Requisito 05, 50% covered

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
 import App from '../App';
 import { Pokedex } from '../components';
 
 describe('tests Pokédex application features with events', () => {
-  test('renders a heading level 2 with text `Encounterd pokémons`', () => {
-    const { getByText, getByRole, getByAltText } = renderWithRouter(<App />);
-    const heading = getByRole('heading', { level: 2 });
-    expect(heading).toBeInTheDocument();
-  });
+  // test('renders a heading level 2 with text `Encounterd pokémons`', () => {
+  //   const { getByText, getByRole, getByAltText } = renderWithRouter(<App />);
+  //   const heading = getByRole('heading', { level: 2 });
+  //   expect(heading).toBeInTheDocument();
+  // });
 
   test('next Pokémon button', () => {
     const { getByText, getByRole, getByAltText } = renderWithRouter(<App />);
     const nextPokemonButton = getByText('Próximo pokémon');
     expect(nextPokemonButton).toBeInTheDocument();
   });
+
+  test('renders a heading level 2 with text ``', () => {
+    const { getByText, getByRole, getByAltText } = renderWithRouter(<Pokedex pokemons={ pokemons } isPokemonFavoriteById={ App.setIsPokemonFavoriteById() } />);
+    const heading = getByRole('heading', { level: 2 });
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent('Encountered pokémons');
+  });
+
+  // test('render heading level 2 with text `Encounterd pokémons`', () => {
+  //   const { getByText, getByRole } = render(
+  //     <MemoryRouter>
+  //       <App />
+  //     </MemoryRouter>,
+  //   );
+  //   const heading = getByRole('heading', { level: 2 });
+  //   expect(heading).toBeInTheDocument();
+  // });
 });
