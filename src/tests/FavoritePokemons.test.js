f61ffb052fb06068a56abbd7af1932ab3b252acc
Vitor Nunes <vitornunes200@gmail.com>
2021-02-10 19:40:26 -0300

    Requisito 3 - verifica mensagem de pokemons favoritos encontrados

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+test('verifica pagina de pokemons favoritos', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const favoriteList = getByText(/No favorite pokemon found/i);
+  expect(favoriteList).toBeInTheDocument();
+});
