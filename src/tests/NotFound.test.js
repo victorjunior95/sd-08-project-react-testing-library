f992a5e24df767df6e1422c75c87a2d34dbce5e7
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-02-10 15:50:06 -0300

    REQ 4: Implemented tests for NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Testing component NotFound.js', () => {
+  test('The page must contain the h2 heading `Page requested not found` ', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    expect(getByText('Page requested not found')).toBeInTheDocument();
+  });
+
+  test('The page must contain the specified image', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const pikachuCryingImage = getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    const pikachuCryinkImageLink = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(pikachuCryingImage.src).toBe(pikachuCryinkImageLink);
+  });
+});
