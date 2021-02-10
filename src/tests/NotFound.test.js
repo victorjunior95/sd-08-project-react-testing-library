61e15f0ba41fd841d9932bd0abc2c394fba2526f
Tiago <tiagaoalb@gmail.com>
2021-02-10 18:17:39 -0300

    added 4th requirement

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente NotFound', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    const { getByText, getByRole } = renderWithRouter(<NotFound />);
+    const heading = getByRole('heading', { level: 2 });
+    const headingText = getByText('Page requested not found');
+    expect(heading).toBeInTheDocument();
+    expect(headingText).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const img = getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
