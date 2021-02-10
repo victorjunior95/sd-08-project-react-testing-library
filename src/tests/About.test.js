40a59dfe05bc17bbd6924ea2d2f76aed3ea3677c
vivianeflowt <vivianeflowt@gmail.com>
2021-02-10 10:03:01 -0300

    update

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('About', () => {
+  test('Title `Pokédex`', () => {
+    render(<About />);
+    const el = screen.getByText(/This application simulates a Pokédex/i);
+    expect(el).toBeInTheDocument();
+  });
+
+  test('subtitle', () => {
+    render(<About />);
+    const el = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+    expect(el).toBeInTheDocument();
+  });
+
+  test('2 x paragraph', () => {
+    const about = render(<About />);
+    const el = about.container.getElementsByTagName('p');
+    expect(el.length).toBe(2);
+  });
+
+  test('imagem', () => {
+    render(<About />);
+    const el = screen.getByRole('img', {
+      name: /Pokédex/i,
+    });
+    expect(el).toBeInTheDocument();
+
+    expect(el).toHaveProperty(
+      'src',
+      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,45 +1,42 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('App', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(screen.getByText(/Pokédex/i)).toBeInTheDocument();
+  });
 
-test('Link `Home`', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const home = screen.getByText(/Home/);
-  expect(home).toBeInTheDocument();
-});
+  test('Link `Home`', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(screen.getByText(/Home/)).toBeInTheDocument();
+  });
 
-test('Link `About`', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const about = screen.getByText(/About/);
-  expect(about).toBeInTheDocument();
-});
+  test('Link `About`', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(screen.getByText(/About/)).toBeInTheDocument();
+  });
 
-test('link `Favorite Pokémons`', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const fav = screen.getByText(/Favorite Pokémons/);
-  expect(fav).toBeInTheDocument();
+  test('link `Favorite Pokémons`', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(screen.getByText(/Favorite Pokémons/)).toBeInTheDocument();
+  });
 });
