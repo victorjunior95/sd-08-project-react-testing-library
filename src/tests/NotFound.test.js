2869007c7ba728c0877939f3e4818cf62eac390d
Fabio Ferreira <fabio35547@gmail.com>
2021-02-10 15:01:19 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Contém um heading h2 com o texto "Page requested not found"', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+
+    const h2 = getByText(/Page requested not found/);
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { getAllByRole } = renderWithRouter(<NotFound />);
+
+    const image = getAllByRole('img');
+    expect(image[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
