a29494b699169ed218e04846f004753429d02022
Seu nome <luiza.dilly@gmail.com>
2021-02-10 11:58:59 -0300

    REQ 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('REQUISITO 4', () => {
+  it('Página contém um heading h2 com o texto Page requested not found 😭', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const notFound = getByRole('heading', {
+      level: 2, name: /Page requested not found/i,
+    });
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Página mostra a imagem', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const notfoundText = getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(notfoundText.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
