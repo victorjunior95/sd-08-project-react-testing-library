cfaa80cab56984a4c8e0bc6a4ef2d88a1176b0ea
Uilton Rocha <itoclick@gmail.com>
2021-02-09 22:42:38 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
+// import { RuleTester } from 'eslint';
+import renderWithRouter from './renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +14,16 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
-}); // ,
+});
+
+test('Testa se o link leva a Home', () => {
+  const { history } = renderWithRouter(<App />);
+  const { pathname } = history.location;
+  const linkhome = screen.getByRole(
+    'link', {
+      name: /Home/i,
+    },
+  );
+  userEvent.click(linkhome);
+  expect(pathname).toBe('/');
+});
