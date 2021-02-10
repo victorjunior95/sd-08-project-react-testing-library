14c718929459846a191e850cf9916b3378a22c90
Digo-Castro <dygo.castro@gmail.com>
2021-02-10 16:13:43 -0300

    [Requisito 4] - Completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requisito 4 - NotFound.test', () => {
+  test('Caminho feliz', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/some-route');
+    const notFound = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found crying emoji/i,
+    });
+    expect(notFound).toBeInTheDocument();
+
+    const imgPokedex = document
+      .querySelector('img[src="https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"]');
+    expect(imgPokedex).toBeInTheDocument();
+  });
+});
