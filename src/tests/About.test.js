be537e7f6510d52c004813971d313dbb6d00d23f
rafasysop <rafaelcostamoura@gmail.com>
2021-02-10 08:53:01 -0300

    Resolvendo Lint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
-import userEvent from '@testing-library/user-event';
 import About from '../components/About';
 
 describe('Testando o About', () => {
