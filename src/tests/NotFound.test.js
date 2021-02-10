738f6d2b829ce104686848aa1c9bb7e9caef3f6f
rafasysop <rafaelcostamoura@gmail.com>
2021-02-10 15:09:36 -0300

    Requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import NotFound from '../components/NotFound';
+
+test('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+  render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>,
+  );
+  const text = screen.getByText(/Page requested not found/i);
+  expect(text).toBeInTheDocument();
+});
+
+test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>,
+  );
+  const img = screen.getByRole('img', {
+    name: /pikachu crying because the page requested was not found/i,
+  });
+  const imgSRC = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  expect(img).toHaveAttribute('src', imgSRC);
+});
