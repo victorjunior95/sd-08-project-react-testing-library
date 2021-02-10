fa9f6b1986372456b5e33f9de44f3333cf8d41df
VitorLimaRios <vitorlimarios999@gmail.com>
2021-02-10 19:03:34 -0300

    third requirement

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import About from '../components/About';
 
-describe('About main page', () => {
+describe('About page', () => {
   it('should have a h2 with the text: `About Pokédex`', () => {
-    render(<About />);
+    renderWithRouter(<About />);
     const heading = screen.getByRole('heading', { level: 2 });
     expect(heading).toBeInTheDocument();
     expect(heading.textContent).toBe('About Pokédex');
   });
   it('should have two paragraphs with Pokédex information', () => {
-    const { container } = render(<About />);
+    const { container } = renderWithRouter(<About />);
     const paragraph = container.querySelectorAll('p');
     expect(paragraph[0]).toBeInTheDocument();
     expect(paragraph[0].textContent.includes('Pokédex'));
@@ -18,7 +19,7 @@ describe('About main page', () => {
     expect(paragraph[1].textContent.includes('Pokédex'));
   });
   it('should have a image of a Pokédex', () => {
-    const { getByRole } = render(<About />);
+    const { getByRole } = renderWithRouter(<About />);
     const img = getByRole('img');
     expect(img).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+const favoritePokemons = [pokemons[0], pokemons[1]];
+
+describe('Favorite Pokemons page', () => {
+  it('should display: `No favorite pokemon found`,'
+  + ' if there is no pokemon favorited', () => {
+    const { container } = renderWithRouter(<FavoritePokemons />);
+    const notFound = container.querySelector('p');
+    expect(notFound).toBeInTheDocument();
+    expect(notFound.textContent).toBe('No favorite pokemon found');
+  });
+  it('should display the favorited pokemons cards', () => {
+    const { getAllByTestId } = renderWithRouter(
+      <FavoritePokemons pokemons={ favoritePokemons } />,
+    );
+    const pokemonsCards = getAllByTestId('pokemon-name');
+    expect(pokemonsCards[0].textContent).toBe('Pikachu');
+    expect(pokemonsCards[1].textContent).toBe('Charmander');
+  });
+  it('should not display a pokemons cards that was not favorited', () => {
+    const { getAllByTestId } = renderWithRouter(
+      <FavoritePokemons pokemons={ favoritePokemons } />,
+    );
+    const pokemonsCards = getAllByTestId('pokemon-name');
+    const NotFavorited = pokemonsCards.some((item) => item.textContent === 'Alakazam');
+    expect(NotFavorited).toBe(false);
+  });
+});
