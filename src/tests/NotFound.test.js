99965015b901ad84789afd28eb986c44b37d509d
Fernanda PC <nandateixmarques@gmail.com>
2021-02-10 15:58:03 -0300

    requisito 4 - Teste o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../components/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('se página tem um heading h2 com o texto Page requested not found', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const heading = getByRole('heading', {
+    level: 2,
+    name: /Page requested not found/i,
+  });
+  expect(heading).toBeInTheDocument();
+});
+
+test('se página mostra a imagem', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const getImage = getByRole('img', {
+    name: /pikachu crying because the page requested was not found/i,
+  });
+  const urlImage = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  expect(getImage.src).toBe(urlImage);
+});
