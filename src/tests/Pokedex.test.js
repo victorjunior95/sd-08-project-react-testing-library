0596f94e609f9b390be112799b1f21f5d44868f7
JRGCast <joao.ralph1@gmail.com>
2021-02-10 14:06:37 -0300

    corrigido pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import Pokedex from '../components/Pokedex';
 
 describe('tests for Pokedex.js', () => {
   it('shows the Pokédex when the route is `/`', () => {
-    const { getByRole } = renderWithRouter(<Pokedex pokemons={ ['Pikachu', 'Charmander'] } />);
+    const { getByRole } = renderWithRouter(<Pokedex
+      pokemons={ ['Pikachu', 'Charmander'] }
+
+    />);
     expect(getByRole(
       'heading', { level: 2, name: /Encountered pokémons/ },
     )).toBeInTheDocument();
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import Pokemon from '../components/Pokemon';
+
+describe('tests for Pokemon.js', () => {
+  it('shows the title `Pokemon Pokédex` and corresponding text', () => {
+    const { getByRole, getByText } = renderWithRouter(<Pokemon />);
+    const title = getByRole('heading', { level: 2, name: /Pokemon Pokédex/ });
+    const text1 = getByText(['This application simulates a Pokédex,',
+      'a digital encliclopedia containing all Pokémons'].join(' '));
+    const text2 = getByText(['One can filter Pokémons by type,',
+      'and see more details for each one of them'].join(' '));
+    const allTextElements = [title, text1, text2];
+    allTextElements.forEach((textElement) => expect(textElement).toBeInTheDocument());
+    // expect(title && text1 && text2).toBeInTheDocument();
+  });
+
+  it('shows the pokedex image correctly', () => {
+    const { getByRole } = renderWithRouter(<Pokemon />);
+    const img = getByRole('img');
+    const src = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const alt = 'Pokédex';
+    expect(img.src).toBe(src);
+    expect(img.alt).toBe(alt);
+  });
+});
