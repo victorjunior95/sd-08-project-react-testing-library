b1bd384e3774715fbdadc6b9445f73cae409e805
Thiago Ferreira <thiagocristhian@gmail.com>
2021-02-10 20:08:15 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import RenderWithRouter from './renderWithRouter';
+
+describe('Testando o componente <NotFound.js />', () => {
+  it('Testando se pág tem um heading h2 e texto Page requested not found 😭', () => {
+    RenderWithRouter(<NotFound />);
+
+    const pokeTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found Crying emoji/i,
+    });
+
+    expect(pokeTitle).toBeInTheDocument();
+  });
+
+  it('Testando se página mostra a imagem', () => {
+    RenderWithRouter(<NotFound />);
+    const link = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const pokeImg = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+
+    expect(pokeImg.src).toBe(link);
+  });
+});
