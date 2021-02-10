e76ac68536525827053b2de6023e3c979b191743
Vagner Pereira <sousa.vagner82@gmail.com>
2021-02-10 16:09:47 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('A página contém um heading h2 com o texto Page requested not found ', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const notFoundText = getByRole('heading', {
+      name: /age requested not found/i,
+      level: 2,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+  test('Teste se página mostra a imagem', () => {
+    const imgUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    expect(getByAltText(/not found/i)).toHaveAttribute('src', imgUrl);
+  });
+});
