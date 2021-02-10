fcb70a58b07e02ac2c8447b83241f3ad0bad66e9
FelipeBelarmino <70665848+FelipeBelarmino@users.noreply.github.com>
2021-02-10 14:54:03 -0300

    NotFound testes ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testes no NotFound.js', () => {
+  test('Teste se a página contém um h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    }));
+  });
+
+  test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+    const img = screen.getByAltText(/Pikachu crying/i);
+    expect(img).toContainHTML('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
