74f54df4b8a98709ec8e677551f7e0167012eb84
victor Farias <victorfarias.tv@gmail.com>
2021-02-10 07:59:56 -0300

    Requisito 4

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -8,6 +8,7 @@ const NotFound = () => (
       <span role="img" aria-label="Crying emoji"> 😭</span>
     </h2>
     <img
+      data-testid='not-found-image'
       className="not-found-image"
       src={`https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`}
       alt="Pikachu crying because the page requested was not found"
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../components/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente "NotFound"', () => {
+  test('a página contém um h com o texto "Page requested not found"', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const heading = getByRole('heading', {
+      level: 2,
+    });
+    expect(heading).toHaveTextContent('Page requested not found');
+  });
+
+  test('a página mostra uma imagem', () => {
+    const { getByTestId } = renderWithRouter(<NotFound />);
+    const imgUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = getByTestId('not-found-image');
+    expect(img.src).toBe(imgUrl);
+  });
+});
