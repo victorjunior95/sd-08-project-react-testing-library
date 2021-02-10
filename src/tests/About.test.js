037025962275a1051a70f7aad5aab36d8935b01d
richardfazeredo <richardcompletetour@gmail.com>
2021-02-10 18:34:39 -0300

    Atualizando

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('tests the component App.js', () => {
+  it('contains info about Pokédex in the page', () => {
+    // const { getAllByRole } = renderWithRouter(<About />);
+    // const paragraphs = getAllByRole('paragraph', {
+
+    // });
+  });
+
+  it('contains a Heading with title About Pokédex in the page', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const aboutPokedex = getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+});
