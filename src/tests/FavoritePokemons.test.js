36ba79ad2c9d348faae2d5039f16e91359e9fbde
Tiago <tiagaoalb@gmail.com>
2021-02-10 16:39:12 -0300

    fixed 2nd requirement

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,8 @@
-test('', () => {});
+/* import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente FavoritePokemons', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found,',
+    'se a pessoa não tiver pokémons favoritos.', () => {
+    });
+}); */
