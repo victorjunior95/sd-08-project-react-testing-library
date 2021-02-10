9822a58702b5cf58696f565317a224e1425b96a3
vivianeflowt <vivianeflowt@gmail.com>
2021-02-10 09:09:18 -0300

    update

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -11,4 +12,34 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('Link `Home`', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const home = screen.getByText(/Home/);
+  expect(home).toBeInTheDocument();
+});
+
+test('Link `About`', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const about = screen.getByText(/About/);
+  expect(about).toBeInTheDocument();
+});
+
+test('link `Favorite Pokémons`', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const fav = screen.getByText(/Favorite Pokémons/);
+  expect(fav).toBeInTheDocument();
 });
