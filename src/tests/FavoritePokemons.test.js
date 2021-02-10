166bc47a46d8120147e932123d69276edc4154e5
Silvio Dayube <scarige@gmail.com>
2021-02-10 16:53:32 -0300

    Refatoração - Requisito 3 + Helper function

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
+import render from '../renderWithRouter';
+import pokemons from '../data';
+
+const ROUTE = { route: '/favorites' };
 
 describe('Tests "Favorite Pokémons" page', () => {
   it('renders a subheading with the text `Favorite pokémons`', () => {
-    const { getByRole } = render(
-      <MemoryRouter initialEntries={ ['/favorites'] }>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByRole } = render(<App />, ROUTE);
     const subHeading = getByRole('heading', { level: 2 });
     expect(subHeading).toContainHTML('Favorite pokémons');
   });
 
   it('renders a message when no favorite is found', () => {
-    const { getByText } = render(
-      <MemoryRouter initialEntries={ ['/favorites'] }>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByText } = render(<App />, ROUTE);
     const noFavorite = getByText(/No favorite pokemon found/i);
     expect(noFavorite).toBeInTheDocument();
+  });
+
+  it('renders cards only when there are favorites', () => {
+    const { getByText, getByLabelText } = render(<App />, ROUTE);
+
+    let parentDiv = document.querySelector('.favorite-pokemons');
+    expect(parentDiv).toBeNull();
+
+    fireEvent.click(getByText('Home'));
+    fireEvent.click(getByText('More details'));
+    fireEvent.click(getByLabelText('Pokémon favoritado?'));
+    fireEvent.click(getByText('Favorite Pokémons'));
+
+    parentDiv = document.querySelectorAll('.favorite-pokemons');
+    expect(parentDiv.length).toBe(1);
+    expect(getByText(pokemons[0].name)).toBeInTheDocument();
   });
 });
