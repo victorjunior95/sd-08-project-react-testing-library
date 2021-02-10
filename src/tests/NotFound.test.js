161fff410ae68094fabc681edd20e9bac897d49b
Silvio Dayube <scarige@gmail.com>
2021-02-10 17:18:04 -0300

    Refatoração - Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+const ROUTE = { route: '/random-string' };
 
 describe('Tests "Not Found" page', () => {
   it('renders a subheading with the text `Page requested not found 😭`', () => {
-    const { getByRole } = render(
-      <MemoryRouter initialEntries={ ['/favoritez'] }>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByRole } = renderWithRouter(<App />, ROUTE);
     const subHeading = getByRole('heading', { level: 2 });
+
     expect(subHeading).toHaveTextContent('Page requested not found 😭');
   });
 
   it('renders crying pikachu gif', () => {
-    const { getByAltText } = render(
-      <MemoryRouter initialEntries={ ['/favoritez'] }>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByAltText } = renderWithRouter(<App />, ROUTE);
     const alt = 'Pikachu crying because the page requested was not found';
     const cryingPikachu = getByAltText(alt);
+
     expect(cryingPikachu.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
