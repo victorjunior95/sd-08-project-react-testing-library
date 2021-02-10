e35eaf47c38c799701ef2b2607d148938411a156
Vitor Nunes <vitornunes200@gmail.com>
2021-02-10 17:53:49 -0300

    Requisito 2 - verifica existencia de heading e imagem

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+test('verifica heading e img na pagina About', () => {
+  const { getByText } = renderWithRouter(<About />);
+  const aboutOnly = getByText(/About Pokédex/i);
+  expect(aboutOnly).toBeInTheDocument();
+
+  const imgAbout = screen.getByRole('img', {
+    alt: /Pokédex/i,
+  });
+  expect(imgAbout).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
