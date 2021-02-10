6ca9cd2f24c1bdaae1511ed2337ab7dc55b2519d
liisaambrosen <liisa.ambrosen@gmail.com>
2021-02-10 17:57:17 -0300

    req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('checks if the favorite pokemons page loads correctly', () => {
+  test('No Favorite Pokemon found message if the user didnt add any favorites', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const noFavorites = getByText(/No favorite pokemon found/i);
+    expect(noFavorites).toBeInTheDocument();
+  })
+})
