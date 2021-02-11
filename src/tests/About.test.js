4ace0af665c4251c6d7b9bf5850f6ea94e5aa73d
Aline Debastiani <debastianialine@gmail.com>
2021-02-11 02:20:16 -0300

    cumpre requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { About } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+test('Check if contains de Pokedex info', () => {
+  const { getByRole } = renderWithRouter(<About />);
+  const aboutAll = getByRole('heading', { level: 2 });
+  expect(aboutAll).toBeInTheDocument();
+  expect(aboutAll).toHaveTextContent('About Pokédex');
+});
+
+test('Check if contains a Pokedex image', () => {
+  const pokedexImage = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  const { getByRole } = renderWithRouter(<About />);
+  const image = getByRole('img', { name: /pokédex/i });
+  expect(image).toBeInTheDocument();
+  expect(image.src).toBe(pokedexImage);
+});
