b087b36d3bfb38f4d298b2fc3b47d21b9b381d56
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-02-10 21:33:52 -0300

    req 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,17 +1,18 @@
-import React from "react";
-import { fireEvent } from "@testing-library/react";
-import { screen } from "@testing-library/dom";
-import renderWithRouter from "../renderWithRouter";
-import App from "../App";
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
 
 describe('testing the <App /> component', () => {
-  test(`If the Pokédex main page is rendered by loading the application in the / URL path`, () => {
+  test(`If the Pokédex main page is rendered by
+   loading the application in the / URL path`, () => {
     const {
       history: {
         location: { pathname },
       },
     } = renderWithRouter(<App />);
-    expect(pathname).toBe("/");
+    expect(pathname).toBe('/');
   });
 
   test(`If 
@@ -23,38 +24,43 @@ describe('testing the <App /> component', () => {
     expect(screen.getByText(/favorite pokémons/i)).toBeInTheDocument();
   });
 
-  test(`If the application is redirected to the home page, at the URL / by clicking on the Home link in the navigation bar.
+  test(`If the application is redirected to the home page,
+   at the URL / by clicking on the Home link in the navigation bar.
   `, () => {
     const { history } = renderWithRouter(<App />);
     fireEvent.click(screen.getByText(/home/i));
     const {
       location: { pathname },
     } = history;
-    expect(pathname).toBe("/");
+    expect(pathname).toBe('/');
   });
 
-  test(`If the application is redirected to the About page, in the URL / about, by clicking on the About link in the navigation bar.`, () => {
+  test(`If the application is redirected to the About page,
+   in the URL / about, by clicking on the About link in the navigation bar.`, () => {
     const { history } = renderWithRouter(<App />);
     fireEvent.click(screen.getByText(/about/i));
     const {
       location: { pathname },
     } = history;
-    expect(pathname).toBe("/about");
+    expect(pathname).toBe('/about');
   });
 
-  test(`If the application is redirected to the Favorite Pokémon page, in the URL / favorites, by clicking on the Favorite Pokémons link in the navigation bar.`, () => {
+  test(`If the application is redirected to the Favorite Pokémon page,
+   in the URL / favorites, by clicking on the
+    Favorite Pokémons link in the navigation bar.`, () => {
     const { history } = renderWithRouter(<App />);
     fireEvent.click(screen.getByText(/favorite pokémons/i));
     const {
       location: { pathname },
     } = history;
-    expect(pathname).toBe("/favorites");
+    expect(pathname).toBe('/favorites');
   });
 
-  test(`If the application is redirected to the Not Found page when entering an unknown URL.
+  test(`If the application is redirected to
+   the Not Found page when entering an unknown URL.
   `, () => {
     const { history } = renderWithRouter(<App />);
-    history.push("/page/page-not-found");
+    history.push('/page/page-not-found');
     expect(screen.getByText(/Page requested not found/i)).toBeInTheDocument();
   });
 });
