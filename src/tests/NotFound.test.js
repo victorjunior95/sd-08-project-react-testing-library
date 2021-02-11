dc1e464e82faf6d798e9854bb9c2c6edfce795ca
Uilton Rocha <itoclick@gmail.com>
2021-02-11 04:07:25 -0300

    Requisitos not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('title', () => {
+  const { history } = renderWithRouter(<App />);
+
+  history.push('xablau');
+  const el = screen.getByRole('heading', {
+    level: 2,
+    name: /Page requested not found/i,
+  });
+  expect(el).toBeInTheDocument();
+});
+
+test('imagem', () => {
+  const { history, container } = renderWithRouter(<App />);
+  history.push('xablau');
+  const el = container.querySelector('.not-found-image');
+  expect(el).toBeInTheDocument();
+
+  expect(el).toHaveProperty(
+    'src',
+    'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+  );
+});
