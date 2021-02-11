e338629da06a5d4c5df62ec4ec8d6b083a5dbc4b
Victor Moraes <moraesvictorgk@gmail.com>
2021-02-11 01:02:13 -0300

    App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
 
-describe('inicial tests', () => {
+describe('App tests', () => {
   it('renders a reading with the text `Pokédex`', () => {
     const { getByText } = render(
       <MemoryRouter>
@@ -22,5 +22,107 @@ describe('inicial tests', () => {
     );
 
     expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  it('shows the correct text at the first link', () => {
+    const { getAllByRole } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const firstLink = getAllByRole('link')[0];
+
+    expect(firstLink).toHaveTextContent('Home');
+  });
+
+  it('shows the correct text at the second link', () => {
+    const { getAllByRole } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const secondLink = getAllByRole('link')[1];
+
+    expect(secondLink).toHaveTextContent('About');
+  });
+
+  it('shows the correct text at the third link', () => {
+    const { getAllByRole } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const thirdLink = getAllByRole('link')[2];
+
+    expect(thirdLink).toHaveTextContent('Favorite Pokémons');
+  });
+
+  it('Verifies if HOME returns to the main page', () => {
+    // Esse teste foi feito com o auxílio do site:https://reactrouter.com/web/guides/testing
+    let testLocation;
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/about'] }>
+        <App />
+        <Route
+          path="*"
+          render={ ({ location }) => {
+            testLocation = location;
+            return null;
+          } }
+        />
+      </MemoryRouter>,
+    );
+    const home = getByText('Home');
+    fireEvent.click(home);
+    expect(testLocation.pathname).toBe('/');
+  });
+
+  it('Verifies if the element /about/ leads to the correct page', () => {
+    let testLocation;
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+        <Route
+          path="*"
+          render={ ({ location }) => {
+            testLocation = location;
+            return null;
+          } }
+        />
+      </MemoryRouter>,
+    );
+    const about = getByText('About');
+    fireEvent.click(about);
+    expect(testLocation.pathname).toBe('/about');
+  });
+
+  it('Verifies if the element /favorite/ leads to the correct page', () => {
+    // Esse teste foi feito com o auxílio do site:https://reactrouter.com/web/guides/testing
+    let testLocation;
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/about'] }>
+        <App />
+        <Route
+          path="*"
+          render={ ({ location }) => {
+            testLocation = location;
+            return null;
+          } }
+        />
+      </MemoryRouter>,
+    );
+    const favorites = getByText('Favorite Pokémons');
+    fireEvent.click(favorites);
+    expect(testLocation.pathname).toBe('/favorites');
+  });
+
+  it('verifies if the application leads to the not found page', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/XABLAU'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const notFoundText = getByText('Page requested not found');
+    expect(notFoundText).toBeInTheDocument();
   });
 });
