b700666dfc76b04eebb67baee918135a7acb2c50
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-02-10 22:35:14 -0300

    req 02 e 03

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/dom';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../components';
+
+describe('testing the <About.js /> component', () => {
+  test('If the page contains information about Pokédex.', () => {
+    renderWithRouter(<About />);
+    expect(screen.getByText(`This application simulates a Pokédex,
+     a digital encliclopedia containing all Pokémons`)).toBeInTheDocument();
+  });
+
+  test('If the page contains an h2 heading with the text About Pokédex.', () => {
+    renderWithRouter(<About />);
+    expect(screen.getByRole('heading')).toHaveTextContent(/about pokédex/i);
+  });
+
+  test(`If the page contains two paragraphs with text about Pokédex.
+  `, () => {
+    const { container } = renderWithRouter(<About />);
+    expect(container.querySelectorAll('p').length.toString()).toMatch('2');
+  });
+
+  test(`If the page contains the following image of a Pokédex: 
+      https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.`, () => {
+    renderWithRouter(<About />);
+    expect(screen.getByRole('img').src).toMatch('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/dom';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+import pokemons from '../data';
+
+describe('testing the <FavoritePokemons.js /> component', () => {
+  it(`If the message No favorite pokemon found is displayed on the screen,
+   if the person does not have favorite pokemon.`, () => {
+    renderWithRouter(<FavoritePokemons />);
+    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+
+  it(`If all favorite Pokémon cards are displayed.
+    `, () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    expect(screen.getByText(/Pikachu/i)).toBeInTheDocument();
+    expect(screen.getByText(/Charmander/i)).toBeInTheDocument();
+  });
+
+  it(`If no Pokémon card is displayed, if it is not favored.
+`, () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    expect(screen.getByText(/Ekans/i)).toBeInTheDocument(null);
+    expect(screen.getByText(/Dragonair/i)).toBeInTheDocument(null);
+  });
+});
