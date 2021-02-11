23d8b53593e32f05b73b9389a2042d225304abd1
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-02-11 02:39:34 -0300

    req 04 e 05

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/dom';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../components';
+
+describe('testing the <NotFound.js /> component', () => {
+  it(`
+  Test if the page contains an heading h2
+   with the text Page requested not found 😭`, () => {
+    renderWithRouter(<NotFound />);
+    expect(screen.getByRole('heading')).toHaveTextContent(/Page requested not found 😭/i);
+  });
+
+  it(' Test if the page shows the image https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    renderWithRouter(<NotFound />);
+    expect(screen.getByAltText(/Pikachu crying because the/i).src)
+      .toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,119 @@
-test('', () => {});
+
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/dom';
+import renderWithRouter from '../renderWithRouter';
+import { Pokedex } from '../components';
+import App from '../App';
+import pokemons from '../data';
+
+const pokemonTypes = [...new Set(pokemons.map((pokemon) => (pokemon.type)))];
+
+describe('testing the <Pokedex.js /> component', () => {
+  test('If the page contains an h2 heading with the text Encountered Pokémon.', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      />,
+    );
+    expect(screen.getByRole('heading')).toHaveTextContent(/Encountered pokémons/i);
+  });
+
+  test(`If the next Pokémon in the list is
+   displayed when the Next Pokémon button is clicked.`, () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      />,
+    );
+
+    expect(screen.getByTestId('next-pokemon')).toHaveTextContent(/Próximo pokémon/i);
+    pokemons.forEach((pokemon) => {
+      expect(screen.getByText(pokemon.name)).toBeInTheDocument();
+      userEvent.click(screen.getByTestId('next-pokemon'));
+    });
+    expect(screen.getByText(pokemons[0].name)).toBeInTheDocument();
+  });
+
+  test('If only one Pokémon is shown at a time.', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      />,
+    );
+    expect(screen.queryAllByTestId('pokemon-name').length).toBe(1);
+  });
+
+  test('If the Pokédex has the filter buttons.', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      />,
+    );
+    userEvent.click(screen.getByText('Fire'));
+    pokemonTypes.forEach((type) => {
+      userEvent.click(screen.getByText(type));
+      pokemons.filter((pokemon) => pokemon.type === type).forEach((pokemon) => {
+        expect(screen.getByTestId('pokemon-name')).toHaveTextContent(pokemon.name);
+        userEvent.click(screen.getByTestId('next-pokemon'));
+      });
+    });
+  });
+
+  test('If the Pokédex contains a button to reset the filter', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      />,
+    );
+    pokemons.forEach((pokemon) => {
+      expect(screen.getByText(pokemon.name)).toBeInTheDocument();
+      userEvent.click(screen.getByTestId('next-pokemon'));
+    });
+
+    expect(screen.getByText('All')).toBeInTheDocument();
+    userEvent.click(screen.getByText('All'));
+    pokemons.forEach((pokemon) => {
+      expect(screen.getByText(pokemon.name)).toBeInTheDocument();
+      userEvent.click(screen.getByTestId('next-pokemon'));
+    });
+  });
+
+  test('If a filter button is created dynamically for each type of Pokémon.', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      />,
+    );
+    expect(screen.getAllByTestId('pokemon-type-button').length).toBe(pokemonTypes.length);
+    expect(screen.getByText('All')).toBeInTheDocument();
+  });
+
+  test(`If The Next Pokémon button should be disabled
+    when the filtered list of Pokémon has only one Pokémon.`, () => {
+    renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      />,
+    );
+    pokemonTypes.forEach((type) => {
+      userEvent.click(
+        screen.getAllByTestId('pokemon-type-button')
+          .find((button) => button.innerHTML === type),
+      );
+      if (pokemons.filter((pokemon) => pokemon.type === type)
+        .length === 1) {
+        expect(screen.getByTestId('next-pokemon')).toBeDisabled();
+      } else {
+        expect(screen.getByTestId('next-pokemon')).not.toBeDisabled();
+      }
+    });
+  });
+});
