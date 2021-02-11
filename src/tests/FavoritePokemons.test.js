e06ce81450600ac8cfa148166d005db4bbbb0231
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-02-10 22:39:47 -0300

    req 02 e 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -5,20 +5,20 @@ import { FavoritePokemons } from '../components';
 import pokemons from '../data';
 
 describe('testing the <FavoritePokemons.js /> component', () => {
-  it(`If the message No favorite pokemon found is displayed on the screen,
+  test(`If the message No favorite pokemon found is displayed on the screen,
    if the person does not have favorite pokemon.`, () => {
     renderWithRouter(<FavoritePokemons />);
     expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
   });
 
-  it(`If all favorite Pokémon cards are displayed.
+  test(`If all favorite Pokémon cards are displayed.
     `, () => {
     renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
     expect(screen.getByText(/Pikachu/i)).toBeInTheDocument();
     expect(screen.getByText(/Charmander/i)).toBeInTheDocument();
   });
 
-  it(`If no Pokémon card is displayed, if it is not favored.
+  test(`If no Pokémon card is displayed, if it is not favored.
 `, () => {
     renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
     expect(screen.getByText(/Ekans/i)).toBeInTheDocument(null);
