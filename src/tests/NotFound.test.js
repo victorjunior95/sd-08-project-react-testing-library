e6725c0c39e2c668821825df7c66bdb75c768c54
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-02-11 02:03:04 -0300

    C4 - 4th Req Sent

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Req 4 - Not Found Test', () => {
+  test('1 - Shows h2 with text', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    history.push('/blablabla');
+
+    const h2 = getByRole('heading', { level: 2 });
+    expect(h2.textContent).toBe('Page requested not found 😭');
+  });
+  test('1 - Shows GIF', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/blablabla');
+    const img = getByRole('img');
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
