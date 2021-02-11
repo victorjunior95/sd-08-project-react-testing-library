c97b9b826d36f65992fddfb4bd8b3828bc5b73d0
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-02-11 01:10:55 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testing component NotFound', () => {
+  test('page contains a tag "h2" with text "Page requested not found"', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    const heading = getByRole('heading');
+    expect(heading).toHaveTextContent(/Page requested not found/i);
+  });
+
+  test('page shows an image', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+
+    const image = getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
