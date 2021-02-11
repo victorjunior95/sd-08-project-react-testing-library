8ec751175d912fa992e64824d0b5c6f7b1d5a896
victor718 <victorfelipeoliveira1@gmail.com>
2021-02-10 22:56:21 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Requisito 4 - Teste o componente \\"NotFound"\\', () => {
+  it(`Teste se página contém um heading h2 com o texto
+    Page requested not found 😭;`,
+  () => {
+    const { getByRole } = render(<NotFound />);
+    expect(getByRole('heading', {
+      name: /page requested not found/i,
+    })).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.',
+    () => {
+      const { getByRole } = render(<NotFound />);
+      const imgNotFound = getByRole('img', {
+        name: /pikachu crying because the page requested was not found/i,
+      });
+      expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    });
+});
