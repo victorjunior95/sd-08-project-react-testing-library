e61ec7fa629ba1c8143ad45a18f0a5b7003da2c5
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-02-10 21:42:27 -0300

    requisito 4 - implementation of tests on the NotFound components

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Render <NotFound/> component,`', () => {
+  it('show heading element with text `Page requested not found`', () => {
+    render(<NotFound />);
+    const noMatchHeading = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+    expect(noMatchHeading).toBeInTheDocument();
+  });
+
+  it('show image element with source `https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`', () => {
+    render(<NotFound />);
+    const noMatchImg = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(noMatchImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
