4b58fc52c8162bbc58e48f6024c73d50b3870e0c
Joao <joaocarlitoneto@gmail.com>
2021-02-11 10:00:01 -0300

    Send Requirement 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requirement 03', () => {
+  it('Teste 01 - Página contém um heading h2 com o texto Page requested notFound', () => {
+    renderWithRouter(<NotFound />);
+    const title = screen.getByRole('heading', {
+      level: 2, name: /Page requested not found/i });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Teste 02 - Página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gi', () => {
+    renderWithRouter(<NotFound />);
+    const imageURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(image.src).toBe(imageURL);
+  });
+});
