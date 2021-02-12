f6ebd9eb3998901cfebe8714c52883dd498522f8
amandaos <amanda.775.souza@gmail.com>
2021-02-12 20:27:53 -0300

    feature: Testa  o componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+test('Verifica se a página contém um heading h2 com Page requested not found', () => {
+  renderWithRouter(<NotFound />);
+  const notFoundTitle = screen.getByRole('heading', {
+    level: 2,
+    name: /Page requested not found/i,
+  });
+  expect(notFoundTitle).toBeInTheDocument();
+});
+
+test('Verifica se a página contém uma imagem', () => {
+  renderWithRouter(<NotFound />);
+  const imageURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const img = screen.getByAltText(
+    'Pikachu crying because the page requested was not found',
+  );
+  expect(img.src).toBe(imageURL);
+});
