908b2fdda7f71323db53d5b913c394c1488cd089
TandyFW <tandyfw@gmail.com>
2021-02-12 11:03:02 -0300

    Requisito 5 concluido, lint consertado

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -9,9 +9,10 @@ import mockPokemons from '../services/mockPokemons';
 import mockFavorite from '../services/mockFavorite';
 
 const POKEMON_NAME = 'pokemon-name';
+const PROXIMO_POKEMON = 'Próximo pokémon';
 
 const pokemonNameTest = (pokemon) => {
-  const button = screen.getByRole('button', { name: /Próximo Pokémon/i });
+  const button = screen.getByRole('button', { name: PROXIMO_POKEMON });
   const pokemonName = screen.getByTestId(POKEMON_NAME);
   expect(pokemonName.textContent).toBe(pokemon.name);
   userEvent.click(button);
@@ -31,7 +32,7 @@ describe('Teste se é exibido o próximo Pokémon da lista quando o'
   + 'botão Próximo pokémon é clicado', () => {
   it('O botão deve conter o texto Próximo pokémon;', () => {
     renderWithRouter(<App />);
-    const button = screen.getByRole('button', { name: /Próximo Pokémon/i });
+    const button = screen.getByRole('button', { name: PROXIMO_POKEMON });
     expect(button).toBeInTheDocument();
   });
 
@@ -43,7 +44,7 @@ describe('Teste se é exibido o próximo Pokémon da lista quando o'
   it('O primeiro Pokémon da lista deve ser mostrado ao clicar no botão,'
     + ' se estiver no último Pokémon da lista;', () => {
     renderWithRouter(<App />);
-    const button = screen.getByRole('button', { name: /Próximo Pokémon/i });
+    const button = screen.getByRole('button', { name: PROXIMO_POKEMON });
     pokemons.forEach(() => {
       userEvent.click(button);
     });
@@ -55,7 +56,7 @@ describe('Teste se é exibido o próximo Pokémon da lista quando o'
 describe('Teste o componente <Pokedex.js /> - 3', () => {
   it('Teste se é mostrado apenas um Pokémon por vez.', () => {
     renderWithRouter(<App />);
-    const button = screen.getByRole('button', { name: /Próximo Pokémon/i });
+    const button = screen.getByRole('button', { name: PROXIMO_POKEMON });
     pokemons.forEach(() => {
       const pokemon = screen.getAllByTestId(POKEMON_NAME);
       expect(pokemon.length).toBe(1);
@@ -75,7 +76,7 @@ describe('Teste se a Pokédex tem os botões de filtro.', () => {
     );
     const firePokemons = mockPokemons.filter((pokemon) => pokemon.type === 'Fire');
     const fireButton = screen.getByRole('button', { name: 'Fire' });
-    const button = screen.getByRole('button', { name: /Próximo Pokémon/i });
+    const button = screen.getByRole('button', { name: PROXIMO_POKEMON });
     userEvent.click(fireButton);
     const pokemonName = screen.getByTestId(POKEMON_NAME).textContent;
     expect(pokemonName).toBe(firePokemons[0].name);
@@ -119,7 +120,7 @@ describe('Teste se a Pokédex contém um botão para resetar o filtro', () => {
       />,
     );
     const allBtn = screen.getByRole('button', { name: 'All' });
-    const nextBtn = screen.getByRole('button', { name: 'Próximo pokémon' });
+    const nextBtn = screen.getByRole('button', { name: PROXIMO_POKEMON });
     userEvent.click(allBtn);
     const firstPoke = screen.getByTestId(POKEMON_NAME);
     expect(firstPoke.textContent).toBe(mockPokemons[0].name);
@@ -138,7 +139,7 @@ describe('Teste se a Pokédex contém um botão para resetar o filtro', () => {
         isPokemonFavoriteById={ mockFavorite }
       />,
     );
-    const nextBtn = screen.getByRole('button', { name: 'Próximo pokémon' });
+    const nextBtn = screen.getByRole('button', { name: PROXIMO_POKEMON });
     const firstPoke = screen.getByTestId(POKEMON_NAME);
     expect(firstPoke.textContent).toBe(mockPokemons[0].name);
     userEvent.click(nextBtn);
@@ -152,26 +153,10 @@ describe('Teste se a Pokédex contém um botão para resetar o filtro', () => {
 
 describe('Teste se é criado, dinamicamente,'
   + ' um botão de filtro para cada tipo de Pokémon', () => {
-  it('Os botões de filtragem devem ser dinâmicos', () => {
-    renderWithRouter(
-      <Pokedex
-        pokemons={ mockPokemons }
-        isPokemonFavoriteById={ mockFavorite }
-      />,
-    );
-    const mockPokeBtn = mockPokemons.reduce((acc, curr) => {
-      if (acc.includes(curr.type)) return [...acc];
-      return [...acc, curr.type];
-    }, []);
-    const screenBtn = screen.getAllByRole('button');
-    const textScreenBtn = screenBtn.map((btn) => btn.textContent);
-    const textScreenBtnWithoutAll = textScreenBtn.slice(1, textScreenBtn.length - 1);
-    expect(mockPokeBtn.entries === textScreenBtnWithoutAll.entries).toBe(true);
-  });
-
-  it('Deve existir um botão de filtragem para cada tipo de Pokémon disponível'
-    + ' nos dados, sem repetição. Ou seja, a sua Pokédex deve possuir pokémons'
-    + ' do tipo Fire, Psychic, Electric e Normal', () => {
+  it('Os botões de filtragem devem ser dinâmicos.'
+  + ' Deve existir um botão de filtragem para cada tipo de Pokémon disponível'
+  + ' nos dados, sem repetição. Ou seja, a sua Pokédex deve possuir pokémons'
+  + ' do tipo Fire, Psychic, Electric e Normal', () => {
     renderWithRouter(
       <Pokedex
         pokemons={ mockPokemons }
@@ -210,7 +195,7 @@ describe('Teste se é criado, dinamicamente,'
 
 describe('Teste o componente <Pokedex.js /> - 7', () => {
   it('O botão de Próximo pokémon deve ser desabilitado quando'
-  + ' a lista filtrada de Pokémons tiver um só pokémon.', () =>{
+  + ' a lista filtrada de Pokémons tiver um só pokémon.', () => {
     renderWithRouter(
       <Pokedex
         pokemons={ mockPokemons }
@@ -219,7 +204,7 @@ describe('Teste o componente <Pokedex.js /> - 7', () => {
     );
     const electricButton = screen.getByRole('button', { name: 'Electric' });
     userEvent.click(electricButton);
-    const nextBtn = screen.getByRole('button', { name: 'Próximo pokémon' });
+    const nextBtn = screen.getByRole('button', { name: PROXIMO_POKEMON });
     expect(nextBtn).toBeDisabled();
   });
 });
