4bce84708b2bff894cb55b8a179b4ed0ba84a37b
josebejohann <josebernardojohann@gmail.com>
2021-02-13 12:58:01 -0300

    Implements About.js component tests

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+import { About } from '../components';
+
+describe('Tests <About /> component', () => {
+  it('should render a h2 tag with the text `About Pokédex`', () => {
+    const { getByText } = renderWithRouter(<About />);
+
+    expect(getByText(/About Pokédex/i)).toBeInTheDocument();
+  });
+
+  it('must contain two p tags describing the `Pokédex`', () => {
+    const { getAllByTestId } = renderWithRouter(<About />);
+
+    expect(getAllByTestId('about-pokedex').length).toBe(2);
+  });
+
+  it('must contain an specific image with the corresponding source', () => {
+    const { getByRole } = renderWithRouter(<About />);
+
+    expect(getByRole('img').src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
