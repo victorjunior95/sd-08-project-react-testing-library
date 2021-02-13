dc6894d6b3b372a2f13375243054fd4f77222a67
albertassi88 <albertassi88@gmail.com>
2021-02-13 09:45:24 -0300

    Criacao do teste FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Teste Requisito 3', () => {
+  test('Teste se é exibido na tela a frase No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText('Favorite Pokémons'));
+    expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido na tela a frase No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText('More details'));
+    const favorite = getByText('Pokémon favoritado?');
+    userEvent.click(favorite);
+    fireEvent.click(getByText('Favorite Pokémons'));
+    expect(getByText('Pikachu')).toBeInTheDocument();
+  });
+});
