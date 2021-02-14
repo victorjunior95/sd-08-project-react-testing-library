b2b7659cf66ceb597d030158466c129f443844d4
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-02-13 22:46:12 -0300

    req 06 e 07

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,84 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { Pokemon } from '../components';
+import App from '../App';
+import pokemons from '../data';
+
+const pokemonsRoll = pokemons;
+const { id, name, type,
+  averageWeight: { value, measurementUnit }, image } = pokemonsRoll[0];
+
+describe('testing the <Pokemon.js /> component', () => {
+  test('If a card with the information of a certain Pokémon is rendered.', () => {
+    renderWithRouter(
+      <Pokemon
+        pokemon={ pokemonsRoll[0] }
+        isFavorite={ App.setIsPokemonFavoriteById()[id] }
+      />,
+    );
+    expect(screen.getByTestId('pokemon-name')).toContainHTML((name));
+    expect(screen.getByTestId('pokemonType')).toContainHTML(type);
+    expect(screen.getByTestId('pokemon-weight')).toContainHTML(
+      `Average weight: ${value} ${measurementUnit}`,
+    );
+    expect(screen.getByAltText(`${name} sprite`).src).toBe(image);
+  });
+
+  test(`If the Pokémon card indicated on the Pokédex contains
+   a navigation link to view details of this Pokémon.
+   The link must have the URL / pokemons / <id>,
+   where <id> is the id of the Pokémon displayed`, () => {
+    renderWithRouter(
+      <Pokemon
+        pokemon={ pokemonsRoll[0] }
+        isFavorite={ App.setIsPokemonFavoriteById()[id] }
+      />,
+    );
+    expect(screen.getByText('More details')).toHaveAttribute(
+      'href',
+      `/pokemons/${id}`,
+    );
+  });
+
+  test(`If clicking on the Pokémon's navigation 
+  link redirects the application to the Pokémon details page.`, async () => {
+    const { history } = renderWithRouter(
+      <Pokemon
+        pokemon={ pokemonsRoll[0] }
+        isFavorite={ App.setIsPokemonFavoriteById()[id] }
+      />,
+    );
+    fireEvent.click(screen.getByRole('link', { href: `/pokemons/${id}` }));
+    const {
+      location: { pathname },
+    } = history;
+    expect(pathname).toBe(`/pokemons/${id}`);
+  });
+
+  test(`If the URL displayed in the browser
+  changes to / pokemon / <id>, where <id> is the id of
+  the Pokémon whose details you want to see.`, () => {
+    const { history } = renderWithRouter(
+      <Pokemon
+        pokemon={ pokemonsRoll[0] }
+        isFavorite={ App.setIsPokemonFavoriteById()[id] }
+      />,
+    );
+    fireEvent.click(screen.getByRole('link', { href: `/pokemons/${id}` }));
+    const {
+      location: { pathname },
+    } = history;
+    expect(pathname).toBe(`/pokemons/${id}`);
+  });
+
+  test('If there is a star icon on favorite Pokémon.', () => {
+    const favoritePokemons = true;
+    renderWithRouter(
+      <Pokemon pokemon={ pokemonsRoll[0] } isFavorite={ favoritePokemons } />,
+    );
+    expect(screen.getByAltText(`${name} is marked as favorite`).src).toContain(
+      '/star-icon.svg',
+    );
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,69 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
+import renderWithRouter from '../renderWithRouter';
+import { PokemonDetails } from '../components';
+import App from '../App';
+import pokemons from '../data';
+
+const pokemonsRoll = pokemons;
+const { id, name, summary, foundAt } = pokemonsRoll[0];
+
+describe('testing the <PokemonDetails.js /> component', () => {
+  test(`If detailed information about the 
+  selected Pokémon is shown on the screen.`, () => {
+    renderWithRouter(
+      <PokemonDetails
+        isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+        match={ { params: { id: pokemonsRoll[0].id } } }
+        onUpdateFavoritePokemons={ () => {} }
+        pokemons={ pokemons }
+      />,
+    );
+    expect(screen.getByText(`${name} Details`)).toBeInTheDocument();
+    expect(screen.queryByRole('link', { href: `/pokemons/${id}` })).not.toBe();
+    expect(
+      screen.getByRole('heading', { name: 'Summary' }),
+    ).toBeInTheDocument();
+    expect(screen.getByText(summary)).toBeInTheDocument();
+  });
+
+  test(`If there is a section on the page with 
+  maps containing the locations of the pokémon`, () => {
+    renderWithRouter(
+      <PokemonDetails
+        isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+        match={ { params: { id: pokemonsRoll[0].id } } }
+        onUpdateFavoritePokemons={ () => {} }
+        pokemons={ pokemons }
+      />,
+    );
+    expect(
+      screen.getByRole('heading', { name: `Game Locations of ${name}` }),
+    ).toBeInTheDocument();
+    expect(screen.queryAllByAltText(`${name} location`).length).toBe(
+      foundAt.length,
+    );
+    foundAt.forEach(({ location, map }, index) => {
+      expect(screen.getByText(location)).toBeInTheDocument();
+      expect(screen.queryAllByRole('img', { src: map })[index + 1].src).toBe(
+        map,
+      );
+    });
+  });
+
+  test('If the user can favor a pokémon through the details page.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(`/pokemons/${id}`);
+    expect(screen.getByText(/pokémon favoritado?/i)).toBeInTheDocument();
+    fireEvent.click(screen.getByText(/pokémon favoritado?/i));
+    expect(
+      screen.getByRole('img', { name: `${name} is marked as favorite` }),
+    ).toBeInTheDocument();
+    fireEvent.click(screen.getByText(/pokémon favoritado?/i));
+    expect(
+      screen.queryByRole('img', { name: `${name} is marked as favorite` }),
+    ).not.toBe();
+    expect(screen.getByLabelText(/Pokémon favoritado?/i)).toBeInTheDocument();
+  });
+});
