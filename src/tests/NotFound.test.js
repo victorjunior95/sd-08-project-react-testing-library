0310e8a59a76d062c473a228442060e6b97b19fc
josebejohann <josebernardojohann@gmail.com>
2021-02-14 10:21:38 -0300

    Implements tests for <NotFound /> component

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+import { NotFound } from '../components';
+
+describe('Tests <NotFound /> component', () => {
+  it('should contain a h2 tag with the text "Page requested not found"', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    expect(getByRole('heading', { name: /Page requested not found/, level: 2 }))
+      .toBeInTheDocument();
+  });
+
+  it('should render a specific Pikachu gif', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+
+    expect(getByAltText('Pikachu crying because the page requested was not found').src)
+      .toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
