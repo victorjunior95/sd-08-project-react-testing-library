786224caf773ff9c894a2c38893e95da7088ad47
albertassi88 <albertassi88@gmail.com>
2021-02-14 09:24:39 -0300

    Criando o teste Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,3 +1,4 @@
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
@@ -10,5 +11,74 @@ describe('Teste Requisito 5', () => {
       name: /Encountered pokémons/i,
       level: 2,
     }));
+  });
+
+  test('Teste se é exibido os Pokémons quando o botão Próximo pokémon é clicado', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const button = getByText('Próximo pokémon');
+    userEvent.click(button);
+    expect(getByText('Charmander')).toBeInTheDocument();
+    userEvent.click(button);
+    expect(getByText('Caterpie')).toBeInTheDocument();
+    userEvent.click(button);
+    expect(getByText('Ekans')).toBeInTheDocument();
+    userEvent.click(button);
+    expect(getByText('Alakazam')).toBeInTheDocument();
+    userEvent.click(button);
+    expect(getByText('Mew')).toBeInTheDocument();
+    userEvent.click(button);
+    expect(getByText('Rapidash')).toBeInTheDocument();
+    userEvent.click(button);
+    expect(getByText('Snorlax')).toBeInTheDocument();
+    userEvent.click(button);
+    expect(getByText('Dragonair')).toBeInTheDocument();
+    userEvent.click(button);
+    expect(getByText('Pikachu')).toBeInTheDocument();
+  });
+
+  test('Teste se é mostrado apenas um Pokémon por vez', () => {
+
+  });
+
+  test('Teste se a Pokédex tem os botões de filtro', () => {
+    const { getAllByText, getByRole  } = renderWithRouter(<App />);
+
+    userEvent.click(getByRole('button', { name: 'Electric' } ));
+    expect(getAllByText('Electric'));
+    userEvent.click(getByRole('button', { name: 'Fire' } ));
+    expect(getAllByText('Fire'));
+    userEvent.click(getByRole('button', { name: 'Bug' } ));
+    expect(getAllByText('Bug'));
+    userEvent.click(getByRole('button', { name: 'Poison' } ));
+    expect(getAllByText('Poison'));
+    userEvent.click(getByRole('button', { name: 'Psychic' } ));
+    expect(getAllByText('Psychic'));
+    userEvent.click(getByRole('button', { name: 'Normal' } ));
+    expect(getAllByText('Normal'));
+    userEvent.click(getByRole('button', { name: 'Dragon' } ));
+    expect(getAllByText('Dragon'));
+  });
+
+  test('Teste se a Pokédex contém um botão para resetar o filtro',() => {
+    const { getByText, getByRole, history } = renderWithRouter(<App />);
+
+    userEvent.click(getByRole('button', { name: 'All' } ));
+    expect(getByText('Pikachu')).toBeInTheDocument();
+
+    history.push('/');
+    expect(getByText('Pikachu')).toBeInTheDocument();
+  });
+
+  test('Teste se é criado um botão de filtro para cada tipo de Pokémon', () => {
+    const { getByText, getByRole, getAllByTestId } = renderWithRouter(< App/>);
+
+    userEvent.click(getByRole('button', { name: 'Fire' } ));
+    expect(getByText('Charmander')).toBeInTheDocument();
+    userEvent.click(getByText('Próximo pokémon'));
+    expect(getByText('Rapidash')).toBeInTheDocument();
+
+    const type = getAllByTestId('pokemon-type-button');
+    expect(type.length).toBe(7);
   });
 });
