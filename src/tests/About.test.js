d86fda4e75541666de4594c52ce469743d3588f3
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-02-15 13:13:13 -0300

    cria funçao renderWithRouter e testes iniciais do componente About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Teste do component About', () => {
+  test('verifica se o component renderiza no path correto', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const aboutHeader = getByRole('heading', {
+      level: 2,
+      name: /about pokédex/i,
+    });
+    expect(aboutHeader).toBeInTheDocument();
+  });
+  test('verifica se existe uma imagem na tela', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const img = getByRole('img');
+    expect(img).toBeInTheDocument();
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,39 +5,41 @@ import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Teste do compoenente App', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 
-test('testa conjunto de links de navegação', () => {
-  const { getAllByRole } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const FOUR = 4;
-  const links = getAllByRole('link');
-  expect(links.length).toBe(FOUR);
-  expect(links[0]).toHaveTextContent(/home/i);
-  expect(links[1]).toHaveTextContent(/about/i);
-  expect(links[2]).toHaveTextContent(/favorite pokémons/i);
-});
+  test('testa conjunto de links de navegação', () => {
+    const { getAllByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const FOUR = 4;
+    const links = getAllByRole('link');
+    expect(links.length).toBe(FOUR);
+    expect(links[0]).toHaveTextContent(/home/i);
+    expect(links[1]).toHaveTextContent(/about/i);
+    expect(links[2]).toHaveTextContent(/favorite pokémons/i);
+  });
 
-test('verifica se o link Home funciona', () => {
-  const history = createMemoryHistory();
-  const { getAllByRole } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const links = getAllByRole('link');
-  userEvent.click(links[0]);
-  const { pathname } = history.location;
-  expect(pathname).toBe('/');
+  test('verifica se o link Home funciona', () => {
+    const history = createMemoryHistory();
+    const { getAllByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const links = getAllByRole('link');
+    userEvent.click(links[0]);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
 });
