cc3ac6970cb98f4805cfcdfcfbbe59227536ac8e
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-02-15 13:27:37 -0300

    Req02 reformulado teste da imagem e acrescentado teste para verificar conteudo dos paragrafos do componente About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -11,9 +11,18 @@ describe('Teste do component About', () => {
     });
     expect(aboutHeader).toBeInTheDocument();
   });
-  test('verifica se existe uma imagem na tela', () => {
-    const { getByRole } = renderWithRouter(<About />);
-    const img = getByRole('img');
+  test('verifica se a imagem renderiza com a fonte correta', () => {
+    renderWithRouter(<About />);
+    const img = document
+      .querySelector('img[src="https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png"]');
     expect(img).toBeInTheDocument();
+  });
+
+  test('verifica se existem dois paragrafos na tela com textos sobre a pokedex', () => {
+    renderWithRouter(<About />);
+    const paragraphs = document.querySelectorAll('p');
+    expect(paragraphs.length).toBe(2);
+    expect(paragraphs[0]).toHaveTextContent(/This application simulates a Pokédex/i);
+    expect(paragraphs[1]).toHaveTextContent(/one can filter pokémons by type/i);
   });
 });
