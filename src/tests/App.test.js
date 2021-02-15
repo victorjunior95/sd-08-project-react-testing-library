7b84a05f023a892e9297bee2a9c03b6dd5edc7bd
Fabio <fabiohigoralm2@gmail.com>
2021-02-15 19:10:26 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,55 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = renderWithRouter(<App />);
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-
-  const linkHome = screen.getByRole('link', {
-    name: /Home/i,
+describe('Teste App', () => {
+  test('Renderiza texto `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
   });
-  expect(linkHome).toBeInTheDocument();
+
+  test('Verifica o link Home e seu pathname ', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', {
+      name: /Home/i,
+    });
+    expect(linkHome).toBeInTheDocument();
+    fireEvent.click(linkHome);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('Verifica o link About e seu pathname ', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', {
+      name: /About/i,
+    });
+    expect(linkAbout).toBeInTheDocument();
+    fireEvent.click(linkAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('Verifica o link Farovite e seu pathname ', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkFarovite = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    expect(linkFarovite).toBeInTheDocument();
+    fireEvent.click(linkFarovite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('Verifica o pagina nao encontrada e seu pathname ', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    history.push('/rotaerrada');
+    const textError = getByText(/Page requested not found/i);
+    expect(textError).toBeInTheDocument();
+  });
 });
