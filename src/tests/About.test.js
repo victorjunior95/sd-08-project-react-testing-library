316b18656f55e827b07fccd514e0753cd210933f
Fabio <fabiohigoralm2@gmail.com>
2021-02-15 18:29:47 -0300

    Primeiro commit

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Primeiro teste', () => {
+  renderWithRouter(<App />);
+  const textProjects = screen.getByRole('heading', {
+    level: 1,
+    name: /Pokédex/i,
+  });
+  expect(textProjects).toBeInTheDocument();
+});
