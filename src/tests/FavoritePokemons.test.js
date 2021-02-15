e5439aa7f072ca8de6139332c3dbdc9bd45c7c0a
Daniel Leon <daniel.radialista@gmail.com>
2021-02-15 11:05:12 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+import pokemons from '../data';
+
+describe('Testa o componente <FavoritePokemons />', () => {
+  test(' Verifica se é exibido na tela a mensagem No favorite pokemon found.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+  test('Verifica se é exibido todos os cards de pokémons favoritados.', () => {
+    const { getAllByTestId } = renderWithRouter(
+      <FavoritePokemons pokemons={ pokemons } />,
+    );
+    expect(getAllByTestId('pokemon-name').length).toBe(pokemons.length);
+  });
+
+  test('Se não houver pokémons favoritados, nenhum card é exibido ', () => {
+    const { queryAllByTestId } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    expect(queryAllByTestId('pokemon-name').length).toBe(0);
+  });
+});
