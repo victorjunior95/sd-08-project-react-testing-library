1e65ddfdee2f3d284ab22fd271d51e6bbf6a31c1
Leandro Lachini <leandrolachini@gmail.com>
2021-02-16 15:06:20 -0300

    task test component App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,68 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/PokédeX/i);
-  expect(heading).toBeInTheDocument();
+describe('Teste componente <App.js />', () => {
+  it('Página principal é renderizada ao carregar a aplicação no caminho de URL /', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Topo da aplicação contém um conjunto fixo de links de navegação`', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    expect(homeLink).toBeInTheDocument();
+
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    expect(aboutLink).toBeInTheDocument();
+
+    const favoritePokémonsLink = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(favoritePokémonsLink).toBeInTheDocument();
+  });
+});
+
+describe('Testes de redirecionamento da página após click', () => {
+  it('Redirecionada para página inicial, na URL / ao clicar no link Home', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    const { pathname } = history.location;
+    const linkHome = getByRole('link', { name: /home/i });
+    userEvent.click(linkHome);
+
+    expect(pathname).toBe('/');
+  });
+
+  it('Redirecionada para página About, na URL /about ao clicar no link About', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    const linkAbout = getByRole('link', { name: /about/i });
+    userEvent.click(linkAbout);
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/about');
+  });
+
+  it('Redirecionada para página Pokémons Favoritados, na URL /favorites', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    const linkFavoritePokemons = getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(linkFavoritePokemons);
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Redirecionada para a página Not Found ao entrar em uma URL desconhecida', () => {
+    const { queryByText, history } = renderWithRouter(<App />);
+
+    history.push('/notfound');
+    const urlNotFound = queryByText(/Page requested not found/i);
+
+    expect(urlNotFound).toBeInTheDocument();
+  });
 });
