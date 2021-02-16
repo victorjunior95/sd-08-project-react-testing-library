eec2bd8f4aa03e1f1a225c3a1ba63f523ea2b934
Lucas Neiva <lucassneiva.code@gmail.com>
2021-02-15 21:14:08 -0300

    adding app tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -11,4 +12,47 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+it('Deve renderizar o componente App', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const home = getByText(/Encountered pokémons/i);
+  expect(home).toBeInTheDocument();
+});
+
+describe('teste da app toda', () => {
+  it('testando se há links', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const home = getByText(/Home/i);
+    const about = getByText(/About/i);
+    const fav = getByText(/Favorite Pokémons/i);
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(fav).toBeInTheDocument();
+  });
+  // it('testando rotas', () => {
+  //   const { getByRole, getByText, history } = renderWithRouter(<App />);
+
+  //   const home = getByText(/Home/i);
+  //   const about = getByText(/About/i);
+  //   const fav = getByText(/Favorite Pokémons/i);
+
+  //   fireEvent.click(fav);
+  //   const pathname = history.location;
+  //   expect(pathname).toBe('/favorites');
+  //   expect(getByRole('heading', {
+  //     level: 2,
+  //     name: /Favorite Pokémons/i,
+  //   })).toBeInTheDocument();
+  // });
+  it('testando caminho inexistente e Not Found', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/pagina/que-nao-existe/');
+    const noMatch = getByText(/Page requested not found/);
+
+    expect(noMatch).toBeInTheDocument();
+  });
 });
