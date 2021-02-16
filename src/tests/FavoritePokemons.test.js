3533df33b17d67c934a687d4884c8eed333072cf
Lucas Neiva <lucassneiva.code@gmail.com>
2021-02-16 15:29:55 -0300

    req4 not found

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+import pokemons from '../data';
+
+describe('testes do FavoritePokemons', () => {
+  it('teste1: mensagem de No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+
+    expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+  it('teste2: mostra pokemons favoritados', async () => {
+    global.fetch = jest.fn(async () => ({
+      json: async () => pokemons[0].id,
+    }));
+
+    const { findByText } = renderWithRouter(<FavoritePokemons />);
+    await findByText(/Pikachu/i);
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente `<NotFound.js', () => {
+  it('heading `h2` com o texto `Page requested not found ', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    expect(
+      getByRole('heading', {
+        level: 2,
+        name: /Page requested not found/i,
+      }),
+    ).toBeInTheDocument();
+  });
+  it('se página mostra a imagem ', () => {
+    renderWithRouter(<NotFound />);
+
+    const displayedImage = document.querySelector('img');
+    expect(displayedImage.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
