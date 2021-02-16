37f92a039af181f364d770849d2cbdbc3db2bba1
Lucas Neiva <lucassneiva.code@gmail.com>
2021-02-16 09:35:31 -0300

    req1: adding app tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -32,21 +32,45 @@ describe('teste da app toda', () => {
     expect(about).toBeInTheDocument();
     expect(fav).toBeInTheDocument();
   });
-  // it('testando rotas', () => {
-  //   const { getByRole, getByText, history } = renderWithRouter(<App />);
+  it('testando rota /favorites', () => {
+    const { getByRole, getByText, history } = renderWithRouter(<App />);
 
-  //   const home = getByText(/Home/i);
-  //   const about = getByText(/About/i);
-  //   const fav = getByText(/Favorite Pokémons/i);
+    const fav = getByText(/Favorite Pokémons/i);
 
-  //   fireEvent.click(fav);
-  //   const pathname = history.location;
-  //   expect(pathname).toBe('/favorites');
-  //   expect(getByRole('heading', {
-  //     level: 2,
-  //     name: /Favorite Pokémons/i,
-  //   })).toBeInTheDocument();
-  // });
+    fireEvent.click(fav);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+    expect(getByRole('heading', {
+      level: 2,
+      name: /Favorite Pokémons/i,
+    })).toBeInTheDocument();
+  });
+  it('testando rota /about', () => {
+    const { getByRole, getByText, history } = renderWithRouter(<App />);
+
+    const about = getByText(/About/i);
+
+    fireEvent.click(about);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+    expect(getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    })).toBeInTheDocument();
+  });
+  it('testando rota /home', () => {
+    const { getByRole, getByText, history } = renderWithRouter(<App />);
+
+    const home = getByText(/Home/i);
+
+    fireEvent.click(home);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+    expect(getByRole('heading', {
+      level: 2,
+      name: /Encountered pokémons/i,
+    })).toBeInTheDocument();
+  });
   it('testando caminho inexistente e Not Found', () => {
     const { getByText, history } = renderWithRouter(<App />);
 
