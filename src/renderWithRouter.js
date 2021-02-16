6e6ee4c7f75fdc522d34939dfacc7925e9d92c99
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-02-16 11:09:07 -0300

    modifica funçao renderWithRouter para utilizar createBrowserHistory em vez de createMemoryHistory

--- src/renderWithRouter.js
+++ src/renderWithRouter.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import { createMemoryHistory} from 'history';
+import { createBroserHistory} from 'history';
 import { render } from '@testing-library/react';
 
 export default function renderWithRouter(component) {
-  const history = createMemoryHistory();
+  const history = createBrowserHistory();
   return ({
     ...render(<Router history={history}>{component}</Router>)
   });
