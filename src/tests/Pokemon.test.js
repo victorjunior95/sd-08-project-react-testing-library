4c4097d6c8b886de58d385a86400ab4fc86aee50
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-02-16 11:50:49 -0300

    realiza testes do componente Pokemon: verifica se o nome, tipo, peso medio e imagem renderizam corretamente na pagina inicial

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('teste do componente Pokemon', () => {
+  test('verifica se o nome do pokemon renderiza na pagina', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const pokeName = getByTestId('pokemon-name');
+    expect(pokeName).toHaveTextContent(/pikachu/i);
+  });
+  test('verifica se o tipo do pokemon aparece na pagina', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const pokeType = getByTestId('pokemonType');
+    expect(pokeType).toHaveTextContent(/electric/i);
+  });
+  test('verifica se o peso medio do pokemon renderiza corretamente', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const pokeWeight = getByTestId('pokemon-weight');
+    expect(pokeWeight).toHaveTextContent(/average weight: 6\.0 kg/i);
+  });
+  test('verifica se a imagem renderiza com fonte e texto alternativo corretos', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const pokeImage = getByRole('img', {
+      name: /pikachu sprite/i,
+      src: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    });
+    expect(pokeImage).toBeInTheDocument();
+  });
+});
