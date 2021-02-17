fb6d0bd035a09147920bb1b0b060772bccaee5f0
Ronald Jr <rpintojunior@gmail.com>
2021-02-17 17:58:24 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testando o Not found', () => {
+  test('Testa se página contém um heading', () => {
+    renderWithRouter(<NotFound />);
+    expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
+    expect(screen.getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
+  test('Testa se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
