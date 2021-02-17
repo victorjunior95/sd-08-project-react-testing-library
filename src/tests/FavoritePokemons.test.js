1782fe6aeeb8f0ee1269507694c5141250a71e02
Ronald Jr <rpintojunior@gmail.com>
2021-02-17 16:00:08 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import FavoritePokemon from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Testando o arquivo de favoritos', () => {
+  test('Se não houver favoritos exibir uma mensagem', () => {
+    renderWithRouter(<FavoritePokemon />);
+    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+
+  test('Se exibe todas as informações de um pokemon favoritado', () => {
+    const favoritePokemon = [pokemons[0], pokemons[1]];
+    renderWithRouter(<FavoritePokemon pokemons={ favoritePokemon } />);
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
+    expect(screen.getByText('Charmander')).toBeInTheDocument();
+    expect(screen.queryByText('Alakazam')).not.toBeInTheDocument();
+  });
+});
