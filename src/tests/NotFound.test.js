920a72573609656be9bae33da2fce487312b8bbf
afonso <afonsoafn@gmail.com>
2021-02-18 14:19:05 -0300

    requesition 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('h2 with text "Page requested not found"', () => {
+  const { getByText, getByRole } = renderWithRouter(<NotFound />);
+  expect(getByRole('heading', { level: 2 })).toBeInTheDocument();
+  expect(getByText('Page requested not found')).toBeInTheDocument();
+});
+
+test('Show the picture', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const image = getByRole('img', {
+    name: 'Pikachu crying because the page requested was not found',
+  });
+  const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  expect(image.src).toBe(src);
+});
