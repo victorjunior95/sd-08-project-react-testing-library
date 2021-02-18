bdecf37b593d1418c0a3f5de10674c62e521802f
Cleberton <clebertonfgc@gmail.com>
2021-02-18 16:57:25 -0300

    Requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testes componente FavoritePokemons.js', () => {
+  it('No favorite pokemon found, se a pessoa não tiver pokémons favoritos.', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFound = screen.getByText(/No favorite pokemon found/i);
+
+    expect(notFound).toBeInTheDocument();
+    expect(notFound).toHaveTextContent('No favorite pokemon found');
+  });
+});
