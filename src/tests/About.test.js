89f47a6891de3288e7fddd8e546eb6688eea3724
richardfazeredo <richardcompletetour@gmail.com>
2021-02-18 04:10:32 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,10 +4,9 @@ import About from '../components/About';
 
 describe('tests the component App.js', () => {
   it('contains info about Pokédex in the page', () => {
-    // const { getAllByRole } = renderWithRouter(<About />);
-    // const paragraphs = getAllByRole('paragraph', {
-
-    // });
+    const { getByText } = renderWithRouter(<About />);
+    const paragraph = getByText(/About Pokédex/i);
+    expect(paragraph).toBeInTheDocument();
   });
 
   it('contains a Heading with title About Pokédex in the page', () => {
@@ -17,5 +16,19 @@ describe('tests the component App.js', () => {
       name: 'About Pokédex',
     });
     expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  it('contains two paragraphs with text about Pokédex', () => {
+    const { getAllByText } = renderWithRouter(<About />);
+    const text = getAllByText(/Pokémons/i);
+    console.log(text);
+    expect(text.length.toString()).toBe('2');
+  });
+
+  it('contains a specific image of a Pokédex', () => {
+    const { getByAltText } = renderWithRouter(<About />);
+    const img = getByAltText(/Pokédex/i);
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
