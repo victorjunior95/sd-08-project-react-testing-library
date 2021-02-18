dc614fedb27ec1a22b73b78b35c5f5ab79e8c2ee
Lucas Neiva <lucassneiva.code@gmail.com>
2021-02-18 11:03:40 -0300

    req 3 favoritePokes

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -10,12 +10,34 @@ describe('testes do FavoritePokemons', () => {
 
     expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
   });
-  it('teste2: mostra pokemons favoritados', async () => {
-    global.fetch = jest.fn(async () => ({
-      json: async () => pokemons[0].id,
-    }));
+  it('All favorite Pokémon cards should be displayed.', () => {
+    const { getAllByTestId, getByText } = renderWithRouter(
+      <FavoritePokemons pokemons={ pokemons } />,
+    );
+    const NINENINE = 9;
 
-    const { findByText } = renderWithRouter(<FavoritePokemons />);
-    await findByText(/Pikachu/i);
+    const pokemonsFavorites = getAllByTestId('pokemon-name');
+
+    expect(pokemonsFavorites.length).toBe(NINENINE);
+
+    const pikachu = getByText('Pikachu');
+    const charmander = getByText('Charmander');
+    const caterpie = getByText('Caterpie');
+    const ekans = getByText('Ekans');
+    const alakazam = getByText('Alakazam');
+    const mew = getByText('Mew');
+    const rapidash = getByText('Rapidash');
+    const snorlax = getByText('Snorlax');
+    const dragonair = getByText('Dragonair');
+
+    expect(pikachu).toBeInTheDocument();
+    expect(charmander).toBeInTheDocument();
+    expect(caterpie).toBeInTheDocument();
+    expect(ekans).toBeInTheDocument();
+    expect(alakazam).toBeInTheDocument();
+    expect(mew).toBeInTheDocument();
+    expect(rapidash).toBeInTheDocument();
+    expect(snorlax).toBeInTheDocument();
+    expect(dragonair).toBeInTheDocument();
   });
 });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,68 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente `<Pokedex.js />', () => {
+  test('um heading `h2` com o texto `Encountered pokémons`.', () => {
+    const { getByRole } = renderWithRouter(<App />);
+
+    expect(getByRole('heading', {
+      level: 2,
+      name: /Encountered pokémons/i,
+    })).toBeInTheDocument();
+  });
+  it('próximo Pokémon da lista quando o botão `Próximo pokémon` é clicado', () => {
+    const { getByRole, getByTestId, getByText } = renderWithRouter(<App />);
+
+    const button = getByText(/Próximo Pokémon/i);
+
+    fireEvent.click(button);
+    expect(getByRole('button', {
+      name: /Próximo pokémon/,
+    })).toBeInTheDocument();
+
+    const paragraph = getByTestId('pokemon-name');
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph).toHaveTextContent(/Charmander/i);
+  });
+  it('botão com o texto `Próximo pokémon`', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+
+    const button = getByText(/Próximo Pokémon/i);
+
+    fireEvent.click(button);
+    expect(getByRole('button', {
+      name: /Próximo pokémon/,
+    })).toBeInTheDocument();
+  });
+  it('próximo Pokémon da lista quando o botão `Próximo pokémon` é clicado', () => {
+    const { getByRole, getByTestId, getByText } = renderWithRouter(<App />);
+
+    const button = getByText(/Próximo Pokémon/i);
+
+    fireEvent.click(button);
+    expect(getByRole('button', {
+      name: /Próximo pokémon/,
+    })).toBeInTheDocument();
+
+    const paragraph = getByTestId('pokemon-name');
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph).toHaveTextContent(/Charmander/i);
+
+    fireEvent.click(button);
+    const paragraph2 = getByTestId('pokemon-name');
+    expect(paragraph2).toBeInTheDocument();
+    expect(paragraph2).toHaveTextContent(/Caterpie/i);
+
+    fireEvent.click(button);
+    const paragraph3 = getByTestId('pokemon-name');
+    expect(paragraph3).toBeInTheDocument();
+    expect(paragraph3).toHaveTextContent(/Ekans/i);
+
+    fireEvent.click(button);
+    const paragraph4 = getByTestId('pokemon-name');
+    expect(paragraph4).toBeInTheDocument();
+    expect(paragraph4).toHaveTextContent(/Alakazam/i);
+  });
+});
