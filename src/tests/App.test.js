cd11680432acecbb74305d6c985555ac489616d0
Cleberton <clebertonfgc@gmail.com>
2021-02-18 15:17:49 -0300

    Requisito 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Testes Componente APP.js', () => {
+  it('Teste se a pagina principal e renderizada.', () => {
+    const { history } = renderWithRouter(<App />);
+    const h1 = screen.getByRole('heading', { level: 1, name: 'Pokédex' });
+    const { pathname } = history.location;
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+    expect(h1).toBeInTheDocument();
+    expect(pathname).toBe('/');
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  it('Teste Link Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    expect(linkHome).toHaveTextContent('Home');
+
+    userEvent.click(linkHome);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Teste link About', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toHaveTextContent('About');
+
+    userEvent.click(linkAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Teste Link Favorite Pokémons', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkFavorite).toHaveTextContent('Favorite Pokémons');
+
+    userEvent.click(linkFavorite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Teste pagina NOT FOUND', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/urlqualquer');
+    const noMatch = screen.getByText(/Page requested not found/i);
+    expect(noMatch).toHaveTextContent('Page requested not found');
+  });
 });
