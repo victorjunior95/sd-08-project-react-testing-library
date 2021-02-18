d5742d8b089d1c6b78589c328faf273f8cef1354
liisaambrosen <liisa.ambrosen@gmail.com>
2021-02-18 18:32:25 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('checks if Not Found page loads everything correctly', () => {
+  test('tests if page has an h2 heading with the text Page requested not found', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const heading = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('tests if the page contains the correct image', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
