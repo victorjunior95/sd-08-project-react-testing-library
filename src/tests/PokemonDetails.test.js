aa94ad4d181ada076d759d88a872963a853fcb54
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-02-18 18:28:14 -0300

    adiciona teste que verifica se os mapas de localizaçao aparecem com os atributos corretos no componente PokemonDetails

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -46,4 +46,15 @@ describe('teste do componente PokemonDetails', () => {
     });
     expect(locationsHeader).toBeInTheDocument();
   });
+  test('verifica se o primeiro mapa aparece com atributos corretos', () => {
+    const { getByRole, getAllByRole } = renderWithRouter(<App />);
+    const detailsLink = getByRole('link', { name: /more details/i });
+    userEvent.click(detailsLink);
+    const locationMaps = getAllByRole('img', { name: /pikachu location/i });
+    expect(locationMaps.length).toBe(2);
+    expect(locationMaps[0]).toHaveAttribute('alt', 'Pikachu location');
+    expect(locationMaps[0]).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(locationMaps[1]).toHaveAttribute('alt', 'Pikachu location');
+    expect(locationMaps[1]).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+  });
 });
