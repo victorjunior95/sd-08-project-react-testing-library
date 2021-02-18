121faa6fd6260e9212d3c1d04e3c937b4ea5b475
afonso <afonsoafn@gmail.com>
2021-02-18 14:29:50 -0300

    requesition 3, 5

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemon from '../data';
+
+test('No favorite pokemon found', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+});
+
+test('All Pokemon cards', () => {
+  const { getAllByTestId, getByText } = renderWithRouter(
+    <FavoritePokemons pokemons={ pokemons } />,
+  );
+
+  const pokemonsFavorites = getAllByTestId('pokemon-name');
+
+  expect(pokemonsFavorites.length).toBe(pokemons.length);
+
+  const pikachu = getByText('Pikachu');
+  const charmander = getByText('Charmander');
+  const caterpie = getByText('Caterpie');
+  const ekans = getByText('Ekans');
+  const alakazam = getByText('Alakazam');
+  const mew = getByText('Mew');
+  const rapidash = getByText('Rapidash');
+  const snorlax = getByText('Snorlax');
+  const dragonair = getByText('Dragonair');
+
+  expect(pikachu).toBeInTheDocument();
+  expect(charmander).toBeInTheDocument();
+  expect(caterpie).toBeInTheDocument();
+  expect(ekans).toBeInTheDocument();
+  expect(alakazam).toBeInTheDocument();
+  expect(mew).toBeInTheDocument();
+  expect(rapidash).toBeInTheDocument();
+  expect(snorlax).toBeInTheDocument();
+  expect(dragonair).toBeInTheDocument();
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,46 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
+
+test('Info card Pokemon', () => {
+  const Pikachu = pokemons[0];
+  const { queryByTestId, getByAltText } = renderWithRouter(<Pokemon
+    pokemon={ Pikachu }
+  />);
+  const pokemonName = queryByTestId('pokemon-name');
+  expect(pokemonName).toHaveTextContent('Pikachu');
+  const imgAltPokemon = getByAltText('Pikachu sprite');
+  expect(imgAltPokemon.src).toBe('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  const pokemonType = queryByTestId('pokemonType');
+  expect(pokemonType).toHaveTextContent('Electric');
+  const pokemonWeight = queryByTestId('pokemon-weight');
+  expect(pokemonWeight).toHaveTextContent('Average weight: 6.0 kg');
+});
+
+test('Card navigation link', () => {
+  const pikachu = pokemons[0];
+  const { getByText } = renderWithRouter(<Pokemon pokemon={ pikachu } />);
+  const btn = getByText('More details');
+  expect(btn.href).toContain('/pokemons/25');
+});
+
+test('Click on navigation link', () => {
+  const firstPokemon = pokemons[0];
+  const { getByText, history } = renderWithRouter(<Pokemon pokemon={ firstPokemon } />);
+  const btnDetails = getByText('More details');
+  expect(btnDetails).toBeInTheDocument();
+  fireEvent.click(btnDetails);
+  expect(history.location.pathname).toBe('/pokemons/25');
+});
+
+test('Star icon on favorite Pokemon', () => {
+  const favoritePikachu = pokemons[0];
+  const { getByAltText } = renderWithRouter(<Pokemon
+    pokemon={ favoritePikachu }
+    isFavorite
+  />);
+  const favoritedIcon = getByAltText('Pikachu is marked as favorite');
+  expect(favoritedIcon.src).toContain('/star-icon.svg');
+});
