815e95d084a2bd81a3db87087d5e1130dae89aae
wagnerberna <cabelo29007@gmail.com>
2021-02-19 08:06:23 -0300

    req03

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { MemoryRouter } from 'react-router-dom';
 import { screen } from '@testing-library/react';
 import About from '../components/About';
 import renderWithRouter from './renderWithRouter';
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Req03 Teste Pokemons Favoritos', () => {
+  test('Teste sem Pokemon fav, se exibe a mensagem "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const message = screen.getByText(/No favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+});
