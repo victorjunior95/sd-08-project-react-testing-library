0cbdf6ab7e0d25c95b540ae858ff172b9d11435c
liisaambrosen <liisa.ambrosen@gmail.com>
2021-02-19 18:19:13 -0300

    refatorando 2

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from '../renderWithRouter';
+import App from '../App';
 
 describe('checks if the favorite pokemons page loads correctly', () => {
   test('No Favorite Pokemon found message if the user didnt add any favorites', () => {
     const { getByText } = renderWithRouter(<FavoritePokemons />);
     const noFavorites = getByText(/No favorite pokemon found/i);
     expect(noFavorites).toBeInTheDocument();
+  });
+
+  test('Adding pokemons to favorites', () => {
+    const { getByText, getByLabelText } = renderWithRouter(<App />);
+    const buttonDetails = getByText(/More details/i);
+    const buttonFavorites = getByText(/Favorite Pokémons/i);
+
+    fireEvent.click(buttonDetails);
+    const markFavorite = getByLabelText('Pokémon favoritado?');
+    fireEvent.click(markFavorite);
+    fireEvent.click(buttonFavorites);
+    const pokemonCard = document.querySelector('.pokemon-overview');
+    expect(pokemonCard).toBeInTheDocument();
   });
 });
