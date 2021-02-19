a88d3b3fe140b968ec87f9aa5ba8494d6f7fdba0
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-02-19 18:54:56 -0300

    adiciona testes ao arquivo de testes da pokedex, testa o h2 e o botao de proximo pokemon

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('teste do componente Pokedex', () => {
+  test('verifica se existe um h2 com o texto "Encountered Pokemons"', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const encounteredHeading = getByRole('heading', {
+      level: 2,
+      name: /encountered pokémons/i,
+    });
+    expect(encounteredHeading).toBeInTheDocument();
+  });
+  test('verifica se o botao de proximo pokemon funciona corretamente', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+    const nextPokemonButton = getByRole('button', { name: /próximo pokémon/i });
+    expect(nextPokemonButton).toBeInTheDocument();
+    userEvent.click(nextPokemonButton);
+    const charmander = getByText(/charmander/i);
+    expect(charmander).toBeInTheDocument();
+  });
+});
