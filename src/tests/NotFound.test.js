1a221d58576fa8341f7dcc9bbc68be2423008f71
wagnerberna <cabelo29007@gmail.com>
2021-02-19 08:24:24 -0300

    req04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Req04 NotFound.js ', () => {
+  test('Teste se têm um heading h2 com o texto "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const headingH2 = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(headingH2).toBeInTheDocument();
+  });
+  test('Teste se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const imgLink = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(img.src).toBe(imgLink);
+  });
+});
