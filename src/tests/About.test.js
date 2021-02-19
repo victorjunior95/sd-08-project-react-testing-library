ccb0ac9777f7797ff65133b31808e34284cc5a42
Douglas Ferreira <dodofp.df@gmail.com>
2021-02-19 19:50:10 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes About.js', () => {
+  test('Testando se a página contem informações', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const aboutPokedex = getByRole('heading', { level: 2, name: /about pokédex/i });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  test('Testando o cumprimento dos parágrafos', () => {
+    const { getAllByTestId } = renderWithRouter(<About />);
+    const paragraphs = getAllByTestId('paragraphs');
+    expect(paragraphs.length).toBe(2);
+  });
+
+  test('Testando a imagem', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const img = getByRole('img');
+    expect(img.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
