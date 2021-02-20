dc9be9374ca898f3b023959bf495908843673341
patriciavsilva <silva.patricia.v@gmail.com>
2021-02-20 20:14:23 -0300

    requisito4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('teste o componente NotFound', () => {
+  test('se a página contém um heading h2', () => {
+    render(<NotFound />);
+    const headingPage = screen.getByRole('heading', {
+      level: 2,
+      name: 'Page requested not found Crying emoji',
+    });
+
+    expect(headingPage).toBeInTheDocument();
+  });
+
+  test('se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif ', () => {
+    render(<NotFound />);
+    const image = screen.getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
