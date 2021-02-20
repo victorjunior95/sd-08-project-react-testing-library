dc78d5918e2478b95b9f7c41586beadbfffd132a
patriciavsilva <silva.patricia.v@gmail.com>
2021-02-20 20:56:03 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Testa o componente <FavoritePokemons />', () => {
+  test(' Verifica se é exibido na tela a mensagem No favorite pokemon found.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+  test('Verifica se é exibido todos os cards de pokémons favoritados.', () => {
+    const { getAllByTestId } = renderWithRouter(
+      <FavoritePokemons pokemons={ pokemons } />,
+    );
+    expect(getAllByTestId('pokemon-name').length).toBe(pokemons.length);
+  });
+  test('Se não houver pokémons favoritados, nenhum card é exibido ', () => {
+    const { queryAllByTestId } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    expect(queryAllByTestId('pokemon-name').length).toBe(0);
+  });
+});
