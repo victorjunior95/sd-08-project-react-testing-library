eb43e8c157e9ca815b826567337498e50a53e682
Giovanna Betti <giovannabetti@gmail.com>
2021-02-20 06:15:03 -0300

    The day is dawning, the sky is already clear and no mutants have appeared alive so far...

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('tests the NotFound component', () => {
+  it('contains <h2>Page requested not found 😭</h2>', () => {
+    render(<NotFound />);
+
+    expect(screen.getByRole('heading', { level: 2 }))
+      .toHaveTextContent(/Page requested not found 😭/i);
+  });
+
+  it('contains a specific image', () => {
+    const { getByAltText } = render(<NotFound />);
+    const altImage = getByAltText(
+      'Pikachu crying because the page requested was not found'
+    );
+    expect(altImage).toBeInTheDocument();
+
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(altImage.src).toBe(url);
+  });
+});
