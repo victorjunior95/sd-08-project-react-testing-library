2d1ac7b17439b8c67aaf2c87db21ec419a534368
Giovanna Betti <giovannabetti@gmail.com>
2021-02-20 04:03:00 -0300

    no one survived, even Lint issues

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import About from '../components/About';
 
-describe('tests if the page About contains', () => {
+describe('test if the component About contains', () => {
   it('Pokédex information', () => {
     const { getByText } = render(<About />);
     const aboutAll = getByText(/This application simulates a Pokédex/i);
@@ -16,18 +16,20 @@ describe('tests if the page About contains', () => {
     expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(/About Pokédex/i);
   });
 
-  // TODO: I don't know how to test this
-  // it('two paragraphs with text about Pokédex', () => {
-  //   render(<About />);
+  // TODO: I would like something that test have two paragraphs and the content within.
+  it('two paragraphs with text about Pokédex', () => {
+    const { getByText } = render(<About />);
 
-  //   expect(screen.getAllByLabelText())
-  // });
+    expect(getByText(/This application simulates a Pokédex/i)).toBeInTheDocument();
+    expect(getByText(/One can filter Pokémons by type/i)).toBeInTheDocument();
+  });
 
   it('an specific image of a Pokédex', () => {
-    render(<About />);
+    const { getByAltText } = render(<About />);
+    const altImage = getByAltText('Pokédex');
+    expect(altImage).toBeInTheDocument();
 
-    expect(screen.querySelector('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.image.src')).toBeTruthy();
-
-    // expect(images.getElement(0).props.src).toEqual('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.image.src');
+    const url = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(altImage.src).toBe(url);
   });
 });
