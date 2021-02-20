4082ed0810e8c0151c3652823ae04cb30029c9a9
Douglas Ferreira <dodofp.df@gmail.com>
2021-02-19 21:37:53 -0300

    Requisito 4, 3 ainda nao feito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('FavoritePokemons.js', () => {
+  test('Testa se é exibido a mensagem no favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const noPokemonMsg = getByText(/No favorite pokemon found/i);
+    expect(noPokemonMsg).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('NotFound.js', () => {
+  test('Testa se a página contém um heading', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const pageTxt = getByRole('heading', { level: 2, name: /Page requested not found/i });
+    expect(pageTxt).toBeInTheDocument();
+  });
+
+  test('Testando src da imagem', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const img = getByRole('img',
+      { name: 'Pikachu crying because the page requested was not found' });
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
