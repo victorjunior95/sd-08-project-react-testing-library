6d4ecc5e3b94c6affaeedae1a2a1080623eac08b
Caio Menezes Rocha <caiomzrocha@gmail.com>
2021-02-20 04:16:11 -0300

    [ADD] testes para o componente App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -11,4 +12,19 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+describe('contains a fixed set of navigation links', () => {
+  it('contains home link', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText(/home/i)).toBeInTheDocument();
+  });
+  it('contains about link', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText(/about/i)).toBeInTheDocument();
+  });
+  it('contains favorite pokémons link', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText(/favorite pokémons/i)).toBeInTheDocument();
+  });
 });
