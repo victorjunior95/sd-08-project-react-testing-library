a4b7af5720c349037f49ff759f4778876189b0e2
wagnerberna <cabelo29007@gmail.com>
2021-02-20 07:07:15 -0300

    req07

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,47 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Req07 Teste Pokemon informações detalhadas.', () => {
+  test('A página tem um texto <name> Details, onde <name> é o nome do Pokémon', () => {
+    renderWithRouter(<App />);
+    userEvent.click(screen.getByText(/more detail/i));
+    const details = screen.getByText(/pikachu details/i);
+    expect(details).toBeInTheDocument();
+  });
+  test('Não existir o link de para os detalhes do Pokémon selecionado', () => {
+    renderWithRouter(<App />);
+    userEvent.click(screen.getByText(/more detail/i));
+    const moreDetails = screen.queryByText(/more detail/i);
+    expect(moreDetails).toBeNull();
+  });
+  test('A seção de detalhes deve conter um heading h2 com o texto Summary ', () => {
+    renderWithRouter(<App />);
+    userEvent.click(screen.getByText(/more detail/i));
+    const headingH2 = screen.getByRole('heading', {
+      name: /Summary/i,
+      level: 2,
+    });
+    expect(headingH2).toBeInTheDocument();
+  });
+  test('parágrafo com o resumo do Pokémon específico sendo visualizado', () => {
+    renderWithRouter(<App />);
+    userEvent.click(screen.getByText(/more detail/i));
+    const resume = screen.getByText(/This intelligent Pokémon/i);
+    expect(resume).toBeInTheDocument();
+  });
+});
+
+describe('Req07 Teste se existe os mapas contendo as localizações do pokémon', () => {
+  test('Teste heading h2 texto Game Locations of <name> que é oPokémon exibido.', () => {
+    renderWithRouter(<App />);
+    userEvent.click(screen.getByText(/more detail/i));
+    const headingH2 = screen.getByRole('heading', {
+      name: /Game Locations of Pikachu/i,
+      level: 2,
+    });
+    expect(headingH2).toBeInTheDocument();
+  });
+});
