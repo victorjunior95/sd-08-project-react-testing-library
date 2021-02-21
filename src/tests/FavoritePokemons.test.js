ce3f235332e612e36b90a1554ddb7ba92b6f53a9
Aline Debastiani <debastianialine@gmail.com>
2021-02-21 14:05:17 -0300

    cumpre requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
+
+test('Test if shows a message when the person has no favorite pokemon', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+  const message = getByText('No favorite pokemon found');
+  expect(message).toBeInTheDocument();
+});
+
+test('test if shows all favorites pokemons cards', () => {
+  const { getAllByTestId } = renderWithRouter(
+    <FavoritePokemons pokemons={ pokemons } />,
+  );
+  expect(getAllByTestId('pokemon-name').length).toBe(pokemons.length);
+});
+
+test('test if there is no card when does not have a favorite pokemon', () => {
+  const { queryAllByTestId } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+  expect(queryAllByTestId('poke-name').length).toBe(0);
+});
