2dc29599ffa843330ad4c109b9c1756000a1bb2a
Aline Debastiani <debastianialine@gmail.com>
2021-02-21 14:55:16 -0300

    cumpre requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../components';
+
+test('test if the page has a h2 with a text', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const text = getByText('Page requested not found');
+  expect(text).toBeInTheDocument();
+});
+
+test('test if the page load an image', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const image = getByRole('img', {
+    name: 'Pikachu crying because the page requested was not found',
+  });
+  expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
