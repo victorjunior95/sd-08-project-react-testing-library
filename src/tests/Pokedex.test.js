53ba4c827fa1bfd26c00b66b86f1ca149cb09874
Giovanna Betti <giovannabetti@gmail.com>
2021-02-20 23:46:49 -0300

    I dont know about the stryker mutants, but I am sure that now I am a mutant - zero mutants survived at Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
@@ -20,7 +20,7 @@ test('The next Pokémon should be shown when "Próximo Pokémon" is clicked', ()
   const btnNext = getByRole('button', {
     name: /próximo pokémon/i,
   });
-  fireEvent.click(btnNext);
+  userEvent.click(btnNext);
 
   const secondPokemon = getByText(/charmander/i);
   expect(secondPokemon).toBeInTheDocument();
@@ -32,7 +32,36 @@ test('', () => {
   expect(firstPokemon.length).toBe(1);
 });
 
-// mostrar primeiro pokemon se estiver no último da lista
-// botões de filtro
-// botão reset
-// botões dinâmicos
+test('Must show only one Pokémon at a time', () => {
+  const { getAllByText } = renderWithRouter(<App />);
+
+  const pokemon = getAllByText('Pikachu');
+  expect(pokemon.length).toBe(1);
+});
+
+test('Pokédex should have all filter buttons', () => {
+  const { queryAllByTestId } = renderWithRouter(<App />);
+  const typeButtons = queryAllByTestId('pokemon-type-button');
+  const types = [
+    'Electric',
+    'Fire',
+    'Bug',
+    'Poison',
+    'Psychic',
+    'Normal',
+    'Dragon',
+  ];
+  expect(typeButtons.length).toBe(types.length);
+  expect(typeButtons[0].textContent).toBe('Electric');
+});
+
+test('Should have a reset button', () => {
+  const { getByRole, getByText } = renderWithRouter(<App />);
+  const resetButton = getByRole('button', { name: /all/i });
+  expect(resetButton).toBeInTheDocument();
+  expect(resetButton).toHaveTextContent('All');
+
+  userEvent.click(resetButton);
+
+  expect(getByText(/pikachu/i)).toBeInTheDocument();
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,11 +1,84 @@
+import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { screen, render } from '@testing-library/react';
-import PokemonDetails from '../components/PokemonDetails';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-describe('tests the PokemonDetails component', () => {
-  it('', () => {});
+describe('Tests the PokemonDetails component', () => {
+  it('Should have <Pokemon Name> Details', () => {
+    const { getByText, getByRole, history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const linkDetails = getByText(/More details/i);
+    userEvent.click(linkDetails);
 
-  it('', () => {});
+    const pokemonDetails = getByRole('heading', {
+      level: 2,
+      name: /Pikachu details/i,
+    });
+    expect(pokemonDetails).toBeInTheDocument();
+  });
 
-  it('', () => {});
+  it('Should not exist the navigation link for details of the selected Pokémon', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const linkDetails = getByText(/More details/i);
+    userEvent.click(linkDetails);
+
+    expect(linkDetails).not.toBeInTheDocument();
+  });
+
+  it('Should contains a heading <h2>Summary</h2>', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const linkDetails = getByText(/More details/i);
+    userEvent.click(linkDetails);
+
+    expect(getByText(/summary/i)).toBeInTheDocument();
+  });
+
+  it('Must contain a <p>summary of the selected Pokémon</p>', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const linkDetails = getByText(/More details/i);
+    userEvent.click(linkDetails);
+
+    expect(getByText(/This intelligent Pokémon roasts hard/i)).toBeInTheDocument();
+  });
+});
+
+describe('There must be a section containing location Pokémon map', () => {
+  it('Should contains a heading <h2>Game Locations of Selected Pokémon</h2>', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const linkDetails = getByText(/More details/i);
+    userEvent.click(linkDetails);
+
+    expect(getByText(/Game locations of Pikachu/i)).toBeInTheDocument();
+  });
+
+  it('Must be shown all Pokémon locations', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const linkDetails = getByText(/More details/i);
+    userEvent.click(linkDetails);
+
+    expect(getByText(/Pikachu location/i)).toBeInTheDocument();
+  });
+
+  it('Must be shown each name location and its image map', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const linkDetails = getByText(/More details/i);
+    userEvent.click(linkDetails);
+    const locationMap = getByText(/Pikachu location/i);
+
+    expect(locationMap.src).toBe('https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+  });
+});
+
+test('The user can favorite a Pokémon through the details page', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/pokemons/25');
+  const linkDetails = getByText(/More details/i);
+  userEvent.click(linkDetails);
+
+  expect(getByText(/Pokémon favoritado/i)).toBeInTheDocument();
 });
