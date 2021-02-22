db45908b081bb5b91a96b67b24bbb91721f315c8
Caio Menezes Rocha <caiomzrocha@gmail.com>
2021-02-21 22:12:31 -0300

    [ADD] testes para o componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Favorite pokemons card', () => {
+  it('', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    expect(getByText(/no favorite pokemon found/i)).toBeInTheDocument();
+  });
+});
