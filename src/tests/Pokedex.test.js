3e1ccb84d22d2c2ace9515906da3e6c38691e3d7
antonio arieiro <sfgfffs@gmail.com>
2021-02-22 12:14:17 -0300

    test pokemon

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,103 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { cleanup } from '@testing-library/react';
+import { Pokedex } from '../components';
+import renderWithRouter from '../components/route/renderWithRouter'
+import data from '../data';
+
+const renderPokedex = ({ pokemons = data, isPokemonFavoriteById = {} } = {}) => (
+  renderWithRouter(<Pokedex { ...{ pokemons, isPokemonFavoriteById } } />)
+);
+
+const nextPokemon = 'Próximo pokémon';
+const testIds = {
+  name: 'pokemon-name',
+  type: 'pokemonType',
+  buttonType: 'pokemon-type-button',
+};
+const pokemonTypes = new Set();
+data.forEach((pokemon) => pokemonTypes.add(pokemon.type));
+describe('requirement05', () => {
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    const { getByRole } = renderPokedex();
+    expect(getByRole('heading', { level: 2, name: 'Encountered pokémons' }))
+      .toBeInTheDocument();
+  });
+
+  test(`Teste se é exibido o próximo Pokémon da lista quando o botão 
+  Próximo pokémon é clicado`, () => {
+    const { getByTestId, getByRole } = renderPokedex();
+    const button = getByRole('button', { name: nextPokemon });
+    const firstPokemon = getByTestId(testIds.name).textContent;
+    expect(button).toBeInTheDocument();
+    data.forEach((pokemon) => {
+      expect(getByTestId(testIds.name).textContent).toBe(pokemon.name);
+      userEvent.click(button);
+    });
+    const currentPokemon = getByTestId(testIds.name).textContent;
+    expect(currentPokemon).toBe(firstPokemon);
+  });
+
+  test('Teste se é mostrado apenas um Pokémon por vez', () => {
+    const { getAllByTestId, getByRole } = renderPokedex();
+    data.forEach(() => {
+      expect(getAllByTestId(testIds.name).length).toBe(1);
+      userEvent.click(getByRole('button', { name: nextPokemon }));
+    });
+  });
+
+  test('Teste se a Pokédex tem os botões de filtro', () => {
+    const { getByTestId, getByRole } = renderPokedex();
+    pokemonTypes.forEach((type) => {
+      const button = getByRole('button', { name: type });
+      expect(button.textContent).toBe(type);
+      userEvent.click(button);
+      data.filter((pokemon) => pokemon.type === type).forEach(() => {
+        expect(getByTestId(testIds.type).textContent).toBe(type);
+        userEvent.click(getByRole('button', { name: nextPokemon }));
+      });
+    });
+  });
+
+  test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    const { getByRole, getByTestId } = renderPokedex();
+    const checkAllPokemons = () => {
+      data.forEach((pokemon) => {
+        expect(getByTestId(testIds.name).textContent).toBe(pokemon.name);
+        userEvent.click(getByRole('button', { name: nextPokemon }));
+      });
+    };
+    checkAllPokemons();
+    userEvent.click(getByRole('button', { name: 'All' }));
+    checkAllPokemons();
+  });
+
+  test(`Teste se é criado, dinamicamente, 
+  um botão de filtro para cada tipo de Pokémon`, () => {
+    const oneOfEveryType = data.reduce((acc, cur) => {
+      if (!acc.find((pokemon) => pokemon.type === cur.type)) {
+        acc.push(cur);
+      }
+      return acc;
+    }, []);
+
+    const sliceA = oneOfEveryType.slice(0, 2);
+    const sliceB = oneOfEveryType.slice(+'3');
+
+    const pokedexA = renderPokedex({ pokemons: sliceA });
+    expect(pokedexA.getAllByTestId(testIds.buttonType).length).toBe(sliceA.length);
+    expect(pokedexA.getByRole('button', { name: 'All' })).toBeInTheDocument();
+
+    cleanup();
+
+    const pokedexB = renderPokedex({ pokemons: sliceB });
+    expect(pokedexB.getAllByTestId(testIds.buttonType).length).toBe(sliceB.length);
+    expect(pokedexB.getByRole('button', { name: 'All' })).toBeInTheDocument();
+  });
+
+  test(`O botão de Próximo pokémon deve ser desabilitado quando
+   a lista filtrada de Pokémons tiver um só pokémon`, () => {
+    const { getByRole } = renderPokedex({ pokemons: [data[0]] });
+    expect(getByRole('button', { name: nextPokemon })).toBeDisabled();
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,53 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { Pokemon } from '../components';
+import renderWithRouter from '../components/route/renderWithRouter'
+import data from '../data';
+
+const testIds = {
+  name: 'pokemon-name',
+  type: 'pokemonType',
+  weight: 'pokemon-weight',
+};
+
+const pokeTest = data[0];
+
+const renderPokemon = ({ pokemon = pokeTest, showDetails, isFavorite } = {}) => (
+  renderWithRouter(<Pokemon { ...{ pokemon, showDetails, isFavorite } } />)
+);
+
+describe('06', () => {
+  test('Teste se é renderizado um card com as informações de determinado pokémon', () => {
+    const { getByTestId, getByAltText } = renderPokemon();
+    const { name, type, averageWeight: { value, measurementUnit } } = pokeTest;
+    expect(getByTestId(testIds.name).textContent).toBe(name);
+    expect(getByTestId(testIds.type).textContent).toBe(type);
+    expect(getByTestId(testIds.weight).textContent)
+      .toBe(`Average weight: ${value} ${measurementUnit}`);
+    expect(getByAltText(`${name} sprite`).src).toBe('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  test(`Teste se o card do Pokémon indicado na Pokédex 
+  contém um link de navegação para exibir detalhes deste Pokémon`, () => {
+    const { getByRole } = renderPokemon({ showDetails: true });
+    const { id } = pokeTest;
+    expect(getByRole('link', { name: 'More details' }).pathname).toBe(`/pokemons/${id}`);
+  });
+
+  test(`Teste se ao clicar no link de navegação do Pokémon, 
+  é feito o redirecionamento da aplicação para a página de detalhes de Pokémon`, () => {
+    const { getByRole, history } = renderPokemon({ showDetails: true });
+    const { id } = pokeTest;
+    expect(history.location.pathname).toBe('/');
+    userEvent.click(getByRole('link', { name: 'More details' }));
+    expect(history.location.pathname).toBe(`/pokemons/${id}`);
+  });
+
+  test('Teste se existe um ícone de estrela nos Pokémons favoritados', () => {
+    const { getByAltText } = renderPokemon({ isFavorite: true });
+    const { name } = pokeTest;
+    const icon = getByAltText(`${name} is marked as favorite`);
+    expect(icon).toBeInTheDocument();
+    expect(icon.src).toContain('/star-icon.svg');
+  });
+});
