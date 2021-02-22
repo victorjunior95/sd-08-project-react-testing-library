5b3645c5cce00c76fc0570bd0b293c35dcc80912
antonio arieiro <sfgfffs@gmail.com>
2021-02-22 10:36:10 -0300

    render router

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import About from '../components/About';
-import renderWithRouter from '../helpers/renderWithRouter';
+import renderWithRouter from '../renderwhtRoute/renderWithRouter';
 
 const firstParagraph = /This application simulates a Pokédex/i;
 const paragraphTwo = /One can filter Pokémons by type/i;
