0dae561ac36ba6cfcb7b4effda53d917e9276e01
Felipe Franca <felipefrancafj@gmail.com>
2021-02-21 22:36:06 -0300

    IMPLEMENTED: App.test.js | check if the links  to be in the document

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { getAllByText, render } from '@testing-library/react';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -11,4 +11,34 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+it('check if a link Home exist in the document', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const linkHomeElement = getByText(/Home/i);
+  expect(linkHomeElement).toBeInTheDocument();
+});
+
+it('check if a link about exist in the document', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const linkAboutElement = getByText(/About/i);
+  expect(linkAboutElement).toBeInTheDocument();
+});
+
+it('ckeck if a link Favorite exist in the document', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const linkFavoriteElement = getAllByText(/Favorite Pokémons/i);
+  expect(linkFavoriteElement).toBeInTheDocument();
 });
