e3d5308d468df0790c62c0cb063a0ee418721d07
Felipe Franca <felipefrancafj@gmail.com>
2021-02-21 22:15:39 -0300

    FIXING: about.test.js with react-test-library

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { About } from  '../components';
-import renderWithRouter from  '..renderWithRouter';
+import { About } from '../components';
+import renderWithRouter from '../renderWithRouter';
 
-it("Check if pockedex infor exist in the document", () => {
-	const { getByRole } = renderWithRouter(<About />);
-	const aboutAll = getByRole('heading',{ level: 2 });
+it('Check if pockedex infor exist in the document', () => {
+  const { getByRole } = renderWithRouter(<About />);
+	const aboutAll = getByRole('heading', { level: 2 });
 	expect(aboutAll).toBeInTheDocument();
 	expect(aboutAll).toHaveTextContent('About Pokédex');
 });
 
-it("Check if image of pokemon to be in the screnn", () => {
-	const pockedexImg = 
-	"https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png";
+it('Check if image of pokemon to be in the screnn', () => {
+	const pockedexImg = "https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png";
 	const { getByRole } = renderWithRouter(<About />);
 	const img = getByRole('img', { name: /pokédex/i });
 	expect(img).toBeInTheDocument();
