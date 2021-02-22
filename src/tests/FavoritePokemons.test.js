350a7fa4684d2878dc9be4bd033f54d342a629ca
Felipe Franca <felipefrancafj@gmail.com>
2021-02-22 11:57:11 -0300

    IMPLEMETED: FavoritePokemons.test.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
+import { FavoritePokemons } from '../components/FavoritePokemons';
+
+it('ckeck if ´No favorite pokemon fount´ to be in the document', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+  const message = getByText('No favorite pokemon found');
+  expect(message).toBeInTheDocument();
+});
+
+it('check if pokemons showing on the screnn to be the expected', () => {
+  const { getAllByTestId } = renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+  expect(getAllByTestId('pokemon-name').length).toBe(pokemons.length);
+});
+
+it('', () => {
+  const { getAllByTestId } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+  expect(getAllByTestId('poke-name').length).toBe(0);
+});
