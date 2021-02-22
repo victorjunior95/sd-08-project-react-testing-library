79b7c87ff2f49d372555c6ac3b00baf2d4f7b457
Felipe Franca <felipefrancafj@gmail.com>
2021-02-21 22:40:34 -0300

    FIXING-2: App.test.js | check if the links  to be in the document

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { getAllByText, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
