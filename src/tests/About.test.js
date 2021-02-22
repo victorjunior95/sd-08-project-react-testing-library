e7cd064c9f4c48c6b196296ca4f74227b33b9c67
Felipe Franca <felipefrancafj@gmail.com>
2021-02-21 22:17:43 -0300

    FIXING-2: about.test.js with react-test-library

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { About } from '../components';
 import renderWithRouter from '../renderWithRouter';
 
-it('Check if pockedex infor exist in the document', () => {
+it('Check if contains de Pokedex info', () => {
   const { getByRole } = renderWithRouter(<About />);
-	const aboutAll = getByRole('heading', { level: 2 });
-	expect(aboutAll).toBeInTheDocument();
-	expect(aboutAll).toHaveTextContent('About Pokédex');
+  const aboutAll = getByRole('heading', { level: 2 });
+  expect(aboutAll).toBeInTheDocument();
+  expect(aboutAll).toHaveTextContent('About Pokédex');
 });
 
-it('Check if image of pokemon to be in the screnn', () => {
-	const pockedexImg = "https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png";
-	const { getByRole } = renderWithRouter(<About />);
-	const img = getByRole('img', { name: /pokédex/i });
-	expect(img).toBeInTheDocument();
-	expect(img.src).toBe(pockedexImg);
+it('Check if contains a Pokedex image', () => {
+  const pokedexImage = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  const { getByRole } = renderWithRouter(<About />);
+  const image = getByRole('img', { name: /pokédex/i });
+  expect(image).toBeInTheDocument();
+  expect(image.src).toBe(pokedexImage);
 });
