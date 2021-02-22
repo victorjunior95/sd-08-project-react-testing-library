4c5340479550dbd6191a9b96d83cac83cac7f2a4
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-02-22 15:46:14 -0300

    refatora codigo req2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import { screen, render } from '{@testing-library/react';
-import About from '../components/About';
+import { About } from '../components';
+import renderWithRouter from '../renderWithRouter';
 
-describe('Test the About component', () => {
-  test('check if page contains a h2 heading', () => {
-    render(<About />);
-    const pageHeading = screen.getByRole('heading', {
-      level: 2,
-      name: 'About Pokédex',
-    });
-    expect(pageHeading).toBeInTheDocument();
-  });
+test('check if page contains Pokedex Info', () => {
+  const { getByRole } = renderWithRouter(<About />);
+  const allAbout = getByRole('heading', { level: 2 });
+  expect(allAbout).toBeInTheDocument();
+  expect(allAbout).toHaveTextContent('About Pokédex');
+});
 
-  test('check if page contains Pokédex info', () => {
-    const { container } = render(<About />);
-    const pokedexInfo = container.querySelectorAll('p');
-    expect(pokedexInfo).toHaveLength(2);
-  });
-
-  test('check if page render Pokedex image', () => {
-    render(<About />);
-    const pokedexImage = screen.getByRole('img');
-    expect(pokedexImage).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
-    expect(pokedexImage).toHaveAttribute('alt', 'Pokédex');
-  });
+test('check if page render Pokedex image', () => {
+  const pokedexImage = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  const { getByRole } = renderWithRouter(<About />);
+  const image = getByRole('img', { name: /pokédex/i });
+  expect(image).toBeInTheDocument();
+  expect(image.src).toBe(pokedexImage);
 });
