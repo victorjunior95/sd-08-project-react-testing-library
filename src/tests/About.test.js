5de6e61ae948fbae44676541f1bd6c7220ae4602
Doug-Fernando <doug.fernando95@gmail.com>
2021-02-22 00:18:42 -0300

    Corrige teste About.test.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,16 +4,16 @@ import About from '../components/About';
 import renderWithRouter from '../helper/renderWithRouter';
 
 it('Teste se a página contém as informações sobre a Pokédex.', () => {
-  renderWithRouter(<About />);
+  const { container } = renderWithRouter(<About />);
   const textAbout = screen.getByRole('heading', {
     level: 2,
     name: /About Pokédex/,
   });
-  const paragraphs = screen.getAllByTestId('paragraph-about').length;
-  const pokedexImg = screen.getByTestId('image-pokedex-about');
+  const paragraphs = container.querySelectorAll('p').length;
+  const pokedexImg = container.querySelector('img').src;
   const pokedexImgSrc = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
 
   expect(textAbout).toBeInTheDocument();
   expect(paragraphs).toBe(2);
-  expect(pokedexImg.src).toBe(pokedexImgSrc);
+  expect(pokedexImg).toBe(pokedexImgSrc);
 });
