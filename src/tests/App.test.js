f6da16e6f624732179743ff74378e056d910156f
Beto-CGF <beto.cgf@gmail.com>
2021-02-22 11:53:52 -0300

    Killer the mutants VQV

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,3 +12,43 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('Verificando se o link Home está na página', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Home')).toBeInTheDocument();
+});
+
+test('Verificando se o link About está na página', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('About')).toBeInTheDocument();
+});
+
+test('Verificando se o link Favorite Pokémons está na página', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+});
