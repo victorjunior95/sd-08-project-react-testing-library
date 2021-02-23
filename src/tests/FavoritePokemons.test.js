38a8c05e7af27acd0ebc027d3ffde36cfa9bd8db
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-02-23 20:11:58 -0300

    Implementado arquivo de teste do componente FavoritePokemons.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+import pokemons from '../data';
+
+describe('Testing the FavoritePokemons.js component', () => {
+  it(`if the message 'No favorite pokemon found' is displayed on the screen, 
+  if the person does not have favorite pokemon.`, () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+
+    const msg = getByText(/No favorite pokemon found/i);
+
+    expect(msg).toBeInTheDocument();
+  });
+
+  it('whether all favorite Pokémon cards are displayed', () => {
+    const { getAllByTestId } = renderWithRouter(
+      <FavoritePokemons pokemons={ pokemons } />,
+    );
+
+    const pokemonsNames = getAllByTestId('pokemon-name');
+
+    expect(pokemonsNames.length).toBe(pokemons.length);
+  });
+
+  it('if no Pokémon card is displayed, if it is not favored', () => {
+    const { queryAllByTestId } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+
+    const pokemonsNames = queryAllByTestId('pokemon-name');
+
+    expect(pokemonsNames.length).toBe(0);
+  });
+});
