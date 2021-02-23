e8b48dbd6bd68d057679d552956d04eddec93d80
Rafael Greco <rafaelmgreco@hotmail.com>
2021-02-23 11:57:46 -0300

    req3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Req 03', () => {
+  test(' No favorite pokemon found, se a pessoa não tiver pokémons favoritos.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const mensagem = 'No favorite pokemon found';
+    expect(getByText(mensagem)).toBeInTheDocument();
+  });
+});
