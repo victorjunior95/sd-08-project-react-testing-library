447f1e6bc4d1ebb1d21107720bc726ce8785cf75
maym4y <jennim.almeida@gmail.com>
2021-02-23 20:51:51 -0300

    Primeiro requisito concluído

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Testa a tela App e seu funcionamento', () => {
+  it('Testa rota da tela inicial', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    const app = getByText('Pokédex');
+    const home = getByText('Home');
+    const about = getByText('About');
+    const favorite = getByText('Favorite Pokémons');
+    const { pathname } = history.location;
+
+    expect(app).toBeInTheDocument();
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favorite).toBeInTheDocument();
+    expect(pathname).toBe('/');
+
+    fireEvent.click(home);
+
+    expect(pathname).toBe('/');
+
+    fireEvent.click(about);
+
+    expect(history.location.pathname).toBe('/about');
+
+    fireEvent.click(favorite);
+
+    expect(history.location.pathname).toBe('/favorites');
+
+    fireEvent.click(home);
+    history.push('/naoexisto');
+    const notFound = getByText('Page requested not found');
+
+    expect(notFound).toBeInTheDocument();
+  });
 });
