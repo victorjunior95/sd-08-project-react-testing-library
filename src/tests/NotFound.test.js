93169c00186c8308220566de1170b89284134d0a
Rafael Greco <rafaelmgreco@hotmail.com>
2021-02-23 12:19:23 -0300

    req4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Req 04', () => {
+  test('Se página contém um heading h2 com o texto Page requested not found ', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    expect(getByText('Page requested not found')).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const image = getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
