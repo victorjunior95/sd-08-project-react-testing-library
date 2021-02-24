f942fe727ee3f6456af0574c8e50398581aeabe2
maym4y <jennim.almeida@gmail.com>
2021-02-23 21:27:09 -0300

    Requisito 3 concluído

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+// import {  } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('Testa página de pokemons favoritos', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const noPoks = getByText('No favorite pokemon found');
+
+  expect(noPoks).toBeInTheDocument();
+});
