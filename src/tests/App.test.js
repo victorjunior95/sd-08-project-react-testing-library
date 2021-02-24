2842a8a0aa5ae0a6ed308b926f164636feea7453
Matheus Gabriel <matheus.gabriel.couto@hotmail.com>
2021-02-24 08:24:48 -0400

    requisito 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
+  const history = createBrowserHistory();
   const { getByText } = render(
-    <MemoryRouter>
+    <Router initialEntries={ ['/'] } history={ history }>
       <App />
-    </MemoryRouter>,
+    </Router>,
   );
+
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+
+  const homeBtn = getByText('Home');
+  const aboutBtn = getByText('About');
+  const favoriteBtn = getByText('Favorite Pokémons');
+  expect(homeBtn).toBeInTheDocument();
+  expect(aboutBtn).toBeInTheDocument();
+  expect(favoriteBtn).toBeInTheDocument();
+
+  userEvent.click(homeBtn);
+  expect(history.location.pathname).toBe('/');
+
+  userEvent.click(aboutBtn);
+  expect(history.location.pathname).toBe('/about');
+  expect(getByText('About Pokédex')).toBeInTheDocument();
+
+  userEvent.click(favoriteBtn);
+  expect(history.location.pathname).toBe('/favorites');
+  expect(getByText('Favorite pokémons')).toBeInTheDocument();
+
+  history.push('/seila');
+  expect(getByText('Page requested not found')).toBeInTheDocument();
 });
