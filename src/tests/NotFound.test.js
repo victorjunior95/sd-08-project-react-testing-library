680dc2e206401156de9638b71e3e2af5a32c7acc
maym4y <jennim.almeida@gmail.com>
2021-02-23 21:34:41 -0300

    Requisito 3 concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+// import {  } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('testando página não existente', () => {
+  const { getByText, getByAltText } = renderWithRouter(<NotFound />);
+
+  const noPage = getByText('Page requested not found');
+  const img = getByAltText('Pikachu crying because the page requested was not found');
+
+  expect(noPage).toBeInTheDocument();
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
