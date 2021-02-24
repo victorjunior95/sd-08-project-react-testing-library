ae18ed9028408b4fe29679dc81cda5eb66d1a12b
Matheus Gabriel <matheus.gabriel.couto@hotmail.com>
2021-02-24 08:44:13 -0400

    requisito 02

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,14 @@
+
 import React from 'react';
 import './about.css';
 
 const About = () => (
   <section>
-    <h2>{`About ` + `Pokédex`}</h2>
+    <h2>{`About Pokédex`}</h2>
     <section>
       <p>
         This application simulates a Pokédex, a
-        digital encyclopedia containing all Pokémons
+        digital encliclopedia containing all Pokémons
       </p>
       <p>One can filter Pokémons by type, and see more details for each one of them</p>
       <img
--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+test('Tests About', () => {
+  const { getByText, getByRole, getByAltText } = render(<About />);
+  const phrasePartOne = 'This application simulates a Pokédex, ';
+  const phrasePartTwo = 'a digital encliclopedia containing all Pokémons';
+  expect(getByText(phrasePartOne + phrasePartTwo)).toBeInTheDocument();
+
+  const secondLine = getByText(
+    'One can filter Pokémons by type, and see more details for each one of them',
+  );
+  expect(secondLine).toBeInTheDocument();
+
+  const title = getByRole('heading', {
+    level: 2,
+    name: 'About Pokédex',
+  });
+  expect(title).toBeInTheDocument();
+
+  const image = getByAltText('Pokédex');
+
+  expect(image).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
