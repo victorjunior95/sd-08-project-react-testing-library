8ae32128a6efe0c307e49edcb89abb2ff28b8553
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-02-24 13:53:48 -0300

    Implementado arquivo de teste do componente NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testing the NotFound.js component if the page contains', () => {
+  it('an h2 heading with the text "Page requested not found Crying emoji"', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    const h2 = getByRole('heading', {
+      name: 'Page requested not found Crying emoji',
+      level: 2,
+    });
+
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('an h2 heading with the text "Page requested not found Crying emoji"', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    const image = getByRole('img');
+
+    expect(image).toBeInTheDocument();
+  });
+});
