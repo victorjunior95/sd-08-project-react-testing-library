d1a562a25e0ba14fa975bd86b3d16c487f5a5127
maym4y <jennim.almeida@gmail.com>
2021-02-23 21:14:58 -0300

    Requisito 2 concluido

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+// import {  } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+test('testa página about', () => {
+  const { getByAltText, getByText } = renderWithRouter(<About />);
+  const img = getByAltText('Pokédex');
+  const about = getByText('About Pokédex');
+
+  expect(about).toBeInTheDocument();
+  expect(img.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
