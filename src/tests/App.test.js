98f064a1408cbaf726511452cacebe08eebfdd6f
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-02-25 15:52:57 -0300

    notfound

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -94,7 +94,7 @@ test('A aplicação é redirecionada ao entrar em uma URL desconhecida.', () => 
   const { getByText, history } = renderWithRouter(
     <App />,
   );
-  const route = '/xablau';
+  const route = '/notfound';
   history.push(route);
   const notFound = getByText(/Page requested not found/i);
   expect(notFound).toBeInTheDocument();
