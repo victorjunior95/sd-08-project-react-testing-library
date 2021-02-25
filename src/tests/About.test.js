f1584f233b3a6fc40cc2a4ba47494addb1243a21
Demistocles <demi.treze@gmail.com>
2021-02-25 11:31:42 -0300

    enviando o segundo requisito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import About from '../components/About';
+
+test('testing the about component', () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={ history }>
+      <About />
+    </Router>,
+  );
+  const infoText = screen.getByText(/This application simulates a Pokédex/i);
+  expect(infoText).toBeInTheDocument();
+  const aboutPokeText = screen.getByRole('heading', {
+    level: 2,
+    name: /About Po/i,
+  });
+  expect(aboutPokeText).toBeInTheDocument();
+
+  const paragraph1 = screen.getByText(/This application simulates a Pokédex/);
+  expect(paragraph1).toBeInTheDocument();
+
+  const paragraph2 = screen.getByText(/One can filter Pokémons by type/);
+  expect(paragraph2).toBeInTheDocument();
+
+  const imagem = screen.getByRole('img');
+  expect(imagem.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
