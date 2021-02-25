caf98527fcdedebb4b313432c09403e9cffe2e7a
Cainan <cainan6697@gmail.com>
2021-02-25 12:27:56 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('FavoritePokemons', () => {
+  test('if the message "No favorite pokemon found" is displayed on the screen', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    expect(getByText(/no favorite pokemon found/i)).toBeInTheDocument();
+  });
+});
