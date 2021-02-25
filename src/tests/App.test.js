8a1d13f7e7e4f3398d30dd47ba22c255fbc8c340
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-02-25 15:13:58 -0300

    req1 finalizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,3 +12,39 @@ test('shows the Pokédex when the route is `/`', () => {
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
+
+test('O primeiro link deve possuir o texto `Home`', () => {
+  const { getByRole } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const home = getByRole('link', {
+    name: /home/i,
+  });
+  expect(home).toBeInTheDocument();
+});
+
+test('O segundo link deve possuir o texto `About`', () => {
+  const { getByRole } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const about = getByRole('link', {
+    name: /about/i,
+  });
+  expect(about).toBeInTheDocument();
+});
+
+test('O terceiro link deve possuir o texto `Favorite Pokémons`', () => {
+  const { getByRole } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const favorite = getByRole('link', {
+    name: /Favorite Pokémons/i,
+  });
+  expect(favorite).toBeInTheDocument();
+});
