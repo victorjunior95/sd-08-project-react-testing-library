758d1c1eab95b032d554d267a54b2b0a61d9a9e9
Cainan <cainan6697@gmail.com>
2021-02-25 15:43:09 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('NotFound', () => {
+  it('page contains an h2 heading with the text "Page requested not found emoji"', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    expect(getByRole('heading', {
+      level: 2,
+      name: 'Page requested not found Crying emoji',
+    })).toBeInTheDocument();
+  });
+  it('page shows the image "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
