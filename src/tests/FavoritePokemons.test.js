8fc0c09995cddea5dcd3c828446fe6ea33fd8b6f
enicio <jessandro42@gmail.com>
2021-02-25 10:26:00 -0300

    Terceiro requisito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+const renderPath = (path) => {
+  const history = createBrowserHistory();
+  history.push(path);
+  const { ...resources } = render(
+    <Router history={ history }>
+      <App />
+    </Router>,
+  );
+  return { ...resources };
+};
+
+test('renders a reading with the text `No favorite pokemon found`', () => {
+  const { getByText } = renderPath('/favorites');
+  const heading = getByText(/No favorite pokemon found/i);
+  expect(heading).toBeInTheDocument();
+});
