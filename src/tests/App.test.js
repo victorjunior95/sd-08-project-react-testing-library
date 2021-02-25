a0502b984dc4918e70ff3447e92978d74aeb7bdf
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-24 22:57:20 -0300

    requisito 1 ajustes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -11,25 +11,16 @@ describe('App.js', () => {
   });
   test('Checks for navigation links', () => {
     const { getByRole } = renderWithRouter(<App />);
-    const homeLink = getByRole('link', {
-      name: 'Home',
-    });
-    const aboutLink = getByRole('link', {
-      name: 'About',
-    });
-    const favoritePokemon = getByRole('link', {
-      name: 'Favorite Pokémons',
-    });
+    const homeLink = getByRole('link', { name: 'Home' });
+    const aboutLink = getByRole('link', { name: 'About' });
+    const favoritePokemon = getByRole('link', { name: 'Favorite Pokémons' });
     expect(homeLink).toBeInTheDocument();
     expect(aboutLink).toBeInTheDocument();
     expect(favoritePokemon).toBeInTheDocument();
   });
   test('Checks whether you are redirected to Home', () => {
     const { getByRole, history } = renderWithRouter(<App />);
-
-    const homeLink = getByRole('link', {
-      name: 'Home',
-    });
+    const homeLink = getByRole('link', { name: 'Home' });
     userEvent.click(homeLink);
     const { pathname } = history.location;
     expect(pathname).toBe('/');
@@ -37,9 +28,7 @@ describe('App.js', () => {
   test('Checks whether you are redirected to About', () => {
     const { getByRole, history } = renderWithRouter(<App />);
 
-    const aboutLink = getByRole('link', {
-      name: 'About',
-    });
+    const aboutLink = getByRole('link', { name: 'About' });
     userEvent.click(aboutLink);
     const { pathname } = history.location;
     expect(pathname).toBe('/about');
@@ -47,13 +36,9 @@ describe('App.js', () => {
 
   test('Checks whether you are redirected to Favorite Pokemon', () => {
     const { getByRole, history } = renderWithRouter(<App />);
-
-    const favoritePokemon = getByRole('link', {
-      name: 'Favorite Pokémons',
-    });
+    const favoritePokemon = getByRole('link', { name: 'Favorite Pokémons' });
     userEvent.click(favoritePokemon);
     const { pathname } = history.location;
-
     expect(pathname).toBe('/favorites');
   });
   test('Checks whether it is redirected to Not Found when accessing unknown URL',
