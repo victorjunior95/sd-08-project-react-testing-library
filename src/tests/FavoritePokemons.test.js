7bae1a1a47e60725538f2d6c9f9288509fe22c0b
Demistocles <demi.treze@gmail.com>
2021-02-25 11:46:15 -0300

    enviando terceiro requisito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -15,4 +15,23 @@ test('pokemon favoritado', () => {
     /No fav/i,
   );
   expect(noFavPoke).toBeInTheDocument();
+  const Favorites = [pokemons[0], pokemons[1]];
+  const pokemons2 = screen.queryByText(<FavoritePokemons pokemons={ Favorites } />);
+
+  expect(pokemons2).not.toBeInTheDocument();
+});
+
+test('pokemon outro', () => {
+  const pokeFavorito = [pokemons[0], pokemons[1]];
+  const history = createMemoryHistory();
+  render(
+    <Router history={ history }>
+      <FavoritePokemons pokemons={ pokeFavorito } />
+    </Router>,
+  );
+
+  const pikachu = screen.getByText('Pikachu');
+  const charmander = screen.getByText('Charmander');
+  expect(pikachu).toBeInTheDocument();
+  expect(charmander).toBeInTheDocument();
 });
