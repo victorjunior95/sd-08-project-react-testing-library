a830073c53b702777afbbfc7b859f4b37b394f17
enicio <jessandro42@gmail.com>
2021-02-24 23:22:06 -0300

    Primeiro requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -11,4 +11,13 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+
+  const Home = getByText(/Home/i);
+  expect(Home).toBeInTheDocument();
+
+  const About = getByText(/About/i);
+  expect(About).toBeInTheDocument();
+
+  const FavoritePokémons = getByText(/Favorite Pokémons/i);
+  expect(FavoritePokémons).toBeInTheDocument();
 });
