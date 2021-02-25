6cd8918c436c19b7bbc74c611a109171f186ed20
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-02-25 16:46:55 -0300

    req2 finalizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,55 +5,57 @@ import { render } from '@testing-library/react';
 // import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
-});
-test('O primeiro link deve possuir o texto Home', () => {
-  const { getByRole } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const home = getByRole('link', {
-    name: /home/i,
+describe('', () => {
+  test('Testa se a página é renderizada com o texto `pokedex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
   });
-  expect(home).toBeInTheDocument();
-});
-test('O segundo link deve possuir o texto About', () => {
-  const { getByRole } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const about = getByRole('link', {
-    name: /about/i,
+  test('Mostrando se a pokedex possui a rota `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
   });
-  expect(about).toBeInTheDocument();
-});
-test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
-  const { getByRole } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const favorite = getByRole('link', {
-    name: /Favorite Pokémons/i,
+  test('O primeiro link deve possuir o texto Home', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const home = getByRole('link', {
+      name: /home/i,
+    });
+    expect(home).toBeInTheDocument();
   });
-  expect(favorite).toBeInTheDocument();
+  test('O segundo link deve possuir o texto About', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const about = getByRole('link', {
+      name: /about/i,
+    });
+    expect(about).toBeInTheDocument();
+  });
+  test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const favorite = getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    expect(favorite).toBeInTheDocument();
+  });
 });
 // test('Teste se ao clicar no link Home a página inicial é redirecionada na URL /', () => {
 //   const { getByText, history } = renderWithRouter(
