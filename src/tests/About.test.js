72c80429ad7f9c2688290fd98a6a24b5a94e40ad
Thalys Matias Carrara <thalysmatiascarrara@gmail.com>
2021-02-27 15:06:29 -0400

    requisito 2 finalizado e eslint corrigido

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('teste da página /About', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const Subtitle = screen.getByRole('heading', { name: /about pokédex/i,
+      level: 2 });
+    expect(Subtitle).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const paragraphs = screen.getAllByTestId('about-p');
+    expect(paragraphs.length).toBe(2);
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    renderWithRouter(<About />);
+    const img = screen.getByRole('img');
+    const imgUrl = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(img.src).toBe(imgUrl);
+  });
+});
