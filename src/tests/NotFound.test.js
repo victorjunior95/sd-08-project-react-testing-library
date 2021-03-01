eba35066db286472c933a7d47afaebe7919f3ed0
Rafael <rafaeljavaro750@gmail.com>
2021-02-28 21:16:22 -0300

    Testes componente <NotFound />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa componente <NotFound />', () => {
+  it('Testa se a mensagem de Pagina não encontrada renderiza', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const textOnScreen = getByText(/Page requested not found/i);
+    expect(textOnScreen).toBeInTheDocument();
+  });
+
+  it('Testa se o Gif de Pagina não encontrada renderiza', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const altTextImg = getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    const gifURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(altTextImg.src).toBe(gifURL);
+  });
+});
