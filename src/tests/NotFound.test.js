5ecff31124248886b6c18e16867aad6cae2fa344
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-03-01 17:03:06 -0300

    NotFound tests

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+
+test('Teste de caminho inexistente e renderização do Not Found', () => {
+  const { getByRole, getByAltText, history } = renderWithRouter(<App />);
+  history.push('/pagina/nao-existe');
+
+  const noMatchText = getByRole('heading', {
+    level: 2,
+  });
+  expect(noMatchText).toHaveTextContent('Page requested not found 😭');
+
+  const noMatchImg = getByAltText(
+    /Pikachu crying because the page requested was not found/i,
+  );
+  expect(noMatchImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
