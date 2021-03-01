62c599cb8c820c9189391053880bbb0464899838
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-03-01 19:22:03 -0300

    About tests

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helper/renderWithRouter';
+
+import About from '../components/About';
+
+test('Deve verificar o que está contido em About', () => {
+  const { getByRole, getAllByTestId, getByAltText } = renderWithRouter(<About />);
+
+  const aboutHeading = getByRole('heading', {
+    level: 2,
+  });
+  expect(aboutHeading).toHaveTextContent('About Pokédex');
+
+  const aboutParagraph = getAllByTestId('about-paragraph');
+  expect(aboutParagraph.length).toBe(2);
+
+  const aboutImg = getByAltText(/Pokédex/i);
+  expect(aboutImg).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
