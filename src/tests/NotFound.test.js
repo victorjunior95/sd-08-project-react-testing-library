6647f35a8906b4177f188791f076ff6308a06ec0
Rodrigo <rd64103@gmail.com>
2021-03-03 04:16:09 -0300

    projeto completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helper/renderWithRouter';
+
+import { NotFound } from '../components';
+
+describe('testa <NotFound /> componente', () => {
+  it('testando texto', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    expect(getByRole('heading', { name: /Page requested not found/, level: 2 }))
+      .toBeInTheDocument();
+  });
+
+  it('aparecer gif', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+
+    expect(getByAltText('Pikachu crying because the page requested was not found').src)
+      .toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,116 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helper/renderWithRouter';
+
+import Pokedex from '../components/Pokedex';
+
+import pokemons from '../data';
+
+const isPokemonFavoriteById = {
+  4: false,
+  10: true,
+  23: false,
+  25: true,
+  65: false,
+  78: false,
+  143: false,
+  148: false,
+  151: false,
+};
+
+const renderPokedexWithRouter = () => renderWithRouter(
+  <Pokedex
+    pokemons={ pokemons }
+    isPokemonFavoriteById={ isPokemonFavoriteById }
+  />,
+);
+
+describe('Teste `<Pokédex />` componente', () => {
+  it('deve rederizar h2 `Encountered pokémons`.', () => {
+    const { getByRole } = renderPokedexWithRouter();
+
+    expect(
+      getByRole('heading', {
+        level: 2,
+        name: /Encountered pokémons/i,
+      }),
+    ).toBeInTheDocument();
+  });
+
+  it('Rederizar prox pokemon `Próximo botão`', () => {
+    const { getByRole, queryByText } = renderPokedexWithRouter();
+
+    const THIRD_POKEMON_INDEX = 3;
+    const LAST_POKEMON_INDEX = 9;
+
+    expect(queryByText(/pikachu/i)).toBeInTheDocument();
+    expect(getByRole('button', { name: /próximo pokémon/i }))
+      .toBeInTheDocument();
+
+    userEvent.click(getByRole('button', { name: /próximo pokémon/i }));
+    expect(queryByText(/pikachu/i)).not.toBeInTheDocument();
+    expect(queryByText(/charmander/i)).toBeInTheDocument();
+
+    userEvent.click(getByRole('button', { name: /próximo pokémon/i }));
+    expect(queryByText(/charmander/i)).not.toBeInTheDocument();
+    expect(queryByText(/caterpie/i)).toBeInTheDocument();
+
+    for (let i = THIRD_POKEMON_INDEX; i <= LAST_POKEMON_INDEX; i += 1) {
+      userEvent.click(getByRole('button', { name: /próximo pokémon/i }));
+    }
+    expect(queryByText(/pikachu/i)).toBeInTheDocument();
+  });
+
+  it('Rederizar unico pokemon', () => {
+    const { getAllByTestId } = renderPokedexWithRouter();
+
+    const pokemon = getAllByTestId('pokemon-name');
+    expect(pokemon.length).toBe(1);
+  });
+
+  it('Filtrar tipo.', () => {
+    const { getByRole, getByTestId } = renderPokedexWithRouter();
+
+    userEvent.click(getByRole('button', { name: /bug/i }));
+    expect(getByTestId('pokemonType').innerHTML).toBe('Bug');
+
+    userEvent.click(getByRole('button', { name: /electric/i }));
+    expect(getByTestId('pokemonType').innerHTML).toBe('Electric');
+  });
+
+  it('botão com texto "All"', () => {
+    const { getByRole, getByTestId } = renderPokedexWithRouter();
+
+    expect(getByRole('button', { name: /all/i })).toBeInTheDocument();
+
+    let previousPokemonType = getByTestId('pokemonType').innerHTML;
+    userEvent.click(getByRole('button', { name: /próximo pokémon/i }));
+    expect(getByTestId('pokemonType').innerHTML)
+      .not.toBe(previousPokemonType);
+
+    userEvent.click(getByRole('button', { name: /bug/i }));
+    userEvent.click(getByRole('button', { name: /all/i }));
+
+    previousPokemonType = getByTestId('pokemonType').innerHTML;
+    userEvent.click(getByRole('button', { name: /próximo pokémon/i }));
+    expect(getByTestId('pokemonType').innerHTML)
+      .not.toBe(previousPokemonType);
+  });
+
+  it('Rederizar botões dinamicamente', () => {
+    const { getAllByTestId } = renderPokedexWithRouter();
+
+    const TYPES_QUANTITY = 7;
+
+    expect(getAllByTestId('pokemon-type-button').length)
+      .toBe(TYPES_QUANTITY);
+  });
+
+  it('Tirar botão caso haja um unico pokemon', () => {
+    const { getByRole } = renderPokedexWithRouter();
+
+    userEvent.click(getByRole('button', { name: /electric/i }));
+    expect(getByRole('button', { name: /próximo pokémon/i }))
+      .toBeDisabled();
+  });
+});
