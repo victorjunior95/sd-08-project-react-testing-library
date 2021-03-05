db2985ba1fc8e6ba48625b418eb5e8fac4b28c5d
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-03-05 13:54:05 -0300

    Favorite Pokemons tests

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+
+describe('Testing component Favorite Pokémons', () => {
+  it('Test: no favorite pokemon found', () => {
+    const { getAllByTestId, getByText } = renderWithRouter(<App />);
+
+    const favoriteBtn = getByText('Favorite Pokémons');
+    fireEvent.click(favoriteBtn);
+    const noFavoritesFound = getByText('No favorite pokemon found');
+    expect(noFavoritesFound).toBeInTheDocument();
+  });
+});
