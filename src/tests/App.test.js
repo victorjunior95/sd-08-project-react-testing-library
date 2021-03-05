13dcf03aa02734a35075bb1df6e9aaadec60faac
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-03-05 11:51:54 -0300

    App tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../helper/renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Testing component Pokedex', () => {
+  it('Test: render the home page', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const homeButton = getByText('Home');
+    fireEvent.click(homeButton);
+    const homePage = getByText('Encountered pokémons');
+    expect(homePage).toBeInTheDocument();
+  });
+
+  it('Test: render the About page', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const aboutButton = getByText('About');
+    fireEvent.click(aboutButton);
+    const aboutPage = getByText('About Pokédex');
+    expect(aboutPage).toBeInTheDocument();
+  });
+
+  it('Test: render the Favorite Pokémons page', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const favoriteButton = getByText('Favorite Pokémons');
+    fireEvent.click(favoriteButton);
+    const favoritePage = getByText('Favorite pokémons');
+    expect(favoritePage).toBeInTheDocument();
+  });
 });
