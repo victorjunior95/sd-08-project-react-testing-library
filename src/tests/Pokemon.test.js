ef43caac31a33f1defec822b69a2bb72dbd58bd0
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-03-06 13:31:56 -0300

    Pokemon tests

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe('Testing component Pokemon', () => {
+  it('Test: render pokémon card', () => {
+    const { getByTestId, getByAltText } = renderWithRouter(<App />);
+    const pokemonName = getByTestId('pokemon-name');
+    const pokemonType = getByTestId('pokemonType');
+    const pokemonWeight = getByTestId('pokemon-weight');
+    const pokemonImage = getByAltText(/Pikachu sprite/i);
+
+    expect(pokemonName).toHaveTextContent('Pikachu');
+    expect(pokemonType).toHaveTextContent('Electric');
+    expect(pokemonWeight).toHaveTextContent('Average weight: 6.0 kg');
+    expect(pokemonImage).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  it('Test: link for more details', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const linkMoreDetails = getByText('More details');
+
+    expect(linkMoreDetails).toHaveAttribute('href', '/pokemons/25');
+  });
+
+  it('Test: favorite icon', () => {
+    const { getByLabelText, getByText, getByAltText, history } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText('More details'));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+
+    fireEvent.click(getByLabelText(/Pokémon favoritado?/i));
+    const starIcon = getByAltText(/Pikachu is marked as favorite/i);
+    expect(starIcon).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
