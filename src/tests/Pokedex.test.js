e031d02bed664076598a20152e7d9a4370f46a1e
Max <gmaxgomes@gmail.com>
2021-03-06 21:30:50 -0300

    reqs

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,93 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Test the <Pokedex.js /> component', () => {
+  it('should have a h2 heading with text "Encountered pokémons"', () => {
+    const { getByRole } = renderWithRouter(<App />);
+
+    expect(getByRole('heading', { name: 'Encountered pokémons' })).toBeInTheDocument();
+    expect(getByRole('heading', { level: 2 })).toBeInTheDocument();
+  });
+
+  it('should show the next pokemon when the button is clicked', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+
+    expect(getByText(/Próximo pokémon/)).toBeInTheDocument();
+    expect(getByRole('img', { name: 'Pikachu sprite' })).toBeInTheDocument();
+    fireEvent.click(getByText(/Fire/));
+    expect(getByRole('img', { name: 'Charmander sprite' })).toBeInTheDocument();
+    fireEvent.click(getByText(/Próximo pokémon/));
+    expect(getByRole('img', { name: 'Rapidash sprite' })).toBeInTheDocument();
+    fireEvent.click(getByText(/Próximo pokémon/));
+    expect(getByRole('img', { name: 'Charmander sprite' })).toBeInTheDocument();
+  });
+
+  it('should show only one pokemon at time', () => {
+    const { getAllByText } = renderWithRouter(<App />);
+
+    expect(getAllByText('More details')).toHaveLength(1);
+  });
+
+  it('should filter pokemons by types', () => {
+    const { getByTestId, getByRole } = renderWithRouter(<App />);
+
+    expect(getByRole('img', { name: 'Pikachu sprite' })).toBeInTheDocument();
+    fireEvent.click(getByRole('button', { name: 'Electric' }));
+    expect(getByTestId('pokemonType')).toHaveTextContent('Electric');
+    fireEvent.click(getByRole('button', { name: 'Fire' }));
+    expect(getByTestId('pokemonType')).toHaveTextContent('Fire');
+    fireEvent.click(getByRole('button', { name: 'Bug' }));
+    expect(getByTestId('pokemonType')).toHaveTextContent('Bug');
+    fireEvent.click(getByRole('button', { name: 'Poison' }));
+    expect(getByTestId('pokemonType')).toHaveTextContent('Poison');
+    fireEvent.click(getByRole('button', { name: 'Psychic' }));
+    expect(getByTestId('pokemonType')).toHaveTextContent('Psychic');
+    fireEvent.click(getByRole('button', { name: 'Normal' }));
+    expect(getByTestId('pokemonType')).toHaveTextContent('Normal');
+    fireEvent.click(getByRole('button', { name: 'Dragon' }));
+    expect(getByTestId('pokemonType')).toHaveTextContent('Dragon');
+  });
+
+  it('should have a button to reset the filters', () => {
+    const { getByText, getByTestId, getByRole } = renderWithRouter(<App />);
+
+    expect(getByRole('button', { name: 'All' })).toBeInTheDocument();
+    const getName = getByTestId('pokemon-name');
+    expect(getName).toHaveTextContent('Pikachu');
+    fireEvent.click(getByText(/Próximo pokémon/));
+    expect(getName).toHaveTextContent('Charmander');
+    fireEvent.click(getByText(/Próximo pokémon/));
+    expect(getName).toHaveTextContent('Caterpie');
+    fireEvent.click(getByText(/Poison/));
+    expect(getName).toHaveTextContent('Ekans');
+    fireEvent.click(getByRole('button', { name: 'All' }));
+    expect(getName).toHaveTextContent('Pikachu');
+    fireEvent.click(getByText(/Próximo pokémon/));
+    expect(getName).toHaveTextContent('Charmander');
+    fireEvent.click(getByText(/Próximo pokémon/));
+    expect(getName).toHaveTextContent('Caterpie');
+  });
+
+  it('should create the type buttons dynamically', () => {
+    const { getByRole, getAllByTestId, queryByTestId } = renderWithRouter(<App />);
+
+    const types = ['Fire', 'Psychic', 'Electric', 'Bug', 'Poison', 'Dragon', 'Normal'];
+    const typesNumber = types.length;
+    expect(getByRole('button', { name: 'All' })).toBeInTheDocument();
+    expect(getAllByTestId('pokemon-type-button')).toHaveLength(typesNumber);
+    expect(queryByTestId('')).toBeNull();
+  });
+
+  it('should disable the "Next Pokémon" button when there is only one pokémon', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+
+    fireEvent.click(getByRole('button', { name: 'Electric' }));
+    expect(getByText(/Próximo pokémon/)).toHaveProperty('disabled');
+    fireEvent.click(getByRole('button', { name: 'Bug' }));
+    expect(getByText(/Próximo pokémon/)).toHaveProperty('disabled');
+    fireEvent.click(getByRole('button', { name: 'Dragon' }));
+    expect(getByText(/Próximo pokémon/)).toHaveProperty('disabled');
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,48 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Test the <Pokemon.js /> component', () => {
+  it('should show a pokemon info card', () => {
+    const { getByRole, getByText, getByTestId } = renderWithRouter(<App />);
+
+    fireEvent.click(getByRole('button', { name: 'Dragon' }));
+    expect(getByText('Dragonair')).toBeInTheDocument();
+    expect(getByTestId('pokemonType')).toHaveTextContent('Dragon');
+    expect(getByText(/Average weight: 16.5 kg/i)).toBeInTheDocument();
+    expect(getByRole('img')).toHaveProperty('src', 'https://cdn.bulbagarden.net/upload/2/2c/Spr_5b_148.png');
+    expect(getByRole('img')).toHaveProperty('alt', 'Dragonair sprite');
+  });
+
+  it('should have a link in the card to info page', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const infoButton = getByText(/More details/i);
+    expect(infoButton).toBeInTheDocument();
+    expect(infoButton).toHaveProperty('href', 'http://localhost/pokemons/25');
+  });
+
+  it('should be redirected to Details page when the button is clicked', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText(/More details/));
+    expect(getByText(/Pikachu Details/)).toBeInTheDocument();
+    expect(getByText(/Game Locations of Pikachu/)).toBeInTheDocument();
+  });
+
+  it('should show a new URL in the browser', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText(/More details/));
+    expect(history.location.pathname).toBe('/pokemons/25');
+  });
+
+  it('should have a star icon in the favorite pokemons', () => {
+    const { getByRole, getByText, getByAltText } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText(/More details/));
+    fireEvent.click(getByRole('checkbox'));
+    expect(getByAltText('Pikachu is marked as favorite')).toHaveProperty('src', 'http://localhost/star-icon.svg');
+  });
+});
