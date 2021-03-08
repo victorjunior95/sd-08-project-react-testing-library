112af7299e58ccab420f9719c09e2b712505f544
Victor Moraes <moraesvictorgk@gmail.com>
2021-03-07 22:30:55 -0300

    pokemon teste ajuste

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -9,7 +9,7 @@ import pokemons from '../data';
 const moreDetails = 'More details';
 
 describe('Pokemon.js', () => {
-  it('shows all info about selected pokemon', () => {
+  it('verifies if it shows all the infos about selected pokemon', () => {
     const { getByText, getByAltText } = renderWithRouter(
       <Pokemon pokemon={ pokemons[8] } isFavorite />,
     );
@@ -27,7 +27,7 @@ describe('Pokemon.js', () => {
     expect(pokemonImage.src).toContain('https://cdn.bulbagarden.net/upload/2/2c/Spr_5b_148.png');
   });
 
-  it('has a link that redirects to a page with details', () => {
+  it('verifies if it has a link that redirects to a page with details', () => {
     const { getByText } = renderWithRouter(
       <Pokemon pokemon={ pokemons[8] } isFavorite />,
     );
@@ -64,7 +64,7 @@ describe('Pokemon.js', () => {
     expect(pathname).toBe('/pokemons/148');
   });
 
-  it('shows favorite icon when pokemon is favorite', () => {
+  it('verifies the favorite icon when pokemon is favorite', () => {
     const { getByAltText } = renderWithRouter(
       <Pokemon pokemon={ pokemons[8] } isFavorite />,
     );
