ff6f0a9163a5181304f99b878e47e87b28b04fe1
FelipeBelarmino <70665848+FelipeBelarmino@users.noreply.github.com>
2021-03-08 18:48:19 -0300

    Teste

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,43 +1,58 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
+import { Pokemon } from '../components';
 import App from '../App';
-import Pokemon from '../components/Pokemon';
 import pokemons from '../data';
 
-describe('É renderizado um card com as informações de determinado pokémon', () => {
-  test('O tipo correto do pokémon deve ser mostrado na tela', () => {
-    const { getByText } = renderWithRouter(<App />);
-    let name = getByText(/Pikachu/i);
-    let gender = screen.getByTestId('pokemonType');
-    expect(name).toBeInTheDocument();
-    expect(gender).toHaveTextContent('Electric');
-    const next = getByText(/Próximo pokémon/i);
-    userEvent.click(next);
-    name = getByText(/Charmander/i);
-    gender = screen.getByTestId('pokemonType');
-    expect(name).toBeInTheDocument();
-    expect(gender).toHaveTextContent('Fire');
+const POKEMON = pokemons[0];
+
+describe('Pokemon card', () => {
+  it('Should render a pokemon card', () => {
+    const { getByText, getByRole, getByAltText } = renderWithRouter(<Pokemon
+      pokemon={ POKEMON }
+      isFavorite={ false }
+    />);
+
+    const imageURL = 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+
+    expect(getByText(/pikachu/i)).toBeInTheDocument();
+    expect(getByText(/electric/i)).toBeInTheDocument();
+    expect(getByText(/average weight: 6.0 kg/i)).toBeInTheDocument();
+    expect(getByRole('img').src).toBe(imageURL);
+    expect(getByAltText('Pikachu sprite')).toBeInTheDocument();
   });
 
-  test('é feito o redirecionamento da aplicação para a página de detalhes', () => {
-    const { getByRole, getByText, history } = renderWithRouter(<App />);
-    const moreDetails = getByRole('link', { name: /more details/i });
-    userEvent.click(moreDetails);
-    const { pathname } = history.location;
-    expect(getByText('Pikachu Details')).toBeInTheDocument();
-    expect(pathname).toBe('/pokemons/25');
-  });
-
-  test('Teste se existe um ícone de estrela nos Pokémons favoritados', () => {
+  it('Should render a link to pokemon details', () => {
     const { getByRole } = renderWithRouter(<Pokemon
-      pokemon={ pokemons[0] }
+      pokemon={ POKEMON }
+      isFavorite={ false }
+    />);
+
+    const detailsLink = getByRole('link', { name: /more details/i });
+    expect(detailsLink).toBeInTheDocument();
+    expect(detailsLink.href).toContain('/pokemons/25');
+  });
+
+  it('Should details link redirect to pokemon details', () => {
+    const { getByRole, getByText, history } = renderWithRouter(<App />);
+
+    const detailsLink = getByRole('link', { name: /more details/i });
+    userEvent.click(detailsLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+    expect(getByText('Pikachu Details')).toBeInTheDocument();
+  });
+
+  it('Should render a star icon for favorited pokemon', () => {
+    const { getByRole } = renderWithRouter(<Pokemon
+      pokemon={ POKEMON }
       isFavorite
     />);
 
-    const favorite = getByRole('img', { name: /pikachu is marked as favorite/i });
-    expect(favorite).toBeInTheDocument();
-    expect(favorite).toHaveAttribute('src', '/star-icon.svg');
+    const favIcon = getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(favIcon).toBeInTheDocument();
+    expect(favIcon.src).toContain('/star-icon.svg');
   });
 });
