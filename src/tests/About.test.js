61ccb36720efac6897b43aef46fd10b4d6049a85
Blucilha <blucilha.nascimento@gmail.com>
2021-03-09 20:06:10 -0300

    requisitos 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,58 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+
+test('Verifica se renderiza informação sobre `Pokédex`', () => {
+
+  const { getByText, history } = renderWithRouter(<App />);
+  const linkAbout = getByText('About');
+
+  fireEvent.click(linkAbout);
+  const { pathname } = history.location;
+
+  expect(pathname).toBe('/about');
+});
+
+test('Verifica se a página contém um heading h2 com o texto "About Pokédex"', () => {
+
+  const { getByText, getByRole } = renderWithRouter(<App />);
+  const linkAbout = getByText('About');
+  
+  fireEvent.click(linkAbout);
+  
+  const titulo = getByRole('heading', {
+    level: 2,
+    name: 'About Pokédex',
+  });
+
+  expect(titulo).toBeInTheDocument();
+});
+
+test('Verifica se a página contém 2 parágrafos', () => {
+
+  const { getByText } = renderWithRouter(<App />);
+  const linkAbout = getByText('About');
+    
+  fireEvent.click(linkAbout);
+    
+  const paragrafoUm = getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémons');
+  
+  const paragrafoDois = getByText('One can filter Pokémons by type, and see more details for each one of them')
+  expect(paragrafoUm).toBeInTheDocument();
+  expect(paragrafoDois).toBeInTheDocument();
+  });
+
+test('Verifica se a página contém uma imagem', () => {
+
+  const { getByText, getByRole } = renderWithRouter(<App />);
+  const linkAbout = getByText('About');
+    
+  fireEvent.click(linkAbout);
+    
+  const image = getByRole('img', {
+    name: 'Pokédex',
+  });
+  
+    expect(image.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,69 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
-    <MemoryRouter>
+    <MemoryRouter initialEntries={ ['/'] }>
       <App />
     </MemoryRouter>,
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('Verifica se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+    );
+  const linkHome = getByText('Home');
+  const linkAbout = getByText('About');
+  const linkFavorite = getByText('Favorite Pokémons');
+
+  expect(linkHome).toBeInTheDocument();
+  expect(linkAbout).toBeInTheDocument();
+  expect(linkFavorite).toBeInTheDocument();
+});
+
+test('Verifica se a aplicação é redirecionada para a página do link "Home"', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const linkHome = getByText('Home');
+
+  fireEvent.click(linkHome);
+  const { pathname } = history.location;
+
+  expect(pathname).toBe('/');
+});
+
+test('Verifica se a aplicação é redirecionada para a página do link "About"', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const linkAbout = getByText('About');
+
+  fireEvent.click(linkAbout);
+  const { pathname } = history.location;
+
+  expect(pathname).toBe('/about');
+});
+
+test('Verifica se a aplicação é redirecionada para a página do link "Favorite pokémons"', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const linkFavorite = getByText('Favorite Pokémons');
+
+  fireEvent.click(linkFavorite);
+  const { pathname } = history.location;
+
+  expect(pathname).toBe('/favorites');
+});
+
+test('Verifica se a aplicação é redirecionada para a página do link "NotFound"', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/page-not-found');
+  const NotFound = getByText('Page requested not found')
+  
+
+  expect(NotFound).toBeInTheDocument();
 });
