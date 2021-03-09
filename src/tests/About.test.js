d55ddc199b92b9e7a96dd01d6142644890e2997c
Maycon Felipe <mayconfelipe.dev@gmail.com>
2021-03-09 10:54:32 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Testa componente <About />', () => {
+  it('Testa a renderização da página About', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const textOnScreen = getByText(/About Pokédex/i);
+    expect(textOnScreen).toBeInTheDocument();
+  });
+  it('Teste que analiza se o componente apresenta dois parágrafos', () => {
+    const { container } = renderWithRouter(<About />);
+    const paragrafos = container.querySelectorAll('p');
+    expect(paragrafos).toHaveLength(2);
+  });
+  it('Testa que analiza a renderização da imagem na Pokedex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const img = getByRole('img');
+    const imageURL = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(img.src).toBe(imageURL);
+  });
+});
