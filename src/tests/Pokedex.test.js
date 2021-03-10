7453bb1859c847718163a4cce3bcfab2d2c93778
enicio <jessandro42@gmail.com>
2021-03-09 23:03:23 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,58 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { createBrowserHistory } from 'history';
+import { render } from '@testing-library/react';
+import App from '../App';
+import pokemons from '../data';
+
+const renderPath = (path) => {
+  const history = createBrowserHistory();
+  history.push(path);
+  const { ...resources } = render(
+    <Router history={ history }>
+      <App />
+    </Router>,
+  );
+  return { ...resources };
+};
+
+test('renders a heading with the text Encountered pokémons', () => {
+  const { getByText } = renderPath('/');
+  const heading = getByText(/Encountered pokémons/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test('renders a button with the text Próximo pokémon', () => {
+  const { getByText } = renderPath('/');
+  const buttonNextPkemon = getByText(/Próximo pokémon/i);
+  expect(buttonNextPkemon).toBeInTheDocument();
+});
+
+test('renders a button with the text All', () => {
+  const { getByText } = renderPath('/');
+  const buttonAll = getByText(/All/i);
+  expect(buttonAll).toBeInTheDocument();
+});
+
+test('test if is created dynamically a filter for each type of pokemon', () => {
+  const { getAllByTestId, getByTestId } = renderPath('/');
+  const typeButtons = getAllByTestId('pokemon-type-button');
+
+  const pokemonsForTest = pokemons.filter((cada) => cada.name !== 'Mew')
+    .filter(((cada) => cada.name !== 'Rapidash'));
+
+  typeButtons.forEach((button, index) => {
+    const pokemonType = button.textContent;
+    userEvent.click(button);
+    expect(getByTestId('pokemon-name').textContent).toBe(pokemonsForTest[index].name);
+    expect(getByTestId('pokemonType').textContent).toBe(pokemonType);
+  });
+});
+
+test('test if there is a button who reset filter', () => {
+  const { getByText, getByTestId } = renderPath('/');
+  const buttonAll = getByText('All');
+  userEvent.click(buttonAll);
+  expect(getByTestId('pokemon-name').textContent).toBe(pokemons[0].name);
+});
