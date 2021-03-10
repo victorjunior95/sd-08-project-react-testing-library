af220cd4f4c2641261379db1f0950a8297f43bb1
Marcela <marcela9908@gmail.com>
2021-03-09 21:20:31 -0300

    Teste o componente <About.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testes no componente About', () => {
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    const { getByText } = render(<About />);
+    expect(getByText(/About Pokédex/i)).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByRole } = render(<About />);
+    const titulo = getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+    expect(titulo).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { container } = renderWithRouter(<About />);
+    const paragrafos = container.querySelectorAll('p');
+    expect(paragrafos.length).toBe(2);
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    const { getByRole } = render(<About />);
+    const img = getByRole('img');
+    expect(img).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
