b0c0e6372ec5d23be02a7bec26c4b38c4a081bb7
Alana Monteiro <lanaah.15@gmail.com>
2021-03-10 16:16:47 -0300

    teste 3

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,18 +1,18 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react'
+import React from 'react';
+import { render, screen } from '@testing-library/react'
 
-// import About from '../components/About';
+import About from '../components/About';
 
 
-// test('renders a page with a pokemon details', () => {
-//     render(<About />);
+test('renders a page with a pokemon details', () => {
+    render(<About />);
 
-//     const pageTitle = screen.getByRole('heading', {
-//         level: 2,
-//         name: /About Pokédex/i,
-//     });
-//     const paragraphs = screen.getAllByRole('article');
-//     expect(pageTitle).toBeInTheDocument();
-//     ;
-//     expect(paragraphs.length).toBe(2);
-// });
+    const pageTitle = screen.getByRole('heading', {
+        level: 2,
+        name: /About Pokédex/i,
+    });
+    const paragraphs = screen.getAllByRole('article');
+    expect(pageTitle).toBeInTheDocument();
+    ;
+    expect(paragraphs.length).toBe(2);
+});
