06a7ee78d8381f65849b2a983dce0fcaf38be6d5
Alana Monteiro <lanaah.15@gmail.com>
2021-03-09 21:13:09 -0300

    primeiros testes

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+// import React from 'react';
+// import { render, screen } from '@testing-library/react'
+
+// import About from '../components/About';
+
+
+// test('renders a page with a pokemon details', () => {
+//     render(<About />);
+
+//     const pageTitle = screen.getByRole('heading', {
+//         level: 2,
+//         name: /About Pokédex/i,
+//     });
+//     const paragraphs = screen.getAllByRole('article');
+//     expect(pageTitle).toBeInTheDocument();
+//     ;
+//     expect(paragraphs.length).toBe(2);
+// });
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,17 +1,79 @@
-import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
-import App from '../App';
+import React from "react";
+import { Router } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { createMemoryHistory } from "history";
+import App from "../App";
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
+test("the navigation links are available", () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
       <App />
-    </MemoryRouter>,
+    </Router>
   );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
- 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  const homeLink = screen.getByRole("link", {
+    name: /home/i,
+  });
+  const aboutLink = screen.getByRole("link", {
+    name: /about/i,
+  });
+  const favoritePokemonsLink = screen.getByRole("link", {
+    name: /favorite pokémons/i,
   });
 
+  expect(homeLink).toBeInTheDocument();
+  expect(aboutLink).toBeInTheDocument();
+  expect(favoritePokemonsLink).toBeInTheDocument();
+});
+
+test("the home link redirects to the right path", () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+
+  const homeLink = screen.getByRole("link", {
+    name: /home/i,
+  });
+  
+  userEvent.click(homeLink);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+});
+
+test("the About link redirects to the right path", () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+
+  const aboutLink = screen.getByRole("link", {
+    name: /about/i,
+  });
+  
+  userEvent.click(aboutLink);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+});
+
+test("the Favorite Pokémons link redirects to the right path", () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+
+  const favoritePokemonsLink = screen.getByRole("link", {
+    name: /favorite pokémons/i,
+  });
+  
+  userEvent.click(favoritePokemonsLink);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+});
