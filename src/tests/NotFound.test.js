f51e00a9ea57b0c2bae8e6a2c732fa7026d3858c
Alana Monteiro <lanaah.15@gmail.com>
2021-03-10 18:33:31 -0300

    pokedex

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from "react";
+import NotFound from "../components/NotFound";
+import { render, screen } from "@testing-library/react";
+
+test('the page contains a heading with the text Page requested not found 😭', () => {
+render(<NotFound />)
+const notFoundHeading = screen.getByRole('heading', {
+	level: 2,
+	name: /Page requested not found Crying emoji/i,
+});
+expect(notFoundHeading).toBeInTheDocument();
+});
+
+test('the page show the not found image', () => {
+	render(<NotFound />);
+	const notFoundImage = screen.getByAltText('Pikachu crying because the page requested was not found').src;
+	expect(notFoundImage).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif')
+
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,46 @@
-test('', () => {});
+import React from "react";
+import { Router } from "react-router-dom";
+
+import App from "../App"
+import userEvent from "@testing-library/user-event";
+import { createMemoryHistory } from "history";
+import { render, screen } from "@testing-library/react";
+
+test('the page contains a heading with the text Encountered pokémons', () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+  const pokedexHeading = screen.getByRole('heading', {
+    level: 2,
+    name: /Encountered pokémons/i
+  });
+  expect(pokedexHeading).toBeInTheDocument();
+});
+
+test('the next pokemon is shown when clicking Próximo pokémon', () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+  const initialPokemon = screen.getByTestId('pokemon-name').textContent;
+  expect(initialPokemon).toBe('Pikachu');
+
+  const nextButton = screen.getByRole('button', {
+    name: /Próximo pokémon/i,
+  });
+  userEvent.click(nextButton);
+
+  const nextPokemon = screen.getByTestId('pokemon-name').textContent;
+  expect(nextPokemon).toBe('Charmander');
+
+  const pokemonCards = screen.getAllByTestId('pokemon-name').length;
+  expect(pokemonCards).toBe(1);
+
+  const pokedexHeading = screen.getByText(/Encountered pokémons/i);
+  expect(pokedexHeading).toBeInTheDocument();
+});
