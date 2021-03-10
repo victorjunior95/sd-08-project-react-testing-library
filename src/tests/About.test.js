fdcebe395617622cb9c3a0126474771b19766d92
Alana Monteiro <lanaah.15@gmail.com>
2021-03-10 17:26:55 -0300

    favorite

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,14 +5,15 @@ import About from '../components/About';
 
 
 test('renders a page with a pokemon details', () => {
-    render(<About />);
+    const { container } = render(<About />);
 
     const pageTitle = screen.getByRole('heading', {
         level: 2,
         name: /About Pokédex/i,
     });
-    const paragraphs = screen.getAllByRole('article');
+    const paragraphs = container.querySelectorAll('p');
+    const pokedexImg = container.querySelector('img');
     expect(pageTitle).toBeInTheDocument();
-    ;
     expect(paragraphs.length).toBe(2);
+    expect(pokedexImg.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import { createMemoryHistory } from "history";
+
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+import { Router } from 'react-router';
+
+
+test('the message No favorite pokemon found is shown if there is no favorite pokemons', () => {
+  render(<FavoritePokemons />)
+  const noPokemons = screen.getByText(/No favorite pokemon found/i);
+  expect(noPokemons).toBeInTheDocument();
+});
+
+test('the message No favorite pokemon found is shown if there is no favorite pokemons', () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+
+  const detailsButton = screen.getByText(/more details/i);
+  userEvent.click(detailsButton);
+
+  const favoriteButton = screen.getByRole('checkbox');
+  userEvent.click(favoriteButton);
+
+  const favoritePage = screen.getByText(/favorite pokémons/i)
+  userEvent.click(favoritePage);
+
+  const favoritedPokemons = screen.getAllByTestId('pokemon-name').length;
+  expect(favoritedPokemons).toBe(1);
+
+});
