eca18d15d95c5ab01cedb96ded06c89575a249fc
Marcela <marcela9908@gmail.com>
2021-03-09 22:52:44 -0300

    Teste o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testes no componente NotFound', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found 😭',
+    () => {
+      const { getByRole } = render(<NotFound />);
+      const titulo = getByRole('heading', {
+        level: 2,
+        name: /Page requested not found/i,
+      });
+      expect(titulo).toBeInTheDocument();
+    });
+
+  it('Teste se a página contém a seguinte imagem', () => {
+    const { getByRole } = render(<NotFound />);
+    const img = getByRole('img');
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
