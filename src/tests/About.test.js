a86a4acb27a8d4ab996f12fa9bd83128da0d522a
victorjunior95 <victorjr.muchacho@gmail.com>
2021-03-10 14:00:53 -0300

    Requisito 4 - c/ uma falha

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,7 +5,7 @@ import About from '../components/About';
 // Será avaliado se o arquivo teste About.test.js contemplam 100% dos casos de uso criados pelo Stryker.
 describe('About.js - the page contains:', () => {
   // Teste se a página contém as informações sobre a Pokédex.
-  test('A heading h2 with text About Pokédex', () => {
+  test('An h2 heading with the text About Pokédex', () => {
     // Teste se a página contém um heading h2 com o texto About Pokédex.
     const { getByRole } = render(<About />);
     const heading = getByRole('heading', {
@@ -15,14 +15,14 @@ describe('About.js - the page contains:', () => {
     expect(heading).toBeInTheDocument();
   });
 
-  test('contains two paragraphs with text about Pokédex', () => {
+  test('Two paragraphs with text about Pokédex', () => {
     const { getByText } = render(<About />);
     // Teste se a página contém dois parágrafos com texto sobre a Pokédex.
     expect(getByText(/This application simulates a Pokédex/i));
     expect(getByText(/One can filter Pokémons by type/i));
   });
 
-  test('contains the image of a Pokédex', () => {
+  test('The image of a Pokédex', () => {
     const { getByAltText } = render(<About />);
     // Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.
     const image = getByAltText('Pokédex');
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+// import { render } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('NotFound.js - the page:', () => {
+  // Teste se página contém um heading h2 com o texto Page requested not found;
+  test('Contains an h2 heading with the text Page request not found', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const text = getByText('Page request not found', {
+      level: 2,
+    });
+    expect(text).toBeInTheDocument();
+  });
+
+  // Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.
+  test(`The page shows the image
+  https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`, () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const img = getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(img).toHaveAttribute('src', URL);
+  });
+});
