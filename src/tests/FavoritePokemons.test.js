71fdb5424467d12a4a02c5a698182eea9f54c8c2
Erick-Marinho <erickgaves@gmail.com>
2021-03-11 16:55:26 -0300

    cumprindo requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
+import { FavoritePokemons } from '../components';
+
+it('ckeck if ´No favorite pokemon fount´ to be in the document', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+  const message = getByText('No favorite pokemon found');
+  expect(message).toBeInTheDocument();
+});
+
+it('check if pokemons showing on the screnn to be the expected', () => {
+  const { getAllByTestId } = renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+  expect(getAllByTestId('pokemon-name').length).toBe(pokemons.length);
+});
+
+it('no cards displayed that are not favored', () => {
+  const { queryAllByTestId } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+  expect(queryAllByTestId('poke-name').length).toBe(0);
+});
