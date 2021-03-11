4019d446eeeec8d6296ed23ed7408311a5d77645
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2021-03-11 18:27:11 -0300

    Requisitos 4 e 5

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import NotFound from '../components/NotFound';
+// import renderWhitRouter from './renderWithRouter';
+
+describe('NotFound.js ', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    render(<NotFound />);
+    const notFoundText = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+  test('Teste se página mostra a imagem:', () => {
+    render(<NotFound />);
+    const notFoundImg = screen.getAllByRole('img');
+    // https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f
+    expect(notFoundImg[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(notFoundImg[1]).toHaveAttribute('alt',
+      'Pikachu crying because the page requested was not found');
+    expect(notFoundImg[1]).toBeInTheDocument();
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,78 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+const next = 'Próximo pokémon';
+const pokeName = 'pokemon-name';
+
+describe('Testa o componente Pokedex', () => {
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const heading = getByRole('heading', {
+      level: 2,
+      name: /Encountered pokémons/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido o próximo Pokémon da lista', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const nextPokemonButton = getByText(next);
+    const pokemonName = screen.getByTestId(pokeName);
+    expect(pokemonName).toHaveTextContent('Pikachu');
+    userEvent.click(nextPokemonButton);
+    expect(pokemonName).toHaveTextContent('Charmander');
+    userEvent.click(nextPokemonButton);
+    expect(pokemonName).toHaveTextContent('Caterpie');
+    userEvent.click(nextPokemonButton);
+    expect(pokemonName).toHaveTextContent('Ekans');
+    userEvent.click(nextPokemonButton);
+    expect(pokemonName).toHaveTextContent('Alakazam');
+    userEvent.click(nextPokemonButton);
+    expect(pokemonName).toHaveTextContent('Mew');
+    userEvent.click(nextPokemonButton);
+    expect(pokemonName).toHaveTextContent('Rapidash');
+    userEvent.click(nextPokemonButton);
+    expect(pokemonName).toHaveTextContent('Snorlax');
+    userEvent.click(nextPokemonButton);
+    expect(pokemonName).toHaveTextContent('Dragonair');
+    userEvent.click(nextPokemonButton);
+    expect(pokemonName).toHaveTextContent('Pikachu');
+  });
+
+  it('Teste se a Pokédex tem os botões de filtro', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const nextPokemonButton = getByText(next);
+    const pokemonName = screen.getByTestId(pokeName);
+    const btnFilter = screen.getAllByTestId('pokemon-type-button');
+    const btnLength = 7;
+    expect(btnFilter.length).toBe(btnLength);
+
+    const btnFilterFire = screen.getByRole('button', {
+      name: /Fire/i,
+    });
+    const btnFilterPsychic = screen.getByRole('button', {
+      name: /Psychic/i,
+    });
+    userEvent.click(btnFilterFire);
+    expect(pokemonName).toHaveTextContent('Charmander');
+    userEvent.click(nextPokemonButton);
+    expect(pokemonName).toHaveTextContent('Rapidash');
+    userEvent.click(btnFilterPsychic);
+    expect(pokemonName).toHaveTextContent('Alakazam');
+    userEvent.click(nextPokemonButton);
+    expect(pokemonName).toHaveTextContent('Mew');
+  });
+
+  it('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const btnAll = getByRole('button', {
+      name: /All/i,
+    });
+    const pokemonName = screen.getByTestId(pokeName);
+    userEvent.click(btnAll);
+    expect(pokemonName).toHaveTextContent('Pikachu');
+  });
+});
