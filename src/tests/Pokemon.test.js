cae514112f500b3e4de186ce8cdde974fd005493
Alana Monteiro <lanaah.15@gmail.com>
2021-03-11 16:52:17 -0300

    pokemon test;

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { getByText, render, screen } from '@testing-library/react';
+import { Pokemon } from '../components';
+import pokemons from '../data'
+
+test('the contains a heading with the text Encountered pokémons', () => {
+  const history = createMemoryHistory();
+  const pokemonData = pokemons;
+  const container = render(
+    <Router history={ history }>
+      <Pokemon pokemon={ pokemonData[0] } isFavorite={ false } />
+    </Router>,
+  );
+
+  const pokemonName = container.getByText(/pikachu/i);
+  const pokemonType = container.getByText(/electric/i);
+  const pokemonWeight = container.getByText(/average weight: 6.0 kg/i);
+  const pokemonImg = container.getByAltText(/pikachu sprite/i);
+  expect(pokemonName).toBeInTheDocument();
+  expect(pokemonType).toBeInTheDocument();
+  expect(pokemonWeight).toBeInTheDocument();
+  expect(pokemonImg).toBeInTheDocument();
+  });
