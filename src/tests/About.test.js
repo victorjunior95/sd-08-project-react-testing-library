f4410cd0b5ff4b5f93bee586c7c09ecb31e3d098
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-03-11 13:34:35 -0300

    About Test

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Tests About.test.js', () => {
+  let renderedScreen;
+  beforeEach(() => {
+    renderedScreen = render(<About />);
+  });
+
+  test('Tests Pokédex Info', () => {
+    [new RegExp('This application simulates a Pokédex, '
+    + 'a digital encyclopedia containing all Pokémons'),
+    new RegExp('One can filter Pokémons by type, '
+    + 'and see more details for each one of them')].forEach((re) => {
+      expect(screen.getByText(re)).toBeInTheDocument();
+    });
+  });
+
+  test('Tests Heading', () => {
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Tests Two Paragraphs', () => {
+    const { container } = renderedScreen;
+    expect(Array.from(container.querySelectorAll('p')).length).toBe(2);
+  });
+});
