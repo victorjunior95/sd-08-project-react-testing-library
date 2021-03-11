40acb3b01ba063832bed33028b10188c5993890d
Erick-Marinho <erickgaves@gmail.com>
2021-03-11 16:58:10 -0300

    cumprindo requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../components';
+
+it('check if the message `Page requested not found` exist in the document', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const message = getByText('Page requested not found');
+  expect(message).toBeInTheDocument();
+});
+
+it('check if the SCR from image to equal', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const img = getByRole('img', {
+    name: 'Pikachu crying because the page requested was not found',
+  });
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
