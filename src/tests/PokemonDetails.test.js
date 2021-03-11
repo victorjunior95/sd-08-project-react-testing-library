7d5b0e3a34055d5f40a566af40fbf7d962547dfc
victorvalim <victor.valim.c@gmail.com>
2021-03-11 19:47:34 -0300

    Requeisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,49 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+// import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+// import Pokemon from '../components/Pokemon';
+import App from '../App';
+
+describe('Pagina inicial', () => {
+  it('As informações detalhadas do Pokémon selecionado são mostradas na tela.', () => {
+    const { history, getByRole, getByText, getByTestId } = renderWithRouter(<App />);
+    const url = history.location.pathname;
+    const link = getByRole('link', {
+      name: /More Details/i,
+    });
+    userEvent.click(link);
+    const pikachuDetails = getByRole('heading', {
+      level: 2,
+      name: /pikachu details/i,
+    });
+    const sumary = getByRole('heading', {
+      level: 2,
+      name: /Summary/i,
+    });
+    const sumaryText = getByText('This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.');
+    expect(pikachuDetails).toBeInTheDocument();
+    expect(sumary).toBeInTheDocument();
+    expect(sumaryText).toBeInTheDocument();
+  });
+  it('Existe na página uma seção com os mapas contendo as localizações do pokémon', () => {
+    const { history, getByRole, getByText, getByTestId, getAllByAltText, getAllByRole } = renderWithRouter(<App />);
+    const url = history.location.pathname;
+    const link = getByRole('link', {
+      name: /More Details/i,
+    });
+    userEvent.click(link);
+    const pikachuDetails = getByRole('heading', {
+      level: 2,
+      name: /Game Locations of Pikachu/i,
+    });
+
+    const pikachuLocations = getAllByAltText(/Pikachu location/i);
+    const favorite = getByText(/Pokémon favoritado/i);
+    expect(favorite).toBeInTheDocument();
+    expect(pikachuLocations[1]).toHaveAttribute('alt', 'Pikachu location');
+    expect(pikachuLocations[1]).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+    expect(pikachuDetails).toBeInTheDocument();
+    expect(pikachuLocations.length).toBe(2);
+  });
+});
