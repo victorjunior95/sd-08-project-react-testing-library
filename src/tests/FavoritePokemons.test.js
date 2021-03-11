570ca39d36d173b1772849c0e91f6092cf0ffca6
richardfazeredo <richardcompletetour@gmail.com>
2021-03-11 15:37:47 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('tests the component FavoritePokemons.js', () => {
+  it('shows a message on the screen if there is no favorited pokémons', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+
+    const message = getByText(/No favorite pokemon found/i);
+
+    expect(message).toBeInTheDocument();
+  });
+});
