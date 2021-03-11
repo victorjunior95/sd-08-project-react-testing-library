b8be3795a447033b0a89e152ae08f7f071569196
paulohp7 <ph.pereira.7@gmail.com>
2021-03-11 15:13:39 -0300

    Third Objective

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+  const { getByText } = render(<FavoritePokemons pokemons={ [] } />);
+  const notFoundText = getByText('No favorite pokemon found');
+  expect(notFoundText).toBeInTheDocument();
+});
+
+test('este se é exibido todos os cards de pokémons favoritados', () => {
+  const { getByRole, getByLabelText, getByText } = renderWithRouter(<App />);
+  const detailsButton = getByRole('link', {
+    name: /More details/i,
+  });
+  userEvent.click(detailsButton);
+  const checkButton = getByLabelText('Pokémon favoritado?');
+  userEvent.click(checkButton);
+  const favButton = getByRole('link', {
+    name: /Favorite Pokémons/i,
+  });
+  userEvent.click(favButton);
+  const pokemon = getByText('Pikachu');
+  expect(pokemon).toBeInTheDocument();
+});
+
+test('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado', () => {
+  const { getByRole, getByLabelText, queryByText } = renderWithRouter(<App />);
+  const detailsButton = getByRole('link', {
+    name: /More details/i,
+  });
+  userEvent.click(detailsButton);
+  const checkButton = getByLabelText('Pokémon favoritado?');
+  userEvent.click(checkButton);
+  const favButton = getByRole('link', {
+    name: /Favorite Pokémons/i,
+  });
+  userEvent.click(favButton);
+  const pokemon = queryByText('Ekans');
+  expect(pokemon).toBeNull();
+});
