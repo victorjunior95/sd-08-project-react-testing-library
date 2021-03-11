6d0d4fe4a5f17010f97a656d2bbf7f1627e54869
Maycon Felipe <mayconfelipe.dev@gmail.com>
2021-03-11 17:33:29 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+const gifURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+describe('Testa o componente <NotFound />', () => {
+  it('Testa o componente <NotFound> com o texto "Page requested not found"', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const textOnScreen = getByText(/Page requested not found/i);
+    expect(textOnScreen).toBeInTheDocument();
+  });
+  it('Testa a renderização do Gif de Página não encontrada', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const altTextImg = getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(altTextImg.src).toBe(gifURL);
+  });
+});
