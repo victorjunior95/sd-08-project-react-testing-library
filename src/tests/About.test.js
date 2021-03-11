d3ac2a16995bc3f35272db0c70bcdf94d716387f
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-03-11 13:45:08 -0300

    Remove Regular Expression

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -9,10 +9,10 @@ describe('Tests About.test.js', () => {
   });
 
   test('Tests Pokédex Info', () => {
-    [new RegExp('This application simulates a Pokédex, '
-    + 'a digital encyclopedia containing all Pokémons'),
-    new RegExp('One can filter Pokémons by type, '
-    + 'and see more details for each one of them')].forEach((re) => {
+    ['This application simulates a Pokédex, '
+    + 'a digital encyclopedia containing all Pokémons',
+    'One can filter Pokémons by type, '
+    + 'and see more details for each one of them'].forEach((re) => {
       expect(screen.getByText(re)).toBeInTheDocument();
     });
   });
