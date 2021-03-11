d86ccc0c124264f809e1a7472cd9191b7204a73c
Erick-Marinho <erickgaves@gmail.com>
2021-03-11 16:49:27 -0300

    criando renderWithRouter e cumprindo requisito 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { About } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+it('Check if pockedex infor exist in the document', () => {
+  const { getByRole } = renderWithRouter(<About />);
+  const aboutAll = getByRole('heading', { level: 2 });
+  expect(aboutAll).toBeInTheDocument();
+  expect(aboutAll).toHaveTextContent('About Pokédex');
+});
+
+it('Check if image of pokemon to be in the screnn', () => {
+  const pokedexImage = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  const { getByRole } = renderWithRouter(<About />);
+  const image = getByRole('img', { name: /pokédex/i });
+  expect(image).toBeInTheDocument();
+  expect(image.src).toBe(pokedexImage);
+});
