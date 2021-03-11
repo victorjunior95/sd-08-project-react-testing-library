1fe3285cfe0be8a88fc35aeef9b4bd43738bf108
Marcela <marcela9908@gmail.com>
2021-03-11 00:32:22 -0300

    Teste o componente <Pokemon.js />

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,48 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, getByAltText } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+import Pokemon from '../components/Pokemon';
+
+describe('Teste no Componente Pokémon', () => {
+  it('Teste se é renderizado um card com as informações de determinado pokémon.',
+    () => {
+      const { getByText, getByAltText } = renderWithRouter(
+        <Pokemon pokemon={ pokemons[0] } isFavorite={ false } />,
+      );
+      const nome = getByText(pokemons[0].name);
+      expect(nome).toBeInTheDocument();
+
+      const tipo = getByText(pokemons[0].type);
+      expect(tipo).toBeInTheDocument();
+
+      const peso = getByText('Average weight: 6.0 kg');
+      expect(peso).toBeInTheDocument();
+
+      const img = getByAltText('Pikachu sprite');
+      expect(img).toHaveAttribute('alt', 'Pikachu sprite');
+      expect(img).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    });
+
+  it('Teste se o card do Pokémon indicado na Pokédex contém um link de navegação',
+    () => {
+      const { getByText, history } = renderWithRouter(
+        <Pokemon pokemon={ pokemons[0] } isFavorite={ false } />,
+      );
+      const link = getByText('More details');
+      fireEvent.click(link);
+      const { pathname } = history.location;
+      expect(pathname).toBe(`/pokemons/${pokemons[0].id}`);
+    });
+
+  it('Teste se existe um ícone de estrela nos Pokémons favoritados.', () => {
+    const { getByAltText } = renderWithRouter(
+      <Pokemon pokemon={ pokemons[0] } isFavorite />,
+    );
+    const imgEstrela = getByAltText('Pikachu is marked as favorite');
+    expect(imgEstrela).toHaveAttribute('src', '/star-icon.svg');
+    expect(imgEstrela).toHaveAttribute('alt',
+      `${pokemons[0].name} is marked as favorite`);
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testes no componente Pokémon  Details', () => {
+  it('Teste se as informações detalhadas do Pokémon selecionado são mostradas na tela.',
+    () => {
+      const { getByTestId, getByRole, getByText } = renderWithRouter(<App />);
+      const nomePokemon = getByTestId('pokemon-name');
+      expect(nomePokemon).toBeInTheDocument();
+      const titulo = getByRole('heading', { level: 2, name: 'Summary' });
+      expect(titulo).toBeInTheDocument();
+      const paragrafo = getByText(pokemons[0].summary);
+      expect(paragrafo).toBeInTheDocument();
+    });
+});
