ebf04a5cc76831cd6a0b48743df656314914835c
Andre Sampaio <andresampaiolm@gmail.com>
2021-03-11 15:47:08 -0300

    Tested component FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import renderWithRouter from '../renderWithRouter'; 
-
-import FavoritePokemons from '../components/FavoritePokemons';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
 
 describe('Test component FavoritePokemons', () => {
   it('should have the message "No favorite pokemon found"', () => {
-    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/favorites');
     const noFavoriteFound = getByText('No favorite pokemon found');
     expect(noFavoriteFound).toBeInTheDocument();
+  });
+
+  it('should show the pokemons marked as favorites', () => {
+    const { getByText, getByLabelText } = renderWithRouter(<App />);
+    const btnMoreDetails = getByText('More details');
+    fireEvent.click(btnMoreDetails);
+    const checkboxFavoritePokemon = getByLabelText('Pokémon favoritado?');
+    fireEvent.click(checkboxFavoritePokemon);
+    const linkFavoritePokemons = getByText('Favorite Pokémons');
+    fireEvent.click(linkFavoritePokemons);
+    const favoritesContainer = document.querySelector('.favorite-pokemons');
+    expect(favoritesContainer).toBeInTheDocument();
   });
 });
