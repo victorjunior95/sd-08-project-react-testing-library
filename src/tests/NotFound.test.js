02ec4e8e3124ef7999f0c2bed7b32e2252cafdbf
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2021-03-11 15:20:07 -0300

    Task 4 - NotFound.test.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+
+import { NotFound } from '../components';
+
+describe('NotFound.js', () => {
+  test('should render a heading with the text `Page requested not found`', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    const notFoundHeading = getByRole('heading', {
+      level: 2,
+    });
+
+    expect(notFoundHeading).toHaveTextContent('Page requested not found 😭');
+  });
+
+  test('if page requested notfound should render a img with a pikachu crying gif', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+
+    const cryingPikachu = getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+
+    expect(cryingPikachu.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
