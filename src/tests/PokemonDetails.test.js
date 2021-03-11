7841e8120203de21db178121bfce606f549c66e1
victorvalim <victor.valim.c@gmail.com>
2021-03-11 20:06:26 -0300

    Resolvendo bugs requisito 7, part.1

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -7,8 +7,7 @@ import App from '../App';
 
 describe('Pagina inicial', () => {
   it('As informações detalhadas do Pokémon selecionado são mostradas na tela.', () => {
-    const { history, getByRole, getByText, getByTestId } = renderWithRouter(<App />);
-    const url = history.location.pathname;
+    const { getByRole, getByText } = renderWithRouter(<App />);
     const link = getByRole('link', {
       name: /More Details/i,
     });
@@ -21,29 +20,30 @@ describe('Pagina inicial', () => {
       level: 2,
       name: /Summary/i,
     });
-    const sumaryText = getByText('This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.');
+    const sumaryText = getByText(`This intelligent Pokémon roasts hard berries
+     with electricity to make them tender enough to eat.`);
     expect(pikachuDetails).toBeInTheDocument();
     expect(sumary).toBeInTheDocument();
     expect(sumaryText).toBeInTheDocument();
   });
-  it('Existe na página uma seção com os mapas contendo as localizações do pokémon', () => {
-    const { history, getByRole, getByText, getByTestId, getAllByAltText, getAllByRole } = renderWithRouter(<App />);
-    const url = history.location.pathname;
-    const link = getByRole('link', {
-      name: /More Details/i,
-    });
-    userEvent.click(link);
-    const pikachuDetails = getByRole('heading', {
-      level: 2,
-      name: /Game Locations of Pikachu/i,
-    });
+  it('Existe na página uma seção com os mapas contendo as localizações do pokémon',
+    () => {
+      const { getByRole, getByText, getAllByAltText } = renderWithRouter(<App />);
+      const link = getByRole('link', {
+        name: /More Details/i,
+      });
+      userEvent.click(link);
+      const pikachuDetails = getByRole('heading', {
+        level: 2,
+        name: /Game Locations of Pikachu/i,
+      });
 
-    const pikachuLocations = getAllByAltText(/Pikachu location/i);
-    const favorite = getByText(/Pokémon favoritado/i);
-    expect(favorite).toBeInTheDocument();
-    expect(pikachuLocations[1]).toHaveAttribute('alt', 'Pikachu location');
-    expect(pikachuLocations[1]).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
-    expect(pikachuDetails).toBeInTheDocument();
-    expect(pikachuLocations.length).toBe(2);
-  });
+      const pikachuLocations = getAllByAltText(/Pikachu location/i);
+      const favorite = getByText(/Pokémon favoritado/i);
+      expect(favorite).toBeInTheDocument();
+      expect(pikachuLocations[1]).toHaveAttribute('alt', 'Pikachu location');
+      expect(pikachuLocations[1]).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+      expect(pikachuDetails).toBeInTheDocument();
+      expect(pikachuLocations.length).toBe(2);
+    });
 });
