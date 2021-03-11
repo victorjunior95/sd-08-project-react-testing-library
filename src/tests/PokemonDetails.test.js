049e68d465d8e1824a00bf6036f5ba9c93715a8a
Alana Monteiro <lanaah.15@gmail.com>
2021-03-11 19:31:54 -0300

    eslint

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -3,16 +3,17 @@ import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Pokemon } from '../components';
 import pokemons from '../data';
 import App from '../App';
 
 test('the detailed information oh the pokemon is shown', () => {
   const history = createMemoryHistory();
   const pokemonData = pokemons;
-  const container = render(<Router history={ history }>
-    <App />
-  </Router>);
+  const container = render(
+    <Router history={ history }>
+     <App />
+    </Router>,
+  );
   const detailsButton = container.getByText('More details');
   userEvent.click(detailsButton);
 
@@ -23,7 +24,7 @@ test('the detailed information oh the pokemon is shown', () => {
   const heading2 = container.getByRole('heading', {
     level: 2,
     name: /summary/i,
-  })
+  });
   expect(heading2).toBeInTheDocument();
 
   const paragraph = container.getByText(pokemonData[0].summary);
@@ -33,9 +34,11 @@ test('the detailed information oh the pokemon is shown', () => {
 test('the detail page has a map with the pokemon area details', () => {
   const history = createMemoryHistory();
   const pokemonData = pokemons;
-  const container = render(<Router history={ history }>
-    <App />
-  </Router>);
+  const container = render(
+    <Router history={ history }>
+      <App />
+    </Router>,
+  );
   const detailsButton = container.getByText('More details');
   userEvent.click(detailsButton);
 
@@ -53,9 +56,11 @@ test('the detail page has a map with the pokemon area details', () => {
 test('the pokemon can be favorited at the details page', () => {
   const history = createMemoryHistory();
   const pokemonData = pokemons;
-  const container = render(<Router history={ history }>
-    <App />
-  </Router>);
+  const container = render(
+    <Router history={ history }>
+      <App />
+    </Router>
+  );
   const detailsButton = container.getByText('More details');
   userEvent.click(detailsButton);
   
@@ -65,7 +70,7 @@ test('the pokemon can be favorited at the details page', () => {
   const favLabel = container.getByText(/Pokémon favoritado?/i)
   expect(favLabel).toBeInTheDocument();
   userEvent.click(favButton);
-  
+
   const favPokemon = container.getByAltText('Pikachu is marked as favorite');
   expect(favPokemon).toBeInTheDocument()
   userEvent.click(favButton);
