de28c6743f26d376a9c3b1ace248c839215e497e
richardfazeredo <richardcompletetour@gmail.com>
2021-03-11 15:39:29 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('tests the component NotFound.js', () => {
+  it('contains a heading h2 with specif test', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const heading = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('shows a specific image', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const image = getByRole('img', {
+      name: /Pikachu crying because the page requested was not found/i,
+    });
+
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
