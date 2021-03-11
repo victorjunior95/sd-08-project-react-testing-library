35c6c3cf5d69af6280483778d9f0b10f055a5f23
Lucas Duque <lucas3rios2014@gmail.com>
2021-03-11 18:47:39 -0300

    Lidando com os favoritos

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -12,6 +12,8 @@ test('Testando se as informações sobra a pokedex estão presentes', () => {
 
   // expect(par1).toBeInTheDocument();
   // expect(par2).toBeInTheDocument();
+  // Tirei inspiração para a utilização do container no site:
+  // https://stackoverflow.com/questions/54593369/unable-to-find-an-element-with-the-text-mytext-error-when-using-react-testing
   const { container } = renderWithRouter(<About />);
   const paragrafos = container.querySelectorAll('p');
   const text1 = 'This application simulates a Pokédex, a';
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,54 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+const FavPok = 'Favorite Pokémons';
+const FavPath = '/favorites';
+const Details = 'More details';
+
+test('Testando se a mensagem <No favorite pokemon found> aparece na tela', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const link = getByText(FavPok);
+
+  fireEvent.click(link);
+  const { pathname } = history.location;
+  expect(pathname).toBe(FavPath);
+  expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+});
+
+test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  expect(getByText('Pikachu')).toBeInTheDocument();
+  fireEvent.click(getByText(Details));
+  expect(getByText('Pikachu Details')).toBeInTheDocument();
+  fireEvent.click(getByText('Pokémon favoritado?'));
+
+  fireEvent.click(getByText(FavPok));
+  const { pathname } = history.location;
+  expect(pathname).toBe(FavPath);
+  expect(getByText('Pikachu')).toBeInTheDocument();
+});
+
+test('Teste se *nenhum* card de pokémon é exibido, se ele não estiver favoritado', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const char = 'Charmander';
+
+  expect(getByText('Pikachu')).toBeInTheDocument();
+  fireEvent.click(getByText(Details));
+  expect(getByText('Pikachu Details')).toBeInTheDocument();
+  fireEvent.click(getByText('Pokémon favoritado?'));
+
+  fireEvent.click(getByText('Home'));
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  fireEvent.click(getByText('Próximo pokémon'));
+  expect(getByText(char)).toBeInTheDocument();
+  fireEvent.click(getByText(Details));
+  expect(getByText('Charmander Details')).toBeInTheDocument();
+
+  fireEvent.click(getByText(FavPok));
+  const { pathname } = history.location;
+  expect(pathname).toBe(FavPath);
+  expect(getByText(char)).not.toBeInTheDocument();
+});
