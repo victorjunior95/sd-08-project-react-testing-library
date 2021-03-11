65ed060003d7a755f91bd3d1d3c8f8a543d8b0c8
Alana Monteiro <lanaah.15@gmail.com>
2021-03-11 19:50:35 -0300

    eslint

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -35,12 +35,13 @@ test('the detailed information oh the pokemon is shown', () => {
 test('the detail page has a map with the pokemon area details', () => {
   const history = createMemoryHistory();
   const pokemonData = pokemons;
+  const buttonText = 'More details';
   const container = render(
     <Router history={ history }>
       <App />
     </Router>,
   );
-  const detailsButton = container.getByText('More details');
+  const detailsButton = container.getByText(buttonText);
   userEvent.click(detailsButton);
 
   const heading2 = container.getByRole('heading', {
@@ -56,12 +57,13 @@ test('the detail page has a map with the pokemon area details', () => {
 
 test('the pokemon can be favorited at the details page', () => {
   const history = createMemoryHistory();
+  const buttonText = 'More details';
   const container = render(
     <Router history={ history }>
       <App />
     </Router>,
   );
-  const detailsButton = container.getByText('More details');
+  const detailsButton = container.getByText(buttonText);
   userEvent.click(detailsButton);
 
   const favButton = container.getByRole('checkbox');
