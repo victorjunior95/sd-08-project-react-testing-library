0ce31edfcebb244dbf1133b89b3064c639509186
Lucas Duque <lucas3rios2014@gmail.com>
2021-03-11 17:26:19 -0300

    Completando os testes do componente About, parte 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,15 +3,25 @@ import renderWithRouter from '../renderWithRouter';
 import About from '../components/About';
 
 test('Testando se as informações sobra a pokedex estão presentes', () => {
-  const { getByText } = renderWithRouter(<About />);
-  const par1 = getByText('This application simulates a Pokédex, a '
-  + 'digital encyclopedia containing all Pokémons');
-  const par2 = getByText(
-    'One can filter Pokémons by type, and see more details for each one of them',
-  );
+  // const { getByText } = renderWithRouter(<About />);
+  // const text = 'This';
+  // const par1 = getByText(text);
+  // const par2 = getByText(
+  //   'One can filter Pokémons by type, and see more details for each one of them',
+  // );
 
-  expect(par1).toBeInTheDocument();
-  expect(par2).toBeInTheDocument();
+  // expect(par1).toBeInTheDocument();
+  // expect(par2).toBeInTheDocument();
+  const { container } = renderWithRouter(<About />);
+  const paragrafos = container.querySelectorAll('p');
+  const text1 = 'This application simulates a Pokédex, a';
+  const text2 = 'digital encyclopedia containing all Pokémons';
+
+  expect(paragrafos.length).toBe(2);
+  expect(paragrafos[0].innerHTML)
+    .toBe(`${text1} ${text2}`);
+  expect(paragrafos[1].innerHTML)
+    .toBe('One can filter Pokémons by type, and see more details for each one of them');
 });
 
 test('Testando se o cabeçalho da pagina está presente', () => {
