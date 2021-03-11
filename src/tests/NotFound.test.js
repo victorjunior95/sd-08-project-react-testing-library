6191527d9a088d4342e820d31b121013cde19fb1
Alana Monteiro <lanaah.15@gmail.com>
2021-03-11 16:06:49 -0300

    teste 1

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -3,18 +3,18 @@ import { render, screen } from '@testing-library/react';
 import NotFound from '../components/NotFound';
 
 test('the page contains a heading with the text Page requested not found 😭', () => {
-	render(<NotFound />);
-	const notFoundHeading = screen.getByRole('heading', {
-		level: 2,
-		name: /Page requested not found Crying emoji/i,
-	});
-	expect(notFoundHeading).toBeInTheDocument();
-	});
+  render(<NotFound />);
+  const notFoundHeading = screen.getByRole('heading', {
+    level: 2,
+    name: /Page requested not found Crying emoji/i,
+  });
+  expect(notFoundHeading).toBeInTheDocument();
+  });
 
 test('the page show the not found image', () => {
-	render(<NotFound />);
-	const notFoundImage = screen.getAllByRole('img', {
-		src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
-	})[1];
-	expect(notFoundImage).toBeInTheDocument();
+  render(<NotFound />);
+  const notFoundImage = screen.getAllByRole('img', {
+    src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+  })[1];
+  expect(notFoundImage).toBeInTheDocument();
 });
