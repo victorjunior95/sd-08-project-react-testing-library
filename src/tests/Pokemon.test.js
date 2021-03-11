dfbb5353fe00c49d08972a31af28d16f62f0538a
Alana Monteiro <lanaah.15@gmail.com>
2021-03-11 16:59:51 -0300

    pokemons test ok

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -4,6 +4,7 @@ import { Router } from 'react-router-dom';
 import { getByText, render, screen } from '@testing-library/react';
 import { Pokemon } from '../components';
 import pokemons from '../data'
+import userEvent from '@testing-library/user-event';
 
 test('the contains a heading with the text Encountered pokémons', () => {
   const history = createMemoryHistory();
@@ -23,3 +24,21 @@ test('the contains a heading with the text Encountered pokémons', () => {
   expect(pokemonWeight).toBeInTheDocument();
   expect(pokemonImg).toBeInTheDocument();
   });
+
+test('the card has a link to the pokemon details', () => {
+	const history = createMemoryHistory();
+  const pokemonData = pokemons;
+  const container = render(
+    <Router history={ history }>
+      <Pokemon pokemon={ pokemonData[0] } isFavorite={ false } />
+    </Router>,
+  );
+
+	const navLink = container.getByRole('link', {
+		name: /more details/i,
+	});
+	userEvent.click(navLink);
+	const { pathname } = history.location;
+  expect(pathname).toBe('/pokemons/25');
+
+});
