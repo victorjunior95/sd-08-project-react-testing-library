84233876d22756a13705d6fd870074f60853a223
Andre Sampaio <andresampaiolm@gmail.com>
2021-03-11 13:19:44 -0300

    tested component About, request 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Test component About', () => {
+  it('should have informations about the Pokedex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const heading = getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent('About Pokédex');
+  });
+
+  it('should have two paragraphs', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const pOne = getByText(
+      'This application simulates a Pokédex, a '
+      + 'digital encyclopedia containing all Pokémons',
+    );
+    const pTwo = getByText(
+      'One can filter Pokémons by type, '
+      + 'and see more details for each one of them'
+    );
+    expect(pOne).toBeInTheDocument();
+    expect(pTwo).toBeInTheDocument();
+  });
+
+  it('should have an image of a Pokedex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const image = getByRole('img');
+    expect(image).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
