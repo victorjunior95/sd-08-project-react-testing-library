9a0f18ceeaa2035126b7630fd403e22383bb7e0e
paulohp7 <ph.pereira.7@gmail.com>
2021-03-11 13:12:11 -0300

    First Objective

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,82 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+test(
+  'Pokédex é renderizada ao carregar a aplicação no caminho de URL /.',
+  () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/');
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  },
+);
+
+test('Topo da aplicação contém um conjunto fixo de links de navegação.', () => {
+  const { getByRole } = renderWithRouter(<App />);
+
+  const homeButton = getByRole('link', {
+    name: /Home/i,
+  });
+  const aboutButton = getByRole('link', {
+    name: /About/i,
+  });
+  const favButton = getByRole('link', {
+    name: /Favorite Pokémons/i,
+  });
+
+  expect(homeButton).toBeInTheDocument();
+  expect(aboutButton).toBeInTheDocument();
+  expect(favButton).toBeInTheDocument();
 });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+test(
+  'redirecionada para a página inicial',
+  () => {
+    const { history, getByRole } = renderWithRouter(<App />);
+    const homeButton = getByRole('link', {
+      name: /Home/i,
+    });
+    userEvent.click(homeButton);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  },
+);
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
-});
+test(
+  'redirecionada para a página about',
+  () => {
+    const { history, getByRole } = renderWithRouter(<App />);
+    const aboutButton = getByRole('link', {
+      name: /About/i,
+    });
+    userEvent.click(aboutButton);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  },
+);
+
+test(
+  'redirecionada para a página favoritos',
+  () => {
+    const { history, getByRole } = renderWithRouter(<App />);
+    const favButton = getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    userEvent.click(favButton);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  },
+);
+
+test(
+  'redirecionada para a página de erro',
+  () => {
+    const { getByText } = renderWithRouter(<App />, { route: '/some/bad/route' });
+    const notFoundText = getByText('Page requested not found');
+    expect(notFoundText).toBeInTheDocument();
+  },
+);
