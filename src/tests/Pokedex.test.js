edb6cc48d6ea5cc00d0aded3dde29102ea0fde99
Andre Sampaio <andresampaiolm@gmail.com>
2021-03-12 00:06:10 -0300

    Tests of component Pokedex refactored

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,78 +1,72 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-describe('Test component Pokedex', () => {
+import pokemons from '../data';
+
+describe('Test component Pokedex, refactoring', () => {
   const TESTID_NEXT_POKEMON = 'next-pokemon';
   const TESTID_POKEMON_NAME = 'pokemon-name';
   const TESTID_POKEMON_TYPE_BUTTON = 'pokemon-type-button';
 
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
   it('should have an h2 tag with the text "Encoutered pokemons"', () => {
-    const { getByRole } = renderWithRouter(<App />);
-    const heading = getByRole('heading', { level: 2 });
+    const heading = screen.getByRole('heading', { level: 2 });
     expect(heading).toHaveTextContent('Encountered pokémons');
   });
 
-  it('should show the next pokemon after click the button Proximo Pokemon', () => {
-    const { getByTestId } = renderWithRouter(<App />);
-    const btnNextPokemon = getByTestId(TESTID_NEXT_POKEMON);
+  it('should show the next pokemon on the list', () => {
+    const NUMBER_OF_EXPECTATIONS = 11;
+    expect.assertions(NUMBER_OF_EXPECTATIONS);
+    const btnNextPokemon = screen.getByTestId(TESTID_NEXT_POKEMON);
     expect(btnNextPokemon).toHaveTextContent('Próximo pokémon');
-
-    expect(getByTestId(TESTID_POKEMON_NAME)).toHaveTextContent('Pikachu');
-
-    fireEvent.click(btnNextPokemon);
-    expect(getByTestId(TESTID_POKEMON_NAME)).toHaveTextContent('Charmander');
-
-    fireEvent.click(btnNextPokemon);
-    expect(getByTestId(TESTID_POKEMON_NAME)).toHaveTextContent('Caterpie');
-
-    if (getByTestId(TESTID_POKEMON_NAME).innerHTML === 'Dragonair') {
+    const currentPokemon = screen.getByTestId(TESTID_POKEMON_NAME);
+    for (let index = 0; index <= pokemons.length; index += 1) {
+      const pokemonIndex = index < pokemons.length
+        ? index
+        : index - pokemons.length;
+      expect(currentPokemon.innerHTML).toBe(pokemons[pokemonIndex].name);
       fireEvent.click(btnNextPokemon);
-      expect(getByTestId(TESTID_POKEMON_NAME)).toHaveTextContent('Pikachu');
     }
   });
 
-  it('should only show one pokemon at a time', () => {
-    const { getAllByTestId } = renderWithRouter(<App />);
-    const pokemonArray = getAllByTestId(TESTID_POKEMON_NAME);
+  it('should only have one pokemon at a time', () => {
+    const pokemonArray = screen.getAllByTestId(TESTID_POKEMON_NAME);
     expect(pokemonArray.length).toBe(1);
   });
 
-  it('should have filter buttons', () => {
-    const { getAllByTestId, getByTestId } = renderWithRouter(<App />);
-    const filterButtonArray = getAllByTestId(TESTID_POKEMON_TYPE_BUTTON);
-    fireEvent.click(filterButtonArray[3]);
-    const pokemonType = getByTestId('pokemonType');
-    expect(filterButtonArray[3]).toHaveTextContent(pokemonType.innerHTML);
+  it('should have filter buttons for each pokemon type', () => {
+    const typeFiltersElements = screen.getAllByTestId(TESTID_POKEMON_TYPE_BUTTON);
+    const typeFiltersText = typeFiltersElements.map((filter) => filter.innerHTML);
+    const typesWithDuplicates = pokemons.map((pokemon) => pokemon.type);
+    const types = Array.from(new Set(typesWithDuplicates));
+    expect.assertions(types.length);
+    types.forEach((type) => {
+      expect(typeFiltersText.includes(type)).toBe(true);
+    });
   });
 
-  it('should have a button to reset the filters', () => {
-    const { getByTestId, getByText } = renderWithRouter(<App />);
-    const btnResetFilter = getByText('All');
-    expect(btnResetFilter).toHaveTextContent('All');
-    const btnNextPokemon = getByTestId(TESTID_NEXT_POKEMON);
-    const pokemon1 = getByTestId('pokemonType').innerHTML;
-    fireEvent.click(btnNextPokemon);
-    const pokemon2 = getByTestId('pokemonType').innerHTML;
-    expect(pokemon1).not.toBe(pokemon2);
-  });
-
-  it('should have one filter button for each pokemon type', () => {
-    const TYPES = ['Fire', 'Psychic', 'Electric', 'Bug', 'Poison', 'Dragon', 'Normal']
-      .sort();
-    const { getAllByTestId } = renderWithRouter(<App />);
-    const filterButtonsElements = getAllByTestId(TESTID_POKEMON_TYPE_BUTTON);
-    const filterButtonsTypesSorted = filterButtonsElements
-      .map((type) => type.innerHTML)
-      .sort();
-    expect(filterButtonsTypesSorted).toEqual(TYPES);
+  it('should have a button to reset the filter', () => {
+    const POKEMONS_PLUS_ALL_CHECK = 10;
+    expect.assertions(POKEMONS_PLUS_ALL_CHECK);
+    const resetFilter = screen.getByText('All');
+    expect(resetFilter).toBeInTheDocument();
+    fireEvent.click(resetFilter);
+    const currentPokemon = screen.getByTestId(TESTID_POKEMON_NAME);
+    const btnNextPokemon = screen.getByTestId(TESTID_NEXT_POKEMON);
+    pokemons.forEach((pokemon) => {
+      expect(currentPokemon).toHaveTextContent(pokemon.name);
+      fireEvent.click(btnNextPokemon);
+    });
   });
 
   it('should disable the button next-pokemon when there\'s only one', () => {
-    const { getByTestId, getAllByTestId } = renderWithRouter(<App />);
-    const btnNextPokemon = getByTestId(TESTID_NEXT_POKEMON);
-    const filterButtonArray = getAllByTestId(TESTID_POKEMON_TYPE_BUTTON);
+    const btnNextPokemon = screen.getByTestId(TESTID_NEXT_POKEMON);
+    const filterButtonArray = screen.getAllByTestId(TESTID_POKEMON_TYPE_BUTTON);
     fireEvent.click(filterButtonArray[2]);
     expect(btnNextPokemon).toHaveAttribute('disabled');
   });
