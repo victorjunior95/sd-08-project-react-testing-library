3cb7770010f8e3dc165448b1cda83e43abc50d1e
richardfazeredo <richardcompletetour@gmail.com>
2021-03-12 13:51:25 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -4,8 +4,15 @@ import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 import pokemons from '../data';
 
-describe('Teste o componente <Pokedex.js />', () => {
-  it('Teste se página contém um heading', () => {
+const nextPokemon = 'Próximo pokémon';
+const filteredPokemons = (array, next) => array.forEach((Pokemon) => {
+  const element = getByText(Pokemon.name);
+  expect(element).toBeInTheDocument();
+  userEvent.click(next);
+});
+
+describe('tests the component Pokedex', () => {
+  it('should render a h2 title with text Encountered pokémons', () => {
     const { getByRole } = renderWithRouter(<App />);
     const paragraph = getByRole('heading', {
       level: 2,
@@ -13,33 +20,25 @@ describe('Teste o componente <Pokedex.js />', () => {
     });
     expect(paragraph).toBeInTheDocument();
   });
-  it('o próximo Pokémon da lista quando o botão Próximo pokémon é clicado.', () => {
+
+  it('should render the next pokémon by clicking Próximo pokémon button', () => {
     const { getByRole, getByText } = renderWithRouter(<App />);
-    const button = getByRole('button', {
-      name: 'Próximo pokémon',
+    const next = getByRole('button', {
+      name: nextPokemon,
     });
-    expect(button).toBeInTheDocument();
-    userEvent.click(button);
-    const Charmander = getByText(/Charmander/i);
-    expect(Charmander).toBeInTheDocument();
-  });
-  it('o próximo Pokémon da lista quando o botão Próximo pokémon é clicado2', () => {
-    const { getByRole, getByText } = renderWithRouter(<App />);
-    const button = getByRole('button', {
-      name: 'Próximo pokémon',
-    });
-    expect(button).toBeInTheDocument();
-    const clickerButton = () => userEvent.click(button);
+    expect(next).toBeInTheDocument();
+    const clickerButton = () => userEvent.click(next);
     for (let i = 0; i < pokemons.length; i += 1) {
       clickerButton();
     }
     const Pikachu = getByText(/Pikachu/i);
     expect(Pikachu).toBeInTheDocument();
   });
-  it('Teste se é mostrado apenas um Pokémon por vez.', () => {
+
+  it('should render only one pokemon by time.', () => {
     const { getAllByRole, getByRole } = renderWithRouter(<App />);
     const button = getByRole('button', {
-      name: 'Próximo pokémon',
+      name: nextPokemon,
     });
     expect(button).toBeInTheDocument();
     const clickerButton = () => userEvent.click(button);
@@ -47,33 +46,55 @@ describe('Teste o componente <Pokedex.js />', () => {
       clickerButton();
       const pokemon = getAllByRole('img');
       expect(pokemon).toHaveLength(1);
-    //   expect(pokemon).toBeInTheDocument();
     }
   });
 
-  it('should', () => {
-    const { getByRole, getByText } = renderWithRouter(<App />);
+  it('must have filter buttons', () => {
+    const { getByRole } = renderWithRouter(<App />);
     const button = getByRole('button', {
-      name: 'Psychic'
+      name: 'Psychic',
     });
 
     userEvent.click(button);
-        
-    const filteredPokemons = pokemons
+
+    const filteringPokemons = pokemons
       .filter(
-        ({type}) => type === 'Psychic'
+        ({ type }) => type === 'Psychic',
       );
 
-    const nextPokemon = getByRole('button', {
-      name: 'Próximo pokémon'
+    const next = getByRole('button', {
+      name: nextPokemon,
     });
 
     expect.assertions(filteredPokemons.length);
-    
-    filteredPokemons.forEach(Pokemon => {
-      const element = getByText(Pokemon.name);
-      expect(element).toBeInTheDocument();
-      userEvent.click(nextPokemon);
+
+    filteredPokemons(filteringPokemons, next);
+  });
+
+  it('should render all pokemons of same type of the clicked button', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const button = getByRole('button', {
+      name: 'All',
     });
+
+    userEvent.click(button);
+    expect(button).toBeInTheDocument();
+
+    const next = getByRole('button', {
+      name: 'Próximo pokémon',
+    });
+
+    expect.assertions(pokemons.length + 1);
+
+    filteredPokemons(pokemons, next);
+  });
+  it('should render a button for each type of pokemons without repetition', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+
+    const pokemonTypes = pokemons.map(({ type }) => type);
+    const pokemonTypesUniques = Array.from(new Set(pokemonTypes));
+    const buttonPokemonTypes = getAllByTestId('pokemon-type-button');
+
+    expect(buttonPokemonTypes).toHaveLength(pokemonTypesUniques.length);
   });
 });
