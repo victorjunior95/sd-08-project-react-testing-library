d3ac3d5c8161edcf30f82aebf9e7ce2276d89fa5
v-icto-r <victor130496@gmail.com>
2021-03-11 21:16:53 -0300

    Primeiro requisito

---
+++ src/renderWithRouter.js
@@ -0,0 +1,14 @@
+//peguei de: https://app.betrybe.com/course/front-end/react/tests/rtl-react-router/conteudos/testando-react-router?use_case=side_bar
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{ component }</Router>), history,
+  });
+};
+
+export default renderWithRouter;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,23 +1,86 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+describe('Teste o componente <App.js />', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    // O primeiro link deve possuir o texto Home.
+
+    const linkHome = getByText('Home');
+
+    // O segundo link deve possuir o texto About.
+
+    const linkAbout = getByText('About');
+
+    // O terceiro link deve possuir o texto Favorite Pokémons. */
+
+    const linkFavorite = getByText('Favorite Pokémons');
+
+    expect(linkHome).toBeInTheDocument();
+
+    expect(linkAbout).toBeInTheDocument();
+
+    // Teste  da barra de navegação.
+
+    expect(linkFavorite).toBeInTheDocument();
+  });
+
+  it('A aplicação é redirecionada para a página do link Home.', () => {
+    // Acessar os elementos da tela
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkHome = getByText('Home');
+    // Interagir com eles
+    fireEvent.click(linkHome);
+    const { pathname } = history.location;
+    // Fazer o teste
+    expect(pathname).toBe('/');
+  });
+
+  it('A aplicação é redirecionada para a página do link About', () => {
+    // Acessar os elementos da tela
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkAbout = getByText('About');
+    // Interagir com eles
+    fireEvent.click(linkAbout);
+    const { pathname } = history.location;
+    // Fazer o teste
+    expect(pathname).toBe('/about');
+  });
+
+  it('A aplicação é redirecionada para a página do link Favorite Pokémons', () => {
+    // Acessar os elementos da tela
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkFavorite = getByText('Favorite Pokémons');
+    // Interagir com eles
+    fireEvent.click(linkFavorite);
+    const { pathname } = history.location;
+    // Fazer o teste
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('A aplicação é redirecionada para a página do link "NotFound"', () => {
+    // Acessar os elementos da tela
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/page-not-found');
+    const NotFound = getByText('Page requested not found');
+    expect(NotFound).toBeInTheDocument();
+  });
 });
