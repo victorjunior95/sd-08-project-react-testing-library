10291f3b6862551f41851592abb9bed375abd1c2
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-03-11 22:40:49 -0300

    primeiro req

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,12 +3,44 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('testes', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Teste se a aplicação é redirecionada para a página inicial,', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkHome = getByText(/Home/i);
+    expect(linkHome).toBeInTheDocument();
+  });
+
+  test('este se a aplicação é redirecionada para a página de About, na URL', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkAbout = getByText(/About/i);
+    expect(linkAbout).toBeInTheDocument();
+  });
+
+  test('Teste se a aplicação é redirecionada para a página de Pokémons Favoritados', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkFavorites = getByText(/Favorite Pokémons/i);
+    expect(linkFavorites).toBeInTheDocument();
+  });
 });
